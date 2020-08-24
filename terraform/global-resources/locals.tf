518aacbe5f1b0d415b42ff85c0b23cfca7257a8b
Jake Mulley <jakemulley@users.noreply.github.com>
2020-08-24 14:37:13 +0100

    Update tagging with the MoJ tagging guidance

--- terraform/global-resources/locals.tf
+++ terraform/global-resources/locals.tf
@@ -1,9 +1,8 @@
 locals {
   global_resources {
-    application-name    = "Modernisation Platform - Global Resources"
-    business-unit       = "Ministry of Justice - Hosting"
-    tech-contact-email  = "modernisation-platform@digital.justice.gov.uk"
-    budget-holder-email = "steve.marshall@digital.justice.gov.uk"
-    stage               = "production"
+    business-unit = "Platforms"
+    application   = "Modernisation Platform"
+    is-production = true
+    owner         = "Modernisation Platform: modernisation-platform@digital.justice.gov.uk"
   }
 }
