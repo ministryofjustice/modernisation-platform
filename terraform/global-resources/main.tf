8d804557c4bcd33e21516862dbc1e68e279f72f6
jakemulley <jakemulley@users.noreply.github.com>
2020-09-15 11:30:22 +0100

    Add a public access block and enable S3 encryption

--- terraform/global-resources/main.tf
+++ terraform/global-resources/main.tf
@@ -1,9 +1,10 @@
 terraform {
   # `backend` blocks do not support variables, so the bucket name is hard-coded here, although created in the s3.tf file.
   backend "s3" {
-    bucket = "modernisation-platform-terraform-state"
-    region = "eu-west-2"
-    key    = "global-resources/terraform.tfstate"
+    bucket  = "modernisation-platform-terraform-state"
+    region  = "eu-west-2"
+    key     = "global-resources/terraform.tfstate"
+    encrypt = true
   }
 }
 
