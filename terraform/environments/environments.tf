e0e7da3ef14164390da69a42288fd4128e46a05b
jakemulley <jakemulley@users.noreply.github.com>
2020-10-05 10:18:34 +0100

    Simplify OU and account creation

--- terraform/environments/environments.tf
+++ terraform/environments/environments.tf
@@ -1,10 +1,6 @@
 module "environments" {
-  providers = {
-    aws = aws
-  }
   source                             = "github.com/ministryofjustice/modernisation-platform-terraform-environments"
   environment_directory              = "../../environments"
-  environment_types                  = ["production", "non-production"]
   environment_parent_organisation_id = local.environment_management.modernisation_platform_organisation_unit_id
   environment_prefix                 = "modernisation-platform"
 }
