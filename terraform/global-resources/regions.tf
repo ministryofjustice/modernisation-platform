a6eaa5ffef9e92a7f14bc33d3fc042404b3d0f07
jakemulley <jakemulley@users.noreply.github.com>
2020-10-06 10:43:34 +0100

    Add a provisioner to generate providers for each region enabled in our AWS account

---
+++ terraform/global-resources/regions.tf
@@ -0,0 +1,12 @@
+# As of 2020-09-29, there is an open issue regarding dynamic providers (hashicorp/terraform#24476).
+# Therefore, we need to generate a file that defines providers as "static" blocks.
+#
+# This generates a file (providers.tf) for additional providers, based on regions that are enabled in our AWS account.
+#
+# This file can be committed as it only holds regional aliases.
+data "aws_regions" "current" {}
+
+resource "local_file" "providers" {
+  filename = "providers.tf"
+  content  = templatefile("./templates/providers.tmpl", { regions: data.aws_regions.current.names })
+}
