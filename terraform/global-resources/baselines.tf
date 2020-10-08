2556335b34a9e567579d5e4855e824f0ac69e05f
jakemulley <jakemulley@users.noreply.github.com>
2020-10-08 13:48:25 +0100

    Add the dynamically generated baseline files

--- terraform/global-resources/baselines.tf
+++ terraform/global-resources/baselines.tf
@@ -1,3 +1,9 @@
 module "baselines" {
   source = "github.com/ministryofjustice/modernisation-platform-terraform-baselines"
+  baseline_directory = "./generated"
+  baseline_provider_key = "aws"
+}
+
+module "generated" {
+  source = "./generated/aws"
 }
