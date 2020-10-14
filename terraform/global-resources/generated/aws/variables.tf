2afad614423a5574861f57a0467c5d71624858ae
jakemulley <jakemulley@users.noreply.github.com>
2020-10-14 11:24:58 +0100

    Update generated files for baselines

--- terraform/global-resources/generated/aws/variables.tf
+++ terraform/global-resources/generated/aws/variables.tf
@@ -1,10 +1,15 @@
 variable "baseline_assume_role" {
   type        = string
-  description = "Which role to assume to manage these resources"
+  description = "Role ARN to assume to manage these resources"
   default     = ""
 }
 
 variable "baseline_tags" {
   type        = map
   description = "Tags to apply to taggable resources"
+}
+
+variable "baseline_root_account_id" {
+  type        = string
+  description = "The AWS Organisations root account ID that this account should be part of"
 }
