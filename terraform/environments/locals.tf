817aaa43360cf3d980ab6bd0f3d58e762043f9ab
jakemulley <jakemulley@users.noreply.github.com>
2020-10-16 14:12:54 +0100

    Update baselines module for environments

--- terraform/environments/locals.tf
+++ terraform/environments/locals.tf
@@ -1,3 +1,10 @@
+# This data sources allows us to get the Modernisation Platform account information for use elsewhere
+# (when we want to assume a role in the MP, for instance)
+data "aws_organizations_organization" "root_account" {}
+data "aws_regions" "current" {
+  provider = aws.modernisation-platform
+}
+
 locals {
   environments = {
     business-unit = "Platforms"
@@ -5,11 +12,7 @@ locals {
     is-production = true
     owner         = "Modernisation Platform: modernisation-platform@digital.justice.gov.uk"
   }
-}
-
-# This allows us to get the Modernisation Platform account information for use elsewhere
-# (when we want to assume a role in the MP, for instance)
-data "aws_organizations_organization" "root" {}
-locals {
-  modernisation_platform_account = data.aws_organizations_organization.root.accounts[index(data.aws_organizations_organization.root.accounts[*].email, "aws+modernisation-platform@digital.justice.gov.uk")]
+  root_account                   = data.aws_organizations_organization.root_account
+  modernisation_platform_account = local.root_account.accounts[index(local.root_account.accounts[*].email, "aws+modernisation-platform@digital.justice.gov.uk")]
+  account_regions                = data.aws_regions.current.names
 }
