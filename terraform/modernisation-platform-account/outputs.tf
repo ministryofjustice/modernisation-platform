825f3767087282b9a6aaf9d2d9a6ac045e6c8c23
jakemulley <jakemulley@users.noreply.github.com>
2020-11-20 16:45:37 +0000

    Remove IAM federated access

--- terraform/modernisation-platform-account/outputs.tf
+++ terraform/modernisation-platform-account/outputs.tf
@@ -2,8 +2,3 @@ output "iam_superadmin_passwords" {
   value     = module.iam.superadmin_passwords
   sensitive = true
 }
-
-output "auth0_login_link" {
-  value     = module.auth0.saml_login_page
-  sensitive = false
-}
