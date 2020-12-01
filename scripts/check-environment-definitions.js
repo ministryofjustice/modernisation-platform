8fcac58fe475e2ceaadb84ba2e88f3351b9b5806
jakemulley <jakemulley@users.noreply.github.com>
2020-12-01 12:04:38 +0000

    Remove name check from check-environment-definitions.js

--- scripts/check-environment-definitions.js
+++ scripts/check-environment-definitions.js
@@ -5,7 +5,7 @@ module.exports = async ({ github, context }) => {
     // Get all configuration files
     const configurations = await utilities.getFilesRecursively('./environments').then(configuration => configuration.map(application => {
       // Ensure each configuration has the correct keys and/or tags
-      const missingConfiguration = utilities.checkKeys(application, ['name', 'environments', 'tags'])
+      const missingConfiguration = utilities.checkKeys(application, ['environments', 'tags'])
       const missingRequiredTags = utilities.checkKeys(application.tags, ['business-unit', 'application', 'owner'])
 
       return {
