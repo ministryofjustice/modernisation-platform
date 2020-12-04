771bc9cabc48065b1674c0535de85b66c4c1e895
jakemulley <jakemulley@users.noreply.github.com>
2020-12-04 17:01:35 +0000

    Change from count to for_each

--- terraform/environments/core-network-services/transit-gateway.tf
+++ terraform/environments/core-network-services/transit-gateway.tf
@@ -16,11 +16,13 @@ resource "aws_ec2_transit_gateway" "TGW" {
     },
   )
 }
+
 #########################
 # Route table and routes
 #########################
 resource "aws_ec2_transit_gateway_route_table" "TGW_route_table" {
-  for_each           = toset(var.env_vpcs)
+  for_each = toset(var.env_vpcs)
+
   transit_gateway_id = aws_ec2_transit_gateway.TGW.id
   tags = merge(
     local.tags,
@@ -29,18 +31,27 @@ resource "aws_ec2_transit_gateway_route_table" "TGW_route_table" {
     },
   )
 }
+
 #########################
 # VPC attachment
 #########################
 resource "aws_ec2_transit_gateway_vpc_attachment" "attachments" {
-  for_each                                        = toset(var.env_vpcs)
-  transit_gateway_id                              = aws_ec2_transit_gateway.TGW.id
-  vpc_id                                          = each.value == "live" ? module.live_vpc.vpc_id : module.non_live_vpc.vpc_id
-  subnet_ids                                      = each.value == "live" ? module.live_vpc.private_tgw_subnet_ids : module.non_live_vpc.private_tgw_subnet_ids
+  for_each = toset(var.env_vpcs)
+
+  transit_gateway_id = aws_ec2_transit_gateway.TGW.id
+  vpc_id             = each.value == "live" ? module.live_vpc.vpc_id : module.non_live_vpc.vpc_id
+  subnet_ids         = each.value == "live" ? module.live_vpc.private_tgw_subnet_ids : module.non_live_vpc.private_tgw_subnet_ids
+
+  # Turn off default route table association and propogation, as we're providing our own
   transit_gateway_default_route_table_association = false
   transit_gateway_default_route_table_propagation = false
-  dns_support                                     = "enable"
-  ipv6_support                                    = "disable"
+
+  # Enable DNS support
+  dns_support = "enable"
+
+  # Turn off IPv6 support
+  ipv6_support = "disable"
+
   tags = merge(
     local.tags,
     {
@@ -48,23 +59,28 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "attachments" {
     },
   )
 }
+
 ##########################
 # Route table association
 ##########################
 resource "aws_ec2_transit_gateway_route_table_association" "tables" {
-  count                          = length(var.env_vpcs)
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.attachments[var.env_vpcs[count.index]].id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.TGW_route_table[var.env_vpcs[count.index]].id
+  for_each = toset(var.env_vpcs)
+
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.attachments[each.value].id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.TGW_route_table[each.value].id
 }
 
 resource "aws_ec2_transit_gateway_route_table_propagation" "propagation" {
-  count                          = length(var.env_vpcs)
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.attachments[var.env_vpcs[count.index]].id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.TGW_route_table[var.env_vpcs[count.index]].id
+  for_each = toset(var.env_vpcs)
+
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.attachments[each.value].id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.TGW_route_table[each.value].id
 }
+
 resource "aws_ec2_transit_gateway_route" "nat_route" {
-  count                          = length(var.env_vpcs)
+  for_each = toset(var.env_vpcs)
+
   destination_cidr_block         = "0.0.0.0/0"
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.attachments[var.env_vpcs[count.index]].id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.TGW_route_table[var.env_vpcs[count.index]].id
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.attachments[each.value].id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.TGW_route_table[each.value].id
 }
