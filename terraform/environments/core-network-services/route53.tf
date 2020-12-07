930d57c2e954a1f0935316b808c2d591a7240cb7
jakemulley <jakemulley@users.noreply.github.com>
2020-12-07 14:39:14 +0000

    Remote Supervision: NS delegation

--- terraform/environments/core-network-services/route53.tf
+++ terraform/environments/core-network-services/route53.tf
@@ -1,4 +1,37 @@
+locals {
+  modernisation-platform-domain = "modernisation-platform.service.justice.gov.uk"
+}
+
 resource "aws_route53_zone" "modernisation-platform" {
-  name = "modernisation-platform.service.justice.gov.uk"
+  name = local.modernisation-platform-domain
   tags = local.tags
+}
+
+# Remote Supervision NS delegation
+resource "aws_route53_record" "remote-supervision-production" {
+  allow_overwrite = true
+  name            = "remote-supervision-production.${local.modernisation-platform-domain}"
+  ttl             = 30
+  type            = "NS"
+  zone_id         = aws_route53_zone.modernisation-platform.zone_id
+  records = [
+    "ns-1127.awsdns-12.org.",
+    "ns-123.awsdns-15.com.",
+    "ns-643.awsdns-16.net.",
+    "ns-1848.awsdns-39.co.uk."
+  ]
+}
+
+resource "aws_route53_record" "remote-supervision-non-production" {
+  allow_overwrite = true
+  name            = "remote-supervision-non-production.${local.modernisation-platform-domain}"
+  ttl             = 30
+  type            = "NS"
+  zone_id         = aws_route53_zone.modernisation-platform.zone_id
+  records = [
+    "ns-1127.awsdns-12.org.",
+    "ns-123.awsdns-15.com.",
+    "ns-643.awsdns-16.net.",
+    "ns-1848.awsdns-39.co.uk."
+  ]
 }
