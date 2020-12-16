8ef7b20b1b2f8d67e8aab59069e705a6a517960d
jakemulley <jakemulley@users.noreply.github.com>
2020-12-16 18:01:02 +0000

    Update core-vpc

--- terraform/environments/core-vpc/.terraform.lock.hcl
+++ terraform/environments/core-vpc/.terraform.lock.hcl
@@ -4,6 +4,7 @@
 provider "registry.terraform.io/hashicorp/aws" {
   version = "3.21.0"
   hashes = [
+    "h1:YGpoWikUMRAqKJAzT5llGi4fE/ce/MFqfXg7m0f7VNA=",
     "h1:jvcUKDDNFXjIRWoxfw55vvfFTlSgnLMYe1qiSZNtWoE=",
     "zh:11b8c85d063775029245e86ebce346ed86aa77aefaa72dc558da37ea347aa77c",
     "zh:15f0f5bcbcffe1d2aeaa595f6573f0779932bd3a7647f28ad6aacf1a20f35562",
@@ -32,5 +33,22 @@ provider "registry.terraform.io/hashicorp/null" {
     "zh:c2e07b49b6efa676bdc7b00c06333ea1792a983a5720f9e2233db27323d2707c",
     "zh:cdc8fe1096103cf5374751e2e8408ec4abd2eb67d5a1c5151fe2c7ecfd525bef",
     "zh:dbdef21df0c012b0d08776f3d4f34eb0f2f229adfde07ff252a119e52c0f65b7",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/time" {
+  version = "0.6.0"
+  hashes = [
+    "h1:AzpZsovYtv0/V2/lp/4chIiaXxPWaAOpqqoXceNAW7g=",
+    "zh:130c61cb41888107bf1e2f1db05b4f664d63af70e7828c0ab16be9fc5302aede",
+    "zh:16132715a10cb31dc6ce6d9320904f84ddb32cbb74516e528009463370d6f715",
+    "zh:2fb8701ac13115bd92187a9c6e47ca2ecedf98943e48e3f5d8a8f3f4bd3fcdf4",
+    "zh:30b4b44748fbfa592a22664c31ac5f664ca5fd18265442a85cd0b41a99b21c86",
+    "zh:5205fa477ebcb1f7d82a701a9a511c6fd555ec209e523081d65b38a5a3423db5",
+    "zh:7584700dc1421f22f6352d61859774e9f6151c48e490f2c0f2ae019274bfd028",
+    "zh:7cc58b561fbb6c86a4f4a7e670434381a1ffa664ca559702fc7eb79ab36164e4",
+    "zh:b7d119ce2f948311a2acb8b32e027562cd291116ecf93af788318c168f82a856",
+    "zh:bd759418034e245f7ca4914ae72c0fbd82cedec12b32980f6ea74aa9c474ba7f",
+    "zh:c06c65224ee9aa8634f8d44552bc803aac0a3a8949b865137873c56d6b2025d7",
   ]
 }
--- terraform/environments/core-vpc/vpc.tf
+++ terraform/environments/core-vpc/vpc.tf
@@ -1,81 +1,36 @@
 locals {
-  production-vpc = {
-    live = {
-      cidr = {
-        vpc = "10.232.0.0/18"
-        subnets = {
-          tgw = [
-            "10.232.48.0/28",
-			      "10.232.48.16/28",
-			      "10.232.48.32/28"
-          ]
-          private = [
-            "10.232.0.0/21",
-			      "10.232.8.0/21",
-			      "10.232.16.0/21"
-          ]
-          dmz = [
-            "10.232.24.0/22",
-			      "10.232.28.0/22",
-		      	"10.232.32.0/22"
-          ]
-          data = [
-           "10.232.36.0/22",
-			     "10.232.40.0/22",
-			     "10.232.44.0/22"
-          ]
-        }
-      }
+  # Get all VPC definitions by type
+  vpcs = {
+    # VPCs that sit within the core-vpc-production account
+    core-vpc-production = {
+      for file in fileset("vpcs", "*-production.json") :
+      replace(file, ".json", "") => jsondecode(file("vpcs/${file}"))
     }
-  }
-    non_live_data-vpc = {
-      non-live = {
-       cidr = {
-        vpc = "10.232.128.0/18"
-        subnets = {
-          tgw = [
-            "10.232.176.0/28",
-			      "10.232.176.16/28",
-			      "10.232.176.32/28"
-          ]
-          private = [
-            "10.232.128.0/21",
-			      "10.232.136.0/21",
-			      "10.232.144.0/21"
-          ]
-          dmz = [
-           "10.230.152.0/22",
-			     "10.230.156.0/22",
-			     "10.230.160.0/22"
-          ]
-          data = [
-           "10.232.164.0/22",
-			     "10.232.168.0/22",
-		       "10.232.172.0/22"
-          ]
-        }
-      }
+    # VPCs that sit within the core-vpc-non-live-data account
+    core-vpc-non-live-data = {
+      for file in fileset("vpcs", "*-non-live-data.json") :
+      replace(file, ".json", "") => jsondecode(file("vpcs/${file}"))
     }
-  }
-  
-
-  useful_vpc_ids = {
-    for key in keys(local.production-vpc) :
-    key => {
-      vpc_id                 = module.vpc[key].vpc_id
-      private_tgw_subnet_ids = module.vpc[key].tgw_subnet_ids
-    }
+    # VPCs that sit within the core-vpc-pre-production account
+    # core-vpc-pre-production = {
+    #   for file in fileset("vpcs", "*-pre-production.json") :
+    #     replace(file, ".json", "") => jsondecode(file("env/${file}"))
+    # }
   }
 }
 
 module "vpc" {
-  for_each = terraform.workspace == "core-vpc-production" ? local.production-vpc : {}
-  
-  source   = "../../modules/core-vpc"
+  for_each = local.vpcs[terraform.workspace]
+
+  source = "../../modules/core-vpc"
 
   # CIDRs
   subnet_cidrs_by_type = each.value.cidr.subnets
   vpc_cidr             = each.value.cidr.vpc
+
+  # NACL rules
+  nacl_ingress = each.value.nacl.ingress
+  nacl_egress  = each.value.nacl.egress
 
   # NAT Gateway
   enable_nat_gateway = false
@@ -84,4 +39,3 @@ module "vpc" {
   tags_common = {}
   tags_prefix = each.key
 }
-
