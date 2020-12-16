9f07b3aec4efc863714dca43e0b54c22cf558010
jakemulley <jakemulley@users.noreply.github.com>
2020-12-16 17:55:40 +0000

    Update core-shared-services

--- terraform/environments/core-shared-services/locals.tf
+++ terraform/environments/core-shared-services/locals.tf
@@ -1,8 +1,9 @@
 locals {
   tags = {
     business-unit = "Platforms"
-    application   = "Modernisation Platform: core-shared-services"
-    is-production = true
+    application   = "Modernisation Platform: ${terraform.workspace}"
+    is-production = substr(terraform.workspace, length(terraform.workspace) - length("production"), length(terraform.workspace)) == "production" ? true : false
     owner         = "Modernisation Platform: modernisation-platform@digital.justice.gov.uk"
   }
+  environment_management = jsondecode(data.aws_secretsmanager_secret_version.environment_management.secret_string)
 }
