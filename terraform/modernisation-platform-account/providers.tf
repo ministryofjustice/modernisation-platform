b4c155b00b96312a910fdf6b2dcd6aae7a909108
jakemulley <jakemulley@users.noreply.github.com>
2020-12-31 09:47:53 +0000

    Restrict baselines to certain regions

--- terraform/modernisation-platform-account/providers.tf
+++ terraform/modernisation-platform-account/providers.tf
@@ -78,8 +78,19 @@ provider "aws" {
   alias  = "modernisation-platform-us-west-2"
 }
 
+locals {
+  enabled_baseline_regions = [
+    "eu-central-1", # Europe (Frankfurt)
+    "eu-north-1",   # Europe (Stockholm)
+    "eu-west-1",    # Europe (Ireland)
+    "eu-west-2",    # Europe (London)
+    "eu-west-3",    # Europe (Paris)
+    "us-east-1",    # US East (N. Virginia) (for global services)
+  ]
+}
+
 module "baselines-modernisation-platform" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-baselines"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-baselines?ref=regional-enablement"
   providers = {
     # Default and replication regions
     aws                    = aws.modernisation-platform-eu-west-2
@@ -103,6 +114,28 @@ module "baselines-modernisation-platform" {
     aws.us-west-1      = aws.modernisation-platform-us-west-1
     aws.us-west-2      = aws.modernisation-platform-us-west-2
   }
+
+  # Regions to enable IAM Access Analyzer in
+  enabled_access_analyzer_regions = local.enabled_baseline_regions
+
+  # Regions to enable AWS Backup in
+  enabled_backup_regions = local.enabled_baseline_regions
+
+  # Regions to enable AWS Config in
+  enabled_config_regions = local.enabled_baseline_regions
+
+  # Regions to enable EBS encryption in
+  enabled_ebs_encryption_regions = local.enabled_baseline_regions
+
+  # Regions to enable GuardDuty in
+  enabled_guardduty_regions = local.enabled_baseline_regions
+
+  # Regions to enable Security Hub in
+  enabled_securityhub_regions = local.enabled_baseline_regions
+
+  # Regions to enable default VPC configuration and VPC Flow Logs in
+  enabled_vpc_regions = local.enabled_baseline_regions
+
   root_account_id = local.root_account.master_account_id
   tags            = local.tags
 }
