05c0acfc89447410c806c8e8831af2a1435455e3
jakemulley <jakemulley@users.noreply.github.com>
2021-01-05 19:49:56 +0000

    Add correct debug output

--- terraform/modules/member-vpc/outputs.tf
+++ terraform/modules/member-vpc/outputs.tf
@@ -31,7 +31,7 @@ output "tgw_subnet_ids" {
 # }
 
 output "debug" {
-  value = local.expanded_rules_with_keys
+  value = local.expanded_default_nacl_rules_and_subnets_with_keys
 }
 output "expanded_worker_subnets_assocation" {
   value = local.expanded_worker_subnets_assocation
