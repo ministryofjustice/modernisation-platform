4f1030bed6a278b322a6c3270b8a54a44cd25e6d
jakemulley <jakemulley@users.noreply.github.com>
2021-01-26 10:50:53 +0000

    Update core-vpc templates to match new structure

--- terraform/environments/core-vpc/locals.tf
+++ terraform/environments/core-vpc/locals.tf
@@ -1,9 +1,15 @@
 locals {
+  application_name       = "core-vpc"
   environment_management = jsondecode(data.aws_secretsmanager_secret_version.environment_management.secret_string)
+
+  # This takes the name of the Terraform workspace (e.g. core-vpc-production), strips out the application name (e.g. core-vpc), and checks if
+  # the string leftover is `-production`, if it isn't (e.g. core-vpc-non-production => -non-production) then it sets the var to false.
+  is-production = substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production"
+
   tags = {
     business-unit = "Platforms"
     application   = "Modernisation Platform: core-vpc"
-    is-production = substr(terraform.workspace, length(terraform.workspace) - length("production"), length(terraform.workspace)) == "production" ? true : false
+    is-production = local.is-production
     owner         = "Modernisation Platform: modernisation-platform@digital.justice.gov.uk"
   }
 }
