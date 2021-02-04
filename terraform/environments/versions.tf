b13099d8fb466a5c1f208a866518b57c88d5fdc9
jakemulley <jakemulley@users.noreply.github.com>
2021-02-04 12:30:59 +0000

    Update required providers to always specify a minimum AWS provider and Terraform version

--- terraform/environments/versions.tf
+++ terraform/environments/versions.tf
@@ -1,13 +1,9 @@
 terraform {
-  required_version = ">= 0.14.2"
   required_providers {
     aws = {
       version = ">= 3.20.0"
       source  = "hashicorp/aws"
     }
-    random = {
-      version = ">= 3.0.0"
-      source  = "hashicorp/random"
-    }
   }
+  required_version = ">= 0.14.2"
 }
