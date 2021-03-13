fb34fab00f1fa4a862981db0b958fd921838c575
user01 <user01@pop-os.localdomain>
2021-03-13 20:34:36 +0000

    added config files for member avvount setup for subnetset and certificate manager

--- terraform/environments/core-sandbox/locals.tf
+++ terraform/environments/core-sandbox/locals.tf
@@ -5,6 +5,7 @@ locals {
   # This takes the name of the Terraform workspace (e.g. core-vpc-production), strips out the application name (e.g. core-vpc), and checks if
   # the string leftover is `-production`, if it isn't (e.g. core-vpc-non-production => -non-production) then it sets the var to false.
   is-production = substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production"
+  is-preproduction = substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-preproduction"
 
   tags = {
     business-unit = "Platforms"
@@ -12,4 +13,9 @@ locals {
     is-production = local.is-production
     owner         = "Modernisation Platform: modernisation-platform@digital.justice.gov.uk"
   }
+
+  json_data = jsondecode(file("networking.auto.tfvars.json"))
+
+  acm_pca = [ substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production" || substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-preproduction" ? "acm-pca-live" : "acm-pca-non-live" ]
+
 }
