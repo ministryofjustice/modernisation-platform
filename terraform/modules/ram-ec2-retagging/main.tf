57d43def80ea5b09b86c5ba1694d8139b21e25ee
user01 <user01@pop-os.localdomain>
2021-03-16 11:13:35 +0000

    added code to tag ram resources to member aws accounts

--- terraform/modules/ram-ec2-retagging/main.tf
+++ terraform/modules/ram-ec2-retagging/main.tf
@@ -24,30 +24,60 @@ resource "aws_ec2_tag" "vpc" {
 
 # Get all subnet IDs in the associated principal account
 data "aws_subnet_ids" "associated" {
-  provider = aws.share-host
+  provider = aws.share-tenant
 
   vpc_id = data.aws_vpc.host.id
 
+}
+
+# Get corresponding subnets, including their tags, from the host account
+data "aws_subnet_ids" "host" {
+  provider = aws.share-host
+
+  vpc_id = data.aws_vpc.host.id
+  
+  
   tags = {
+    Name = "${var.vpc_name}-${var.subnet_set}*"
+  }
+
+}
+
+# List all subnets in the host account
+data "aws_subnet" "host" {
+  
+  provider = aws.share-host
+
+  for_each = data.aws_subnet_ids.host.ids
+  id       = each.key
+
+   tags = {
     Name = "${var.vpc_name}-${var.subnet_set}*"
   }
 }
 
-# Get corresponding subnets, including their tags, from the host account
-data "aws_subnet" "host" {
-  provider = aws.share-host
 
-  for_each = data.aws_subnet_ids.associated.ids
-
-  vpc_id = data.aws_vpc.host.id
-  id     = each.key
-}
 
 # Retag subnets in the associated account
 resource "aws_ec2_tag" "subnets" {
+  
   for_each = data.aws_subnet.host
 
   resource_id = each.value.id
   key         = "Name"
   value       = each.value.tags.Name
 }
+
+
+
+# output "aws_subnet_host" {
+  
+#   #garden-production-patio
+#   value = data.aws_subnet.host
+# }
+
+# output "aws_subnet_tennant" {
+  
+#   #garden-production-patio
+#   value = data.aws_subnet_ids.associated
+# }
