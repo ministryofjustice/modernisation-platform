c389f5320227ba97859730c67e1c2cc097d136ff
philhorrocks <56348247+philhorrocks@users.noreply.github.com>
2021-03-18 11:52:26 +0000

    Update locals.tf
    fixed merge conflict

--- terraform/environments/core-sandbox/locals.tf
+++ terraform/environments/core-sandbox/locals.tf
@@ -17,16 +17,11 @@ locals {
     owner         = "Modernisation Platform: modernisation-platform@digital.justice.gov.uk"
   }
 
-<<<<<<< HEAD
-
   environment = trimprefix(terraform.workspace, "${var.networking[0].application}-")
   vpc_name = var.networking[0].business-unit 
   subnet_set = var.networking[0].set
   acm_pca = [substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production" || substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-preproduction" ? "acm-pca-live" : "acm-pca-non-live"]
 
-  #json_data = fileexists("networking.auto.tfvars.json") ? file("networking.auto.tfvars.json") : local.default_content
-
-  #file_exists = fileexists("networking.auto.tfvars.json") ? tobool(true) : tobool(false)
 }
 
 variable "networking" {
@@ -34,74 +29,3 @@ variable "networking" {
    type=list(any)
 
 }
-
-output "environment" {
-  
-  value = local.environment
-}
-
-output "vpc_name" {
-  
-  value = var.networking[0].business-unit 
-}
-=======
-  #Required for the false condition 
-  default_content = jsonencode(
-    {
-      networking = [
-        {
-          application   = ""
-          business-unit = ""
-          set           = ""
-        },
-      ]
-    }
-  )
-
-  json_data = fileexists("networking.auto.tfvars.json") ? file("networking.auto.tfvars.json") : local.default_content
->>>>>>> 470cf89972b8c743e85f4bc22e93f49196c22900
-
-output "subnet_set" {
-  
-  value = var.networking[0].set
-}
-
-
-  # #Required for the false condition 
-  # default_content = jsonencode(
-  #       {
-  #          networking = [
-  #              {
-  #                  application   = ""
-  #                  business-unit = ""
-  #                  set           = ""
-  #               },
-  #           ]
-  #       }
-  #   )
-  
-  # json_data = fileexists("networking.auto.tfvars.json") ? file("networking.auto.tfvars.json") : local.default_content
-
-  # file_exists = fileexists("networking.auto.tfvars.json") ? tobool(true) : tobool(false)
-
-<<<<<<< HEAD
-  #acm_pca = [substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production" || substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-preproduction" ? "acm-pca-live" : "acm-pca-non-live"]
-=======
-  vpc_name = jsondecode(local.json_data).networking[0].business-unit
->>>>>>> 470cf89972b8c743e85f4bc22e93f49196c22900
-
-  # subnet_set = jsondecode(local.json_data).networking[0].set
-
-<<<<<<< HEAD
-  # vpc_name = jsondecode(local.json_data).networking[0].business-unit 
-
-  # environment = substr(terraform.workspace, length(local.application_name), length(terraform.workspace))
-
-  # provider = "aws.core-vpc${local.environment}"
-  
-  
-=======
-  provider = "aws.core-vpc${local.environment}"
-
-}
->>>>>>> 470cf89972b8c743e85f4bc22e93f49196c22900
