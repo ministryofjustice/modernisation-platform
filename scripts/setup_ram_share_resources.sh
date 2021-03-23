e47808db55515eb90edb89f4e117e219e7b5fa1b
modernisation-platform-ci <modernisation-platform+github@digital.justice.gov.uk>
2021-03-23 16:31:50 +0000

    Workflow: created files in terraform/environments

--- scripts/setup_ram_share_resources.sh
+++ scripts/setup_ram_share_resources.sh
@@ -2,67 +2,87 @@
 
 basedir=terraform/environments
 
-envs=("production", "preproduction", "test", "development")
+envs=("production" "preproduction" "test" "development")
+#envs=("production")
 
 application_name="$1"
 
-echo "Setting up for application: $application_name"
-
 setup_ram_share_core() {
 
-    #Runs a Terraform plan/apply in the core-vpc-<env> to create the RAM share and association
+
+  if [[ "$application_name" != " " ]]; then
 
     for env in "${envs[@]}"; do
 
-    echo "Running terraform across core accounts core-vpc-$env"
+      #Runs a Terraform plan/apply in the core-vpc-<env> to create the RAM share and association
+      echo "[+] Selecting terraform workspace: core-vpc-$env"
 
-    # Select workspace
-    select_workspace=`terraform -chdir=$basedir/core-vpc workspace select core-vpc-$env`
+      echo "[+] Running Terraform init"
+      ./scripts/terraform-init.sh "$basedir/core-vpc"   
+      
+      # Select workspace
+      workspace=$(terraform -chdir="$basedir/core-vpc" workspace select "core-vpc-${env}")
+      
+      if [[ "${workspace}" ]]; then
 
-    if [[ $select_workspace ]]; then
+        echo "[+] Running Terraform plan"     
+        # Run terraform plan
+        ./scripts/terraform-plan.sh "$basedir/core-vpc"
 
-       echo "Terraform init"
-      ./scripts/terraform-init.sh "$basedir/core-vpc"
-
-      echo "Terraform plan"
-      # Run terraform plan
-      ./scripts/terraform-plan.sh "$basedir/core-vpc"
-
-      # Run terraform apply
-      #./scripts/terraform-apply.sh $basedir/core-vpc
-    fi
-    echo "Finished running terraform across vpc-core accounts"
-   
+        # Run terraform apply
+        #./scripts/terraform-apply.sh $basedir/core-vpc
+      fi
     done
+        echo "[+] Finished running terraform across vpc-core accounts"
+        echo "[+] Running RAM share association"
+    else
+      echo "[+] No new AWS accounts to process\n"
+  fi
+
+}
 
 setup_ram_share_association() {
 
-    #Runs a Terraform plan/apply in the member-vpc workspace to setup the RAM association
+  if [[ "$application_name" != " " ]]; then
 
+    echo "[+] processing ====> "${application_name}""
+
+    #Runs a Terraform plan/apply in the member-vpc workspace to setup the RAM association
     for env in "${envs[@]}"; do
 
-    echo "Running terraform across new workspace $application_name-$env"
+      #convert string to array
+      IFS=' ' read -ra get_apps <<< "${application_name}"
 
-    # Select workspace
-    select_workspace=`terraform -chdir=$basedir/$application_name workspace select $application-$env`
+      for new_app in "${get_apps[@]}"; do
 
-    if [[ $select_workspace ]]; then
+        echo "[+] Running Terraform init"
+        ./scripts/terraform-init.sh "$basedir/$new_app" 
         
-       echo "Terraform init" 
-      ./scripts/terraform-init.sh "$basedir/$application_name"
+        #Runs a Terraform plan/apply in the core-vpc-<env> to create the RAM share and association
+        echo "[+] Selecting terraform application workspace ====> $new_app-$env"
 
-        # Run terraform plan
-      echo "Terraform plan"
-      ./scripts/terraform-plan.sh "$basedir/$application_name"
+        # Select workspace
+        workspace=$(terraform -chdir="$basedir/$new_app" workspace select "$new_app-$env")
+        
+        if [[ "${workspace}" ]]; then
 
-      # Run terraform apply
-      #./scripts/terraform-apply.sh $basedir/$application_name
-    fi 
-    echo "Finished running terraform across new accoutn: $application_name"
+         echo "[+] Running Terraform plan"
+        
+         # Run terraform plan
+         ./scripts/terraform-plan.sh "$basedir/$new_app"
 
-    done
+         # Run terraform apply
+         #./scripts/terraform-apply.sh "$basedir/$new_app"
+        fi
+      done
+  done
+  echo "[+] Finished running terraform across member accounts"
+  else
+      echo "[+] No RAM associations to process"
+  fi
 }
-}
+
 
 setup_ram_share_core
 setup_ram_share_association
+
