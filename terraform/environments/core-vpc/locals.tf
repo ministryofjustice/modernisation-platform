4a34dd35b27270924ec4808a35e236042fed2765
Don Masters <don.masters@digital.justice.gov.uk>
2021-04-06 08:59:27 +0100

    bugfix/correct-core-vpc-routing-issue

--- terraform/environments/core-vpc/locals.tf
+++ terraform/environments/core-vpc/locals.tf
@@ -5,7 +5,7 @@ locals {
   # This takes the name of the Terraform workspace (e.g. core-vpc-production), strips out the application name (e.g. core-vpc), and checks if
   # the string leftover is `-production`, if it isn't (e.g. core-vpc-non-production => -non-production) then it sets the var to false.
   is-production = substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production"
-  is-live_data  = substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "production"
+  is-live_data  = (substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production") || (substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-preproduction")
 
   tags = {
     business-unit = "Platforms"
