74522bb85ff4ca9007e2034f1e239131977948eb
Don Masters <don.masters@digital.justice.gov.uk>
2021-04-28 15:35:23 +0100

    feature/refactor-state-creation-script
    - Refactored orginal script
    - Added functionality for terraform state file creation for
      modernisation-platform-enivornments repositiory

--- scripts/provision-terraform-workspaces.sh
+++ scripts/provision-terraform-workspaces.sh
@@ -19,79 +19,125 @@
 # within their terraform/environments/${application_name} subdirectory.
 # Use case: to ensure a single applications workspaces are configured (e.g. if a new environment is created).
 
-TMP_DIR=tmp/terraform-workspaces
-TF_ENV_DIR=terraform/environments
+iterate_environments_bootstrap() {
+# set friendly Parameter names
+BOOTSTRAP_TYPE="${1}"  # this value can equal (delegate-access, secure-baselines or single-sign-on)
 
-iterate_environments() {
+
 # Loop through each application json file
-for JSON_FILE in ${git_dir}/environments/${3}.json
+for JSON_FILE in ${git_dir}/environments/*.json
 do
 APPLICATION=`basename "${JSON_FILE}" .json`
-
-  # Build temporary folder to emulate real folder
-  if  [ "${2}" != "bootstrap" ]
-  then
-      if [ -d "${git_dir}/tmp" ]
-      then
-        rm -r "${git_dir}/tmp"
-      fi
-      mkdir "${git_dir}/tmp"
-      # Copy files to emulation folder
-      sed "s/\$application_name/${APPLICATION}/g" "${git_dir}/terraform/templates/backend.tf" > "${git_dir}/tmp/backend.tf"
-      sed "s/\$application_name/${APPLICATION}/g" "${git_dir}/terraform/templates/locals.tf" > "${git_dir}/tmp/locals.tf"
-      cp "${git_dir}/terraform/templates/providers.tf" "${git_dir}/tmp/providers.tf"
-      cp "${git_dir}/terraform/templates/secrets.tf" "${git_dir}/tmp/secrets.tf"
-      cp "${git_dir}/terraform/templates/versions.tf" "${git_dir}/tmp/versions.tf"
-  fi
 
   # Loop through each environment for specific application
   for ENV in `cat "${JSON_FILE}" | jq -r --arg FILENAME "${APPLICATION}" '.environments[].name'`
   do
     # Check if state file exists in S3
-    if [ "${2}" = "bootstrap" ]
-    then
-      # For Bootstrap files
-      aws s3api head-object --bucket modernisation-platform-terraform-state --key "environments/${1}/${APPLICATION}-${ENV}/terraform.tfstate"  > /dev/null 2>&1
+      aws s3api head-object --bucket modernisation-platform-terraform-state --key "environments/bootstrap/${BOOTSTRAP_TYPE}/${APPLICATION}-${ENV}/terraform.tfstate"  > /dev/null 2>&1
       RETURN_CODE="${?}"
-    else
-      aws s3api head-object --bucket modernisation-platform-terraform-state --key "environments/${APPLICATION}/${APPLICATION}-${ENV}/terraform.tfstate" > /dev/null 2>&1 
-      RETURN_CODE="${?}"
-    fi
     
-    echo "${1}       ${APPLICATION}-${ENV}:-------Return Code: ${RETURN_CODE}"
-  
     if [[ "${RETURN_CODE}" -ne 0 ]]
     then
-      [ "${2}" = "bootstrap" ] && TERRAFORM_PATH="${git_dir}/terraform/environments/${1}" || TERRAFORM_PATH="${git_dir}/tmp"
-      # move to Terraform environment folder
-      echo "terraform working folder:    ${TERRAFORM_PATH}"
+      TERRAFORM_PATH="${git_dir}/terraform/environments/bootstrap/${BOOTSTRAP_TYPE}"
+      echo -en "BOOTSTRAP - ${BOOTSTRAP_TYPE} - ${APPLICATION}-${ENV} - ${YELLOW}CREATING${NORMAL}\n"
       terraform -chdir="${TERRAFORM_PATH}" init > /dev/null
       terraform -chdir="${TERRAFORM_PATH}" workspace new "${APPLICATION}-${ENV}"
+    else
+      echo -en "BOOTSTRAP - ${BOOTSTRAP_TYPE} - ${APPLICATION}-${ENV} - ${GREEN}EXISTS${NORMAL}\n"
     fi
  
   done
 done
 }
 
+
+iterate_environments_member() {
+# set friendly Parameter names
+ENVIRONMENT_TYPE="${1}"  # this value can equal (* or an application name)
+
+# Loop through each application json file
+for JSON_FILE in ${git_dir}/environments/${ENVIRONMENT_TYPE}.json
+do
+  APPLICATION=`basename "${JSON_FILE}" .json`
+
+  # Build temporary folder to emulate real folder
+  [ -d "${git_dir}/tmp" ] && rm -r "${git_dir}/tmp"
+  mkdir "${git_dir}/tmp"
+
+  # Copy files to emulation folder
+  sed "s/\$application_name/${APPLICATION}/g" "${git_dir}/terraform/templates/backend.tf" > "${git_dir}/tmp/backend.tf"
+  sed "s/\$application_name/${APPLICATION}/g" "${git_dir}/terraform/templates/locals.tf" > "${git_dir}/tmp/locals.tf"
+  cp "${git_dir}/terraform/templates/providers.tf" "${git_dir}/tmp/providers.tf"
+  cp "${git_dir}/terraform/templates/secrets.tf" "${git_dir}/tmp/secrets.tf"
+  cp "${git_dir}/terraform/templates/versions.tf" "${git_dir}/tmp/versions.tf"
+  if [ `uname` = "Linux" ]
+  then
+    sed -i "s/environments\//environments\/members\//g" "${git_dir}/tmp/backend.tf"
+  else
+    # This must be a Mac
+    sed -i '' "s/environments\//environments\/members\//g" "${git_dir}/tmp/backend.tf"
+  fi
+
+ 
+  # Loop through each environment for specific application to check if state file exists in S3
+  for ENV in `cat "${JSON_FILE}" | jq -r --arg FILENAME "${APPLICATION}" '.environments[].name'`
+  do
+    # Check if state file exists in S3 for modernisation-platform repository
+      aws s3api head-object --bucket modernisation-platform-terraform-state --key "environments/accounts/${APPLICATION}/${APPLICATION}-${ENV}/terraform.tfstate" > /dev/null 2>&1 
+      RETURN_CODE_CORE_REPO="${?}"
+     # Check if state file exists in S3 for modernisation-platform-environments repository
+      aws s3api head-object --bucket modernisation-platform-terraform-state --key "environments/members/${APPLICATION}/${APPLICATION}-${ENV}/terraform.tfstate" > /dev/null 2>&1 
+      RETURN_CODE_MEMBER_REPO="${?}"
+    
+    TERRAFORM_PATH="${git_dir}/tmp"
+
+    # Creating MEMBER account state file for modernisation-platform if it does not exist
+    if [[ "${RETURN_CODE_CORE_REPO}" -ne 0 ]]
+    then
+      echo -en "MEMBER ACCOUNT IN CORE REPO      - ${APPLICATION}-${ENV} - ${YELLOW}CREATING${NORMAL}\n"
+      terraform -chdir="${TERRAFORM_PATH}" init > /dev/null
+      terraform -chdir="${TERRAFORM_PATH}" workspace new "${APPLICATION}-${ENV}"
+    else
+      echo -en "MEMBER ACCOUNT IN CORE REPO      - ${APPLICATION}-${ENV} - ${GREEN}EXISTS${NORMAL}\n"
+    fi
+
+    # Creating MEMBER account state file for modernisation-platform-environments if it does not exist
+    ACCOUNT_TYPE=$(jq -r '."account-type"' ${JSON_FILE})
+    if [[ "${RETURN_CODE_MEMBER_REPO}" -ne 0 && "${ACCOUNT_TYPE}" != "core" ]]
+    then
+      echo -en "MEMBER ACCOUNT IN ENVIRONMENTS REPO - ${APPLICATION}-${ENV} - ${YELLOW}CREATING${NORMAL}\n"
+      terraform -chdir="${TERRAFORM_PATH}" init > /dev/null
+      terraform -chdir="${TERRAFORM_PATH}" workspace new "${APPLICATION}-${ENV}"
+    else
+      [[ ${ACCOUNT_TYPE} != "core" ]] && RESPONSE_TEXT="EXISTS" || RESPONSE_TEXT="CORE ACCOUNT - NOT REQUIRED IN MEMBER REPO"
+      echo -en "MEMBER ACCOUNT IN ENVIRONMENTS REPO - ${APPLICATION}-${ENV} - ${GREEN}${RESPONSE_TEXT}${NORMAL}\n"
+    fi
+  done
+done
+}
+
 main() {
+# set friendly Parameter names
+REQUEST_VALUE="${1}"  # this value can equal (bootstrap, all-environments or an application name)
+
 # Set root path to repository
 git_dir="$( git rev-parse --show-toplevel )"
 
 # Determine workspace build type
-case "${1}" in
+case "${REQUEST_VALUE}" in
 all-environments)
-  iterate_environments "" "${1}" "*"
+  iterate_environments_member "*"
   ;;
 bootstrap)
-  iterate_environments "bootstrap/delegate-access" "${1}" "*"
-  iterate_environments "bootstrap/secure-baselines" "${1}" "*"
-  iterate_environments "bootstrap/single-sign-on" "${1}" "*"
+  iterate_environments_bootstrap "delegate-access"
+  iterate_environments_bootstrap "secure-baselines"
+  iterate_environments_bootstrap "single-sign-on"
   ;;
 *)
-  # Check if folder exists for application
-  if [ -f ${git_dir}/environments/${1}.json ]
+  # This must be an individual application, check if json file exists for it
+  if [ -f ${git_dir}/environments/${REQUEST_VALUE}.json ]
   then
-    iterate_environments "" "${1}" "${1}"
+    iterate_environments_member "${REQUEST_VALUE}"
   else
     echo "ERROR: Incorrect Parameter received"
     exit 1
@@ -100,4 +146,13 @@ bootstrap)
 esac
 }
 
-main "${1}"
+# set friendly Parameter names
+REQUEST_VALUE="${1}"
+
+# setup colours for output
+NORMAL="\001\033[0;0m\002"
+YELLOW="\001\033[1;33m\002"
+GREEN="\001\033[1;32m\002"
+
+# call main function
+main "${REQUEST_VALUE}"
