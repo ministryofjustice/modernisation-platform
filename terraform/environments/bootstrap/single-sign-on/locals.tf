101c9a9b37bc83a7ecc83d7378edc54b53e61313
user01 <user01@pop-os.localdomain>
2021-05-11 07:00:56 +0100

    added multiple git_slug options to developer group

--- terraform/environments/bootstrap/single-sign-on/locals.tf
+++ terraform/environments/bootstrap/single-sign-on/locals.tf
@@ -1,6 +1,8 @@
 locals {
 
-  app_name = replace("${terraform.workspace}", "/-([[:alnum:]]+)$/", "")
+  app_name = try(regex("^bichard*.", "${terraform.workspace}"), replace("${terraform.workspace}", "/-([[:alnum:]]+)$/", ""))
+
+  #app_name = replace("${terraform.workspace}", "/-([[:alnum:]]+)$/", "")
   env_name = replace("${terraform.workspace}", "${local.app_name}-", "")
 
   environment_management = jsondecode(data.aws_secretsmanager_secret_version.environment_management.secret_string)
@@ -9,9 +11,8 @@ locals {
 
   sso_data = {  for data in local.defname.environments:
             
-                data.name => data.access[0]
+                data.name => data.access
   
                 if (data.name == local.env_name) 
              }
-  
 }
--- terraform/environments/bootstrap/single-sign-on/main.tf
+++ terraform/environments/bootstrap/single-sign-on/main.tf
@@ -7,6 +7,7 @@ data "aws_ssoadmin_instances" "default" {
 locals {
   sso_instance_arn      = coalesce(data.aws_ssoadmin_instances.default.arns...)
   sso_identity_store_id = coalesce(data.aws_ssoadmin_instances.default.identity_store_ids...)
+
 }
 
 # Get AWS SSO permission sets
@@ -17,7 +18,7 @@ data "aws_ssoadmin_permission_set" "administrator" {
   name         = "AdministratorAccess"
 }
 
-data "aws_ssoadmin_permission_set" "read-only" {
+data "aws_ssoadmin_permission_set" "view-only" {
   provider = aws.sso-management
 
   instance_arn = local.sso_instance_arn
@@ -28,8 +29,6 @@ data "aws_ssoadmin_permission_set" "read-only" {
 data "aws_identitystore_group" "platform_admin" {
   provider = aws.sso-management
 
-  #for_each = toset(local.current-environment-definition[*].github_slug)
-
   identity_store_id = local.sso_identity_store_id
 
   filter {
@@ -38,10 +37,11 @@ data "aws_identitystore_group" "platform_admin" {
   }
 }
 
+
 # Get Identity Store groups
 data "aws_identitystore_group" "developer" {
 
-  count = local.sso_data[local.env_name].github_slug != "" ? 1 : 0 
+  for_each = toset(local.sso_data[local.env_name][*].github_slug)
 
   provider = aws.sso-management
 
@@ -49,10 +49,9 @@ data "aws_identitystore_group" "developer" {
 
   filter {
     attribute_path  = "DisplayName"
-    attribute_value = try(local.sso_data[local.env_name].github_slug, null)
+    attribute_value = try(each.value, null)
   }
 }
-
 
 resource "aws_ssoadmin_account_assignment" "platform_admin" {
 
@@ -71,14 +70,46 @@ resource "aws_ssoadmin_account_assignment" "platform_admin" {
 
 resource "aws_ssoadmin_account_assignment" "developer" {
 
-  count = local.sso_data[local.env_name].github_slug != "" ? 1 : 0 
+  for_each = {
+
+    for sso_assignment in local.sso_data[local.env_name][*]:
+
+    "${sso_assignment.github_slug}-${sso_assignment.level}" => sso_assignment
+
+    if( "${sso_assignment.level}" == "development" )
+   }
 
   provider = aws.sso-management
 
   instance_arn       = local.sso_instance_arn
-  permission_set_arn = local.sso_data[local.env_name].level == "developer" ? data.aws_ssoadmin_permission_set.read-only.arn : data.aws_ssoadmin_permission_set.read-only.arn
+  permission_set_arn = data.aws_ssoadmin_permission_set.view-only.arn
 
-  principal_id   = data.aws_identitystore_group.developer[0].group_id
+  principal_id   = data.aws_identitystore_group.developer[each.value.github_slug].group_id
+  principal_type = "GROUP"
+
+  target_id   = local.environment_management.account_ids[terraform.workspace]
+  target_type = "AWS_ACCOUNT"
+}
+
+
+# This is only used for legacy bichard 
+resource "aws_ssoadmin_account_assignment" "administator" {
+
+  for_each = {
+
+    for sso_assignment in local.sso_data[local.env_name][*]:
+
+    "${sso_assignment.github_slug}-${sso_assignment.level}" => sso_assignment
+
+    if( "${sso_assignment.level}" == "administrator" )
+   }
+
+  provider = aws.sso-management
+
+  instance_arn       = local.sso_instance_arn
+  permission_set_arn = data.aws_ssoadmin_permission_set.administrator.arn
+
+  principal_id   = data.aws_identitystore_group.developer[each.value.github_slug].group_id
   principal_type = "GROUP"
 
   target_id   = local.environment_management.account_ids[terraform.workspace]
