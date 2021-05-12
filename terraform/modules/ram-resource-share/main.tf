4f0af3b1d5411d33bf7fbc38679a9a24b885a663
user01 <user01@pop-os.localdomain>
2021-05-12 12:36:52 +0100

    added adepend_on to the aws ram resource block

--- terraform/modules/ram-resource-share/main.tf
+++ terraform/modules/ram-resource-share/main.tf
@@ -1,4 +1,5 @@
 resource "aws_ram_resource_share" "default" {
+
   name                      = "${var.tags_prefix}-resource-share"
   allow_external_principals = false
   tags = merge(
@@ -10,7 +11,10 @@ resource "aws_ram_resource_share" "default" {
 }
 
 resource "aws_ram_resource_association" "default" {
+
   for_each = toset(var.resource_arns)
+
+  depends_on = [module.vpc]
 
   resource_arn       = each.value
   resource_share_arn = aws_ram_resource_share.default.arn
