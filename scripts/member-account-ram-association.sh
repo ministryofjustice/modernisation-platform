ac458b4792cdfd85d8c3bdba02d45416a0fe0f5b
user01 <user01@pop-os.localdomain>
2021-05-12 22:18:47 +0100

    updated git diff script to run from environmentnetworks

--- scripts/member-account-ram-association.sh
+++ scripts/member-account-ram-association.sh
@@ -8,24 +8,24 @@ setup_ram_share_association() {
 
     #Runs a Terraform plan/apply in the member-vpc workspace to setup the RAM association
 
-    echo "Running terraform across workspace $account-$environment"
+    echo "Running terraform across workspace $account"
 
     # Select workspace
-    select_workspace=`terraform -chdir=$basedir/$account workspace select $account-$environment`
+    select_workspace=`terraform -chdir=$basedir/$account workspace select $account`
 
     if [[ $select_workspace ]]; then
 
        echo "Terraform init"
       ./scripts/terraform-init.sh "$basedir/$account"
 
-        # Run terraform plan
-      echo "Terraform plan"
-      ./scripts/terraform-plan.sh "$basedir/$account"
+      #   # Run terraform plan
+      # echo "Terraform plan"
+      # ./scripts/terraform-plan.sh "$basedir/$account"
 
       # Run terraform apply
       ./scripts/terraform-apply.sh $basedir/$account
     fi
-    echo "Finished running terraform across new account: $account-$environment"
+    echo "Finished running ram share association for account: $account"
 }
 
 setup_ram_share_association
