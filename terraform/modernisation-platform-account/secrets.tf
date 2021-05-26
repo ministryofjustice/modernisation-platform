0857a519753bf6705dbfa5e8c7dd59fa8275fd37
David Elliott <david.elliott@digital.justice.gov.uk>
2021-05-26 11:26:10 +0100

    Add member ci user to mod plat account
    We don't want to use the main ci user with access to everything for the
    member ci workflows, this user will use account focussed or restricted
    access roles.
    
    Resolves #755

--- terraform/modernisation-platform-account/secrets.tf
+++ terraform/modernisation-platform-account/secrets.tf
@@ -1,3 +1,4 @@
+# Core CI User
 resource "aws_secretsmanager_secret" "ci_iam_user_keys" {
   name        = "ci_iam_user_keys"
   description = "Access keys for the CI user, this secret is used by GitHub to set the correct repository secrets."
@@ -9,5 +10,20 @@ resource "aws_secretsmanager_secret_version" "ci_iam_user_keys" {
   secret_string = jsonencode({
     AWS_ACCESS_KEY_ID     = aws_iam_access_key.ci.id
     AWS_SECRET_ACCESS_KEY = aws_iam_access_key.ci.secret
+  })
+}
+
+# Member CI user
+resource "aws_secretsmanager_secret" "member_ci_iam_user_keys" {
+  name        = "member_ci_iam_user_keys"
+  description = "Access keys for the CI user, this secret is used by GitHub to set the correct repository secrets."
+  tags        = local.tags
+}
+
+resource "aws_secretsmanager_secret_version" "member_ci_iam_user_keys" {
+  secret_id = aws_secretsmanager_secret.member_ci_iam_user_keys.id
+  secret_string = jsonencode({
+    AWS_ACCESS_KEY_ID     = aws_iam_access_key.member-ci.id
+    AWS_SECRET_ACCESS_KEY = aws_iam_access_key.member-ci.secret
   })
 }
