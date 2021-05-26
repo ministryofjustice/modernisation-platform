706066bbe6e83fc87e2dbaab266118bd536d496e
David Elliott <david.elliott@digital.justice.gov.uk>
2021-05-26 11:38:50 +0100

    Add member ci user to environments repo
    Adding the new user and secrets to the env repo.

--- terraform/github/aws.tf
+++ terraform/github/aws.tf
@@ -1,4 +1,4 @@
-# This gets the AWS access keys for CI/CD from AWS Secrets Manager to set as repository secrets.
+# This gets the AWS access keys for Core CI/CD from AWS Secrets Manager to set as repository secrets.
 data "aws_secretsmanager_secret" "ci_iam_user_keys" {
   name = "ci_iam_user_keys"
 }
@@ -7,6 +7,15 @@ data "aws_secretsmanager_secret_version" "ci_iam_user_keys" {
   secret_id = data.aws_secretsmanager_secret.ci_iam_user_keys.id
 }
 
+# This gets the AWS access keys for Member CI/CD from AWS Secrets Manager to set as repository secrets.
+data "aws_secretsmanager_secret" "member_ci_iam_user_keys" {
+  name = "member_ci_iam_user_keys"
+}
+
+data "aws_secretsmanager_secret_version" "member_ci_iam_user_keys" {
+  secret_id = data.aws_secretsmanager_secret.member_ci_iam_user_keys.id
+}
 locals {
-  ci_iam_user_keys = jsondecode(data.aws_secretsmanager_secret_version.ci_iam_user_keys.secret_string)
+  ci_iam_user_keys        = jsondecode(data.aws_secretsmanager_secret_version.ci_iam_user_keys.secret_string)
+  member_ci_iam_user_keys = jsondecode(data.aws_secretsmanager_secret_version.member_ci_iam_user_keys.secret_string)
 }
