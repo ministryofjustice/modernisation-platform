9ebd6f9b14994984f71c581142a7c70050ed2c92
David Elliott <david.elliott@digital.justice.gov.uk>
2021-06-02 15:44:49 +0100

    Add network-services provider
    We need a provider for network services to allow DNS validation records
    to be created and destroyed.

--- terraform/templates/member-providers.tf
+++ terraform/templates/member-providers.tf
@@ -21,3 +21,13 @@ provider "aws" {
     role_arn = "arn:aws:iam::${local.environment_management.account_ids[local.provider_name]}:role/dns-${local.vpc_name}-${local.environment}"
   }
 }
+
+# AWS provider for network services to enable dns entries for certificate validation to be created
+provider "aws" {
+  alias  = "core-network-services"
+  region = "eu-west-2"
+
+  assume_role {
+    role_arn = "arn:aws:iam::${local.environment_management.account_ids["core-network-services-production"]}:role/modify-dns-records"
+  }
+}
