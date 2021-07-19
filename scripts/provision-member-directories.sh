8b12e5068c34dcb26b2e5e96a28fb4db801b17ee
David Elliott <david.elliott@digital.justice.gov.uk>
2021-07-19 10:30:28 +0100

    Fix environments repo url not substituted
    Closes #922

--- scripts/provision-member-directories.sh
+++ scripts/provision-member-directories.sh
@@ -104,10 +104,18 @@ copy_templates() {
         if [ `uname` = "Linux" ]
         then
           sed -i "s/environments\//environments\/members\//g" "$1/$filename"
-          sed -i "s/github\.com\/ministryofjustice\/modernisation-platform/github\.com\/ministryofjustice\/modernisation-platform-environments/g" "$1/$filename"
         else
           # This must be a Mac
           sed -i '' "s/environments\//environments\/members\//g" "$1/$filename"
+        fi
+      fi
+      if [ ${filename} == "locals.tf" ]
+      then
+        if [ `uname` = "Linux" ]
+        then
+          sed -i "s/github\.com\/ministryofjustice\/modernisation-platform/github\.com\/ministryofjustice\/modernisation-platform-environments/g" "$1/$filename"
+        else
+          # This must be a Mac
           sed -i '' "s/github\.com\/ministryofjustice\/modernisation-platform/github\.com\/ministryofjustice\/modernisation-platform-environments/g" "$1/$filename"
         fi
       fi
