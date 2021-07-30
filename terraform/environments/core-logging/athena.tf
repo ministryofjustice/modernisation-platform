a248a78aa3a710a3b1413c29dfce63ed5b09ba60
philiphorrocks <e$zAmp3r>
2021-07-30 22:46:23 +0100

    added lambda to automate the Athena creation for Clouldtrail logs

---
+++ terraform/environments/core-logging/athena.tf
@@ -0,0 +1,193 @@
+
+data "aws_iam_policy_document" "lambda_assume_role_policy" {
+
+  provider = aws.modernisation-platform
+
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:sts::${data.aws_caller_identity.current.account_id}:assumed-role/athena_lambda/athena_table_update"]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda_secretsmanager_access" {
+
+  provider = aws.modernisation-platform
+  
+  name = "lambda_secretsmanager_access"
+
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
+
+  inline_policy {
+    name = "lambda_secretsmanager_access_policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = [
+              "secretsmanager:GetSecretValue",
+              "secretsmanager:DescribeSecret"
+          ]
+          Effect   = "Allow"
+          Resource = [
+
+            "arn:aws:secretsmanager:eu-west-2:946070829339:secret:environment_management-BLRCDb",
+            "arn:aws:secretsmanager:eu-west-2:946070829339:secret:environment_management"
+          ]
+        },
+      ]
+    })
+  }
+}
+
+resource "aws_s3_bucket" "athena_query" {
+  bucket = "athena-cloudtrail-query"
+  acl    = "private"
+
+  tags = {
+    Name        = "athena-cloudtrail-query"
+    Environment = "Production"
+  }
+}
+
+resource "aws_athena_workgroup" "mod-platform" {
+  name = "mod-platform"
+
+  configuration {
+    enforce_workgroup_configuration    = true
+    publish_cloudwatch_metrics_enabled = true
+
+    result_configuration {
+      output_location = "s3://${aws_s3_bucket.athena_query.bucket}/"
+
+  }
+ }
+}
+
+resource "aws_s3_bucket_policy" "athena_query_policy" {
+  bucket = aws_s3_bucket.athena_query.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression's result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "athenas3querypolicy"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal =  {
+        "AWS": "*"
+      },
+        Action    = "s3:*"
+        Resource = [
+          aws_s3_bucket.athena_query.arn,
+          "${aws_s3_bucket.athena_query.arn}/*",
+        ]
+
+      },
+    ]
+  })
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_block_public_access" {
+  
+  depends_on = [
+    aws_s3_bucket_policy.athena_query_policy
+  ]
+
+  bucket = aws_s3_bucket.athena_query.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
+
+
+data "aws_iam_policy_document" "athena_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+resource "aws_iam_role" "athena_lambda" {
+  
+  name = "athena_lambda"
+
+  assume_role_policy = data.aws_iam_policy_document.athena_assume_role_policy.json
+
+  inline_policy {
+    name = "athena_lambda_policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = [
+                "glue:GetDatabase",
+                "sts:AssumeRole",
+                "glue:CreateTable",
+                "glue:CreateDatabase",
+                "glue:CreateSchema",
+                "glue:CreatePartition",
+                "athena:*",
+                "glue:GetDatabases"]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
+}
+
+data "archive_file" "lambda_zip" {
+
+  depends_on = [module.s3-bucket-cloudtrail]
+
+  type          = "zip"
+  source_file = "lambda/index.py"
+  output_path   = "lambda/lambda_function.zip"
+}
+
+resource "aws_lambda_function" "athena_table_update" {
+
+  depends_on = [
+    aws_s3_bucket.athena_query,
+   
+  ]
+
+  filename      = "lambda/lambda_function.zip"
+  function_name = "athena_table_update"
+  role          = aws_iam_role.athena_lambda.arn
+  handler       = "index.lambda_handler"
+  source_code_hash = data.archive_file.lambda_zip.output_path
+  runtime = "python3.7"
+}
+
+resource "aws_cloudwatch_event_rule" "every_day" {
+    name = "every-five-minutes"
+    description = "Fires every five minutes"
+    schedule_expression = "cron(15 12 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "trigger_lamber_every_day" {
+    rule = "${aws_cloudwatch_event_rule.every_day.name}"
+    target_id = "athena_table_update"
+    arn = "${aws_lambda_function.athena_table_update.arn}"
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda" {
+    statement_id = "AllowExecutionFromCloudWatch"
+    action = "lambda:InvokeFunction"
+    function_name = "${aws_lambda_function.athena_table_update.function_name}"
+    principal = "events.amazonaws.com"
+    source_arn = "${aws_cloudwatch_event_rule.every_day.arn}"
+}
---
+++ terraform/environments/core-logging/lambda/index.py
@@ -0,0 +1,229 @@
+﻿import boto3
+import base64
+from botocore.exceptions import ClientError
+import json
+import time
+
+athena_client = boto3.client('athena', region_name='eu-west-2')
+sts_client = boto3.client('sts')
+query_location='s3://athena-cloudtrail-query'
+athena_db="mod_cloudtrail_logs"
+
+#Gets secret from modernation platform account
+def get_accounts():
+
+        # Call the assume_role method of the STSConnection object and pass the role
+        # ARN and a role session name.
+        assumed_role_object=sts_client.assume_role(
+            RoleArn="arn:aws:iam::946070829339:role/lambda_secretsmanager_access",
+            RoleSessionName="ListModPlatformAccounts"
+        )
+    
+        # From the response that contains the assumed role, get the temporary 
+        # credentials that can be used to make subsequent API calls
+        credentials=assumed_role_object['Credentials']  
+    
+        secret_name = "environment_management"
+        region_name = "eu-west-2"
+    
+        client = boto3.client(
+            service_name='secretsmanager',
+            region_name=region_name,
+            aws_access_key_id=credentials['AccessKeyId'],
+            aws_secret_access_key=credentials['SecretAccessKey'],
+            aws_session_token=credentials['SessionToken'],
+        )
+        
+        try:
+            get_secret_value_response = client.get_secret_value(
+                    SecretId=secret_name
+            )
+            res=json.loads(json.dumps(get_secret_value_response, default=str))
+            
+            return res
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'DecryptionFailureException':
+                # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+                # Deal with the exception here, and/or rethrow at your discretion.
+                raise e
+            elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+                # An error occurred on the server side.
+                # Deal with the exception here, and/or rethrow at your discretion.
+                raise e
+            elif e.response['Error']['Code'] == 'InvalidParameterException':
+                # You provided an invalid value for a parameter.
+                # Deal with the exception here, and/or rethrow at your discretion.
+                raise e
+            elif e.response['Error']['Code'] == 'InvalidRequestException':
+                # You provided a parameter value that is not valid for the current state of the resource.
+                # Deal with the exception here, and/or rethrow at your discretion.
+                raise e
+            elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+                # We can't find the resource that you asked for.
+                # Deal with the exception here, and/or rethrow at your discretion.
+                raise e
+        else:
+            # Decrypts secret using the associated KMS CMK.
+            # Depending on whether the secret is a string or binary, one of these fields will be populated.
+            if 'SecretString' in get_secret_value_response:
+                secret = get_secret_value_response['SecretString']
+                #res = json.loads(json.dumps(secret, default=str))
+                return secret
+            else:
+                decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+                #res = json.loads(json.dumps(decoded_binary_secret, default=str))
+                return decoded_binary_secret
+                
+
+def create_athena_database():
+    
+    print("[+] Creating Athena database" + athena_db)
+    
+    try:
+        response = athena_client.start_query_execution(
+           
+        QueryString='CREATE DATABASE IF NOT EXISTS %s' % athena_db,
+        
+        ResultConfiguration={'OutputLocation': query_location})
+        
+    except ClientError as e:
+        print("Unexpected error: %s" % e)     
+
+
+def drop_athena_table():
+    
+    print("[+] Dropping Athena table")
+    
+    try:
+        response = athena_client.start_query_execution(
+           
+        QueryString='DROP DATABASE %s'  % athena_db,
+        
+         QueryExecutionContext={
+                'Database': athena_db
+         },
+         
+        ResultConfiguration={'OutputLocation': query_location})
+        
+    except ClientError as e:
+        
+        print("[!] Could not drop Athena table")
+        print("Unexpected error: %s" % e)     
+        
+        
+        
+def create_athena_table(list_accounts):
+
+    print("[+] Updating Athena table")
+    
+    print("[+] Adding Mod accounts to Athena query: ")
+    print(list_accounts)
+        
+    query = """
+            CREATE EXTERNAL TABLE cloudtrail_logs(
+                eventVersion STRING,
+                userIdentity STRUCT<
+                    type: STRING,
+                    principalId: STRING,
+                    arn: STRING,
+                    accountId: STRING,
+                    invokedBy: STRING,
+                    accessKeyId: STRING,
+                    userName: STRING,
+                    sessionContext: STRUCT<
+                        attributes: STRUCT<
+                            mfaAuthenticated: STRING,
+                            creationDate: STRING>,
+                        sessionIssuer: STRUCT<
+                            type: STRING,
+                            principalId: STRING,
+                            arn: STRING,
+                            accountId: STRING,
+                            userName: STRING>>>,
+                eventTime STRING,
+                eventSource STRING,
+                eventName STRING,
+                awsRegion STRING,
+                sourceIpAddress STRING,
+                userAgent STRING,
+                errorCode STRING,
+                errorMessage STRING,
+                requestParameters STRING,
+                responseElements STRING,
+                additionalEventData STRING,
+                requestId STRING,
+                eventId STRING,
+                readOnly STRING,
+                resources ARRAY<STRUCT<
+                    arn: STRING,
+                    accountId: STRING,
+                    type: STRING>>,
+                eventType STRING,
+                apiVersion STRING,
+                recipientAccountId STRING,
+                serviceEventDetails STRING,
+                sharedEventID STRING,
+                vpcEndpointId STRING
+              )
+            PARTITIONED BY (
+               `account` string, `region` string, `timestamp` string)
+            ROW FORMAT SERDE 'com.amazon.emr.hive.serde.CloudTrailSerde'
+            STORED AS INPUTFORMAT 'com.amazon.emr.cloudtrail.CloudTrailInputFormat'
+            OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+            LOCATION
+              's3://modernisation-platform-logs-cloudtrail/AWSLogs'
+            TBLPROPERTIES (
+              'projection.enabled'='true',
+              'projection.account.type' = 'enum',
+              'projection.account.values' = '%s',
+              'projection.region.type' = 'enum',
+              'projection.region.values' = 'eu-west-1, eu-west-2',
+              'projection.timestamp.format'='yyyy/MM/dd',
+              'projection.timestamp.interval'='1',
+              'projection.timestamp.interval.unit'='DAYS',
+              'projection.timestamp.range'='2020/01/01,NOW',
+              'projection.timestamp.type'='date',
+              'storage.location.template'='s3://modernisation-platform-logs-cloudtrail/AWSLogs/${account}/CloudTrail/${region}/${timestamp}')
+      """ % list_accounts
+    try:
+        response = athena_client.start_query_execution(
+            QueryString=query,
+            
+            QueryExecutionContext={
+                'Database': athena_db
+         },
+            ResultConfiguration={
+                'OutputLocation': query_location,
+            }
+        )
+        print(response)
+        return response
+    
+    except ClientError as e:
+        print("[-] Could not create Athena table") 
+        print("Unexpected error: %s" % e)     
+    
+ 
+
+def lambda_handler(event, context):
+     
+    # #Get list of accounts 
+    list_accounts = []
+    
+    account = get_accounts()
+    
+    data_object = json.loads(account['SecretString'])
+    data_json =  json.dumps(data_object['account_ids'])
+    
+    #Get values for JSON 
+    for key, value in json.loads(data_json).items():
+        list_accounts.append(value)
+    
+    #creat athena database if it doesnt exist
+    create_athena_database()
+    
+    drop_athena_table()
+    
+    #update Athena table 
+    create_athena_table(','.join(map(str, list_accounts)))
+    
