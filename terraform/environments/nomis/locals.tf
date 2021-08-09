dae706baf773583302adfb29af00d7fddbd21994
Fasih Rehman <fasih.rehman@hmcts.net>
2021-08-09 16:19:19 +0100

    Fix nomis env

--- terraform/environments/nomis/locals.tf
+++ terraform/environments/nomis/locals.tf
@@ -38,5 +38,5 @@ locals {
   # environment specfic variables
   # example usage:  
   # example_data = local.application_data.accounts[local.environment].example_var
-  application_data = jsondecode(file("./application_variables.json"))
+  # application_data = jsondecode(file("./application_variables.json"))
 }
