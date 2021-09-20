ab16bc234975a97d4c0e2eddd8199a6ee1bbf49e
Seb Norris <sebastian@automationlogic.com>
2021-09-20 15:05:14 +0100

    working terraform plan in core-sandbox

--- terraform/environments/core-sandbox/playground.tf
+++ terraform/environments/core-sandbox/playground.tf
@@ -2,17 +2,19 @@ data "aws_caller_identity" "current" {}
 
 module "ram-principal-association" {
 
+  count = (var.networking[0].set == "") ? 0 : 1
+
   source = "../../modules/ram-principal-association"
 
   providers = {
-    aws.share-acm    = aws.core-network-services-production
-    aws.share-host   = aws.core-vpc-production # Core VPC production holds the share
-    aws.share-tenant = aws                     # The default provider (unaliased, `aws`) is the tenant
+    aws.share-acm    = aws.core-network-services
+    aws.share-host   = aws.core-vpc
+    aws.share-tenant = aws
   }
-
-  principal  = data.aws_caller_identity.current.account_id
-  vpc_name   = local.json_data.networking[0].business-unit
-  subnet_set = local.json_data.networking[0].set
-  acm_pca    = local.acm_pca[0]
+  principal   = data.aws_caller_identity.current.account_id
+  vpc_name    = var.networking[0].business-unit
+  subnet_set  = var.networking[0].set
+  acm_pca     = "acm-pca-${local.is_live[0]}"
+  environment = local.environment
 
 }
