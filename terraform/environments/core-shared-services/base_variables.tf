f6fb6c4e1240e128621b0a17dace292f705eff12
Fasih Rehman <fasih.rehman@digital.justice.gov.uk>
2021-09-23 15:59:20 +0100

    Added new internal ecr repo module for shared services.
    First implementation for performance-hub.

--- terraform/environments/core-shared-services/base_variables.tf
+++ terraform/environments/core-shared-services/base_variables.tf
@@ -3,3 +3,14 @@ variable "networking" {
   type = list(any)
 
 }
+
+variable "app_name" {
+  type    = string
+  default = "performance-hub"
+}
+
+variable "account_arns" {
+  description = "list of account arns"
+  type        = list(string)
+  default     = []
+}
