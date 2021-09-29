df0cd87131f74fc424a010b04bf435d99c56bdf0
adrianweetman <adrian.weetman@digital.justice.gov.uk>
2021-09-29 14:45:04 +0100

    Adding pre-req role and instance profile for image builder

--- terraform/environments/core-shared-services/iam.tf
+++ terraform/environments/core-shared-services/iam.tf
@@ -1,3 +1,35 @@
 data "aws_iam_role" "vpc-flow-log" {
   name = "AWSVPCFlowLog"
 }
+
+# Create IAM role and instance profile for image builder
+# Instance profile gets associated to the image builder Infrastructure Configuration resource
+data "aws_iam_policy" "image_builder_managed_policies" {
+  for_each = toset([
+    "EC2InstanceProfileForImageBuilder",
+    "AmazonSSMManagedInstanceCore"
+  ])
+  name = each.key
+}
+
+data "aws_iam_policy_document" "image_builder_assume_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "image_builder" {
+  name                = "image-builder"
+  assume_role_policy  = data.aws_iam_policy_document.image_builder_assume_policy.json
+  managed_policy_arns = [for k in data.aws_iam_policy.image_builder_managed_policies : k.arn]
+}
+
+resource "aws_iam_instance_profile" "image_builder" {
+  name = "image-builder"
+  role = aws_iam_role.image_builder.name
+}
