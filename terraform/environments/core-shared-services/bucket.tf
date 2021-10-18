12218fc1e03c49f9703ebae34915fa9aa65fc05d
Piotr Grzeskowiak <piotr.grzeskowiak@digital.justice.gov.uk>
2021-10-18 15:07:03 +0100

    added a shared bucket for image build logs

---
+++ terraform/environments/core-shared-services/bucket.tf
@@ -0,0 +1,17 @@
+module "imagebuilder_log_bucket" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v5.0.0"
+
+  providers = {
+    aws.bucket-replication = aws.bucket-replication
+  }
+
+  bucket_prefix       = "ec2-image-builder-logs-"
+  versioning_enabled  = false
+  replication_enabled = false
+
+  tags = local.tags
+}
+
+output "imagebuilder_log_bucket_id" {
+  value = module.imagebuilder_log_bucket.bucket.id
+}
