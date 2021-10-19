3713d65d2240ff2f6307f8748f878522f97758db
adeweetman-al <89138740+adeweetman-al@users.noreply.github.com>
2021-10-19 09:38:22 +0100

    Adding member policy and update guidance around member account deletion (#1187)
    Co-authored-by: adrianweetman <adrian.weetman@digital.justice.gov.uk>

--- terraform/environments/scp_mp_member.tf
+++ terraform/environments/scp_mp_member.tf
@@ -13,7 +13,35 @@ data "aws_iam_policy_document" "scp_mp_member_ou" {
   }
 }
 
+# Separate policy for preventing root access (so it can be detached separately if need be)
+data "aws_iam_policy_document" "scp_mp_member_ou_prevent_root" {
+  version = "2012-10-17"
+
+  statement {
+    effect    = "Deny"
+    actions   = ["*"]
+    resources = ["*"]
+    condition {
+      test     = "ForAnyValue:StringLike"
+      variable = "aws:PrincipalOrgPaths"
+      values   = ["${data.aws_organizations_organization.root_account.id}/*/${local.modernisation_platform_ou_id}/*"]
+    }
+    # For testing this against a specific account, follow this method
+    # condition {
+    #   test     = "ForAnyValue:StringLike"
+    #   variable = "aws:PrincipalOrgPaths"
+    #   values   = ["${data.aws_organizations_organization.root_account.id}/*/${local.modernisation_platform_ou_id}/*/<member ou id>/*"] 
+    # } 
+    condition {
+      test     = "StringLike"
+      variable = "aws:PrincipalArn"
+      values   = ["arn:aws:iam::*:root"]
+    }
+  }
+}
+
 # This policy will only be applied to accounts within the "Modernisation Platform Member" ou
+# The default policy
 resource "aws_organizations_policy" "scp_mp_member_ou" {
   name        = "Modernisation Platform Member OU SCP"
   description = "Restricts permissions for all OUs and accounts under the Modernisation Platform Member OU"
@@ -27,8 +55,30 @@ resource "aws_organizations_policy" "scp_mp_member_ou" {
   }
 }
 
+# The policy preventing root access
+resource "aws_organizations_policy" "scp_mp_member_ou_prevent_root" {
+  name        = "Modernisation Platform Member OU SCP - Prevent Root"
+  description = "Prevents root user access for all OUs and accounts under the Modernisation Platform Member OU"
+  type        = "SERVICE_CONTROL_POLICY"
+  content     = data.aws_iam_policy_document.scp_mp_member_ou_prevent_root.json
+
+  tags = {
+    business-unit = "Platforms"
+    component     = "SERVICE_CONTROL_POLICY"
+    source-code   = local.github_repository
+  }
+}
+
+
 # Enrol all accounts within the Modernisation Platform Member OU (current and future) to the Modernisation Platform Member OU SCP
+# The default policy
 resource "aws_organizations_policy_attachment" "scp_mp_member_ou" {
   policy_id = aws_organizations_policy.scp_mp_member_ou.id
+  target_id = module.environments.modernisation_platform_member_ou_id
+}
+
+# The policy preventing root access
+resource "aws_organizations_policy_attachment" "scp_mp_member_ou_prevent_root" {
+  policy_id = aws_organizations_policy.scp_mp_member_ou_prevent_root.id
   target_id = module.environments.modernisation_platform_member_ou_id
 }
