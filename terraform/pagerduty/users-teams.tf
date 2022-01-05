4a5266b3ccb6a64607c912b34e9baa0afe24163c
David Sibley <david.sibley@digital.justice.gov.uk>
2022-01-05 14:17:37 +0000

    added explicit depends_on for relationship between users and platform membership

--- terraform/pagerduty/users-teams.tf
+++ terraform/pagerduty/users-teams.tf
@@ -7,6 +7,9 @@ resource "pagerduty_team_membership" "modernisation_platform_membership" {
   for_each = toset(local.modernisation_platform_users[*].id)
   team_id  = pagerduty_team.modernisation_platform.id
   user_id  = each.value
+  depends_on = [
+    pagerduty_user.pager_duty_users
+  ]
 }
 
 #users
