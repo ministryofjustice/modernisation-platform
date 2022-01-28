02fb762e688b671af85f31234a12a79586d18723
David Elliott <david.elliott@digital.justice.gov.uk>
2022-01-28 09:56:42 +0000

    Fix uneeded alarm regarding api calls
    Incidents like this - https://moj-digital-tools.pagerduty.com/incidents/Q38DC32KBA0XF3
    are caused due to terraform trying to check supported EC2 platforms when
    it doesn't have permission in these regions.  We don't need to do this
    for these regions so skipping this check to quieten down the alarms.
    
    https://registry.terraform.io/providers/hashicorp/aws/latest/docs
    
    Similar to https://github.com/ministryofjustice/modernisation-platform/pull/1329

--- terraform/modules/dns-zone/versions.tf
+++ terraform/modules/dns-zone/versions.tf
@@ -1,9 +1,9 @@
 terraform {
   required_providers {
     aws = {
-      version = ">= 3.47.0"
-      source  = "hashicorp/aws"
-      configuration_aliases = [ aws.core-network-services ]
+      version               = ">= 3.47.0"
+      source                = "hashicorp/aws"
+      configuration_aliases = [aws.core-network-services]
     }
   }
   required_version = ">= 1.0.1"
