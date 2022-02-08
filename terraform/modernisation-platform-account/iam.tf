e0fb4faf4848ed09bba904c536099b874cb8a1c2
dms1981 <david.sibley@digital.justice.gov.uk>
2022-02-08 08:42:19 +0000

    rebased from main and fixed reference (#1452)
    Updated reference to external ministry of justice module for IAM superadmins

--- terraform/modernisation-platform-account/iam.tf
+++ terraform/modernisation-platform-account/iam.tf
@@ -1,5 +1,5 @@
 module "iam" {
-  source        = "github.com/ministryofjustice/modernisation-platform-terraform-iam-superadmins?ref=v1.0.7"
+  source        = "github.com/ministryofjustice/modernisation-platform-terraform-iam-superadmins?ref=v1.0.8"
   account_alias = "moj-modernisation-platform"
 }
 
