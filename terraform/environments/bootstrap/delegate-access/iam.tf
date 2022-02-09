6ba4031a680093cb44d309601ec0aae8a80a0525
David Sibley <david.sibley@digital.justice.gov.uk>
2022-02-09 17:09:40 +0000

    removed lingering iam change

--- terraform/environments/bootstrap/delegate-access/iam.tf
+++ terraform/environments/bootstrap/delegate-access/iam.tf
@@ -90,7 +90,6 @@ data "aws_iam_policy_document" "member-access" {
       "iam:AddClientIDToOpenIDConnectProvider",
       "iam:AddUserToGroup",
       "iam:AttachGroupPolicy",
-      "iam:AttachRolePolicy",
       "iam:AttachUserPolicy",
       "iam:CreateAccountAlias",
       "iam:CreateGroup",
