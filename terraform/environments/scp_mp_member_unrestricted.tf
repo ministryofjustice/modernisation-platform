aba24e2a4a324baee87d92fe317e682974584d9b
David Elliott <david.elliott@digital.justice.gov.uk>
2022-02-22 14:14:11 +0000

    Remove MP OUs SCP prevent root access policy
    This is now implemented at the P&A level so inherited from therefore no
    longer required at this level. Updating the userguidance as well to
    reflect this.
    
    Closes #1324

--- terraform/environments/scp_mp_member_unrestricted.tf
+++ terraform/environments/scp_mp_member_unrestricted.tf
@@ -1,5 +1,5 @@
-#  Modernisation Platform Member Unrestriced ou SCP policy
-data "aws_iam_policy_document" "scp_mp_member_unrestriced_ou" {
+#  Modernisation Platform Member Unrestricted ou SCP policy
+data "aws_iam_policy_document" "scp_mp_member_unrestricted_ou" {
   #checkov:skip=CKV_AWS_1:Policy applied to a limited number of accounts
   #checkov:skip=CKV_AWS_49:Policy applied to a limited number of accounts
   #checkov:skip=CKV_AWS_107:Policy applied to a limited number of accounts
@@ -16,40 +16,13 @@ data "aws_iam_policy_document" "scp_mp_member_unrestriced_ou" {
   }
 }
 
-# Separate policy for preventing root access (so it can be detached separately if need be)
-data "aws_iam_policy_document" "scp_mp_member_unrestricted_ou_prevent_root" {
-  version = "2012-10-17"
-
-  statement {
-    effect    = "Deny"
-    actions   = ["*"]
-    resources = ["*"]
-    condition {
-      test     = "ForAnyValue:StringLike"
-      variable = "aws:PrincipalOrgPaths"
-      values   = ["${data.aws_organizations_organization.root_account.id}/*/${local.modernisation_platform_ou_id}/*"]
-    }
-    # For testing this against a specific account, follow this method
-    # condition {
-    #   test     = "ForAnyValue:StringLike"
-    #   variable = "aws:PrincipalOrgPaths"
-    #   values   = ["${data.aws_organizations_organization.root_account.id}/*/${local.modernisation_platform_ou_id}/*/<member ou id>/*"] 
-    # } 
-    condition {
-      test     = "StringLike"
-      variable = "aws:PrincipalArn"
-      values   = ["arn:aws:iam::*:root"]
-    }
-  }
-}
-
-# This policy will only be applied to accounts withing the "Modernisation Platform Member Unrestriced" ou
+# This policy will only be applied to accounts withing the "Modernisation Platform Member Unrestricted" ou
 # The default policy
-resource "aws_organizations_policy" "scp_mp_member_unrestriced_ou" {
-  name        = "Modernisation Platform Member Unrestriced OU SCP"
-  description = "Restricts permissions for all OUs and accounts under the Modernisation Platform Member Unrestriced OU"
+resource "aws_organizations_policy" "scp_mp_member_unrestricted_ou" {
+  name        = "Modernisation Platform Member unrestricted OU SCP"
+  description = "Restricts permissions for all OUs and accounts under the Modernisation Platform Member unrestricted OU"
   type        = "SERVICE_CONTROL_POLICY"
-  content     = data.aws_iam_policy_document.scp_mp_member_unrestriced_ou.json
+  content     = data.aws_iam_policy_document.scp_mp_member_unrestricted_ou.json
 
   tags = {
     business-unit = "Platforms"
@@ -58,30 +31,9 @@ resource "aws_organizations_policy" "scp_mp_member_unrestriced_ou" {
   }
 }
 
-# The policy preventing root access
-resource "aws_organizations_policy" "scp_mp_member_unrestricted_ou_prevent_root" {
-  name        = "Modernisation Platform Member Unrestricted OU SCP - Prevent Root"
-  description = "Prevents root user access for all OUs and accounts under the Modernisation Platform Unrestricted OU"
-  type        = "SERVICE_CONTROL_POLICY"
-  content     = data.aws_iam_policy_document.scp_mp_member_unrestricted_ou_prevent_root.json
-
-  tags = {
-    business-unit = "Platforms"
-    component     = "SERVICE_CONTROL_POLICY"
-    source-code   = local.github_repository
-  }
-}
-
-# Enrol all accounts within the Modernisation Platform Member Unrestriced OU (current and future) to the Modernisation Platform Member Unrestriced OU SCP
+# Enrol all accounts within the Modernisation Platform Member unrestricted OU (current and future) to the Modernisation Platform Member unrestricted OU SCP
 # The policy attachment for the default policy
-resource "aws_organizations_policy_attachment" "scp_mp_member_unrestriced_ou" {
-  policy_id = aws_organizations_policy.scp_mp_member_unrestriced_ou.id
-  target_id = module.environments.modernisation_platform_member_unrestricted_ou_id
-}
-
-# Enrol all accounts within the Modernisation Platform Member Unrestricted OU (current and future) to the Modernisation Platform Member Unrestricted OU SCP
-# The policy attachment for the policy preventing root access
-resource "aws_organizations_policy_attachment" "scp_mp_member_unrestricted_ou_prevent_root" {
-  policy_id = aws_organizations_policy.scp_mp_member_unrestricted_ou_prevent_root.id
+resource "aws_organizations_policy_attachment" "scp_mp_member_unrestricted_ou" {
+  policy_id = aws_organizations_policy.scp_mp_member_unrestricted_ou.id
   target_id = module.environments.modernisation_platform_member_unrestricted_ou_id
 }
