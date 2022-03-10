bab632a5805dcfee7117fdca739546d94f1d8d51
David Sibley <david.sibley@digital.justice.gov.uk>
2022-03-10 08:51:07 +0000

    updated reference to remote superadmins module, removed ezman from superadmins local

--- terraform/modernisation-platform-account/iam.tf
+++ terraform/modernisation-platform-account/iam.tf
@@ -1,5 +1,5 @@
 module "iam" {
-  source        = "github.com/ministryofjustice/modernisation-platform-terraform-iam-superadmins?ref=v1.0.8"
+  source        = "github.com/ministryofjustice/modernisation-platform-terraform-iam-superadmins?ref=v1.0.9"
   account_alias = "moj-modernisation-platform"
 }
 
