8a24faaf94754b48d53011cc4903063541ca6991
David Elliott <david.elliott@digital.justice.gov.uk>
2022-05-06 15:13:59 +0100

    Add high and low priority SNS alarm topics in core
    These topics will allow us to push alarms to the pagerduty high and low
    priority services.
    
    The code was repetative so drying it out by moving to a module.
    
    Added encryption to SNS topics.

--- terraform/modernisation-platform-account/monitoring.tf
+++ terraform/modernisation-platform-account/monitoring.tf
@@ -1,5 +1,4 @@
-module "pagerduty_core_alerts" {
-  source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=v1.0.0"
-  sns_topics                = ["config", "securityhub-alarms", "cloudtrail"]
-  pagerduty_integration_key = local.pagerduty_integration_keys["core_alerts_cloudwatch"]
+module "core_monitoring" {
+  source                     = "../modules/core-monitoring"
+  pagerduty_integration_keys = local.pagerduty_integration_keys
 }
---
+++ terraform/modules/core-monitoring/README.md
@@ -0,0 +1,6 @@
+# Core Monitoring
+
+Terraform module used to create monitoring topics and pagerduty subscriptions for core accounts.
+
+<!-- BEGIN_TF_DOCS -->
+<!-- END_TF_DOCS -->
