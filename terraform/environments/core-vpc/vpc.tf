22618fc511c95d93388922bfe2cfafd0250d4cc3
David Sibley <david.sibley@digital.justice.gov.uk>
2022-06-22 16:09:01 +0100

    update reference to VPC module tagged version

--- terraform/environments/core-vpc/vpc.tf
+++ terraform/environments/core-vpc/vpc.tf
@@ -117,7 +117,7 @@ locals {
 module "vpc" {
   for_each = local.vpcs[terraform.workspace]
 
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-member-vpc?ref=v1.0.2"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-member-vpc?ref=v1.0.3"
 
   subnet_sets = { for key, subnet in each.value.cidr.subnet_sets : key => subnet.cidr }
 
