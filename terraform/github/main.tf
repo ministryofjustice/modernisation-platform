6654f4feec9092e0120196b2c193a4371e690964
David Sibley <david.sibley@digital.justice.gov.uk>
2022-07-01 12:20:07 +0100

    added pagerduty_integration secrest and required checks

--- terraform/github/main.tf
+++ terraform/github/main.tf
@@ -248,6 +248,8 @@ module "modernisation-platform-terraform-pagerduty-integration" {
     "pagerduty",
     "alerting"
   ]
+  required_checks = ["Run Go Unit Tests"]
+  secrets         = nonsensitive(local.testing_ci_iam_user_keys)
 }
 
 # Everyone, with access to the above repositories
