5d2443f86e6f8ce3b97387bb6bcf4d8a27615fc5
David Sibley <david.sibley@digital.justice.gov.uk>
2022-07-04 16:42:13 +0100

    add data call for pagerduty key, allow access in IAM policy

--- terraform/environments/testing/iam.tf
+++ terraform/environments/testing/iam.tf
@@ -52,7 +52,8 @@ data "aws_iam_policy_document" "testing_ci_policy" {
     resources = [
       "${data.aws_kms_key.s3_state_bucket.arn}",
       "${data.aws_kms_key.dynamodb_state_lock.arn}",
-      "${data.aws_kms_key.environment_management.arn}"
+      "${data.aws_kms_key.environment_management.arn}",
+      "${data.aws_kms_key.pagerduty.arn}"
     ]
   }
 
