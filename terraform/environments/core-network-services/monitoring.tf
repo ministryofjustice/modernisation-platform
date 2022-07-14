02d58a5cff94d2f9c6d237cf1d991606883dad72
dms1981 <david.sibley@digital.justice.gov.uk>
2022-07-14 09:43:37 +0100

    corrected the treat_missing_data value to one the API will accept (#2005)

--- terraform/environments/core-network-services/monitoring.tf
+++ terraform/environments/core-network-services/monitoring.tf
@@ -115,7 +115,7 @@ resource "aws_cloudwatch_metric_alarm" "production_attachment_no_traffic_5_minut
   period             = "60"
   statistic          = "Sum"
   threshold          = "1"
-  treat_missing_data = "Breaching"
+  treat_missing_data = "breaching"
   tags               = local.tags
 }
 
