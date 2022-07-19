dff1df64c957bdc3fd589aa7ef0cc8c77377070c
dms1981 <david.sibley@digital.justice.gov.uk>
2022-07-19 11:03:43 +0100

    Fix/lambda path refs (#2029)
    * replaced local references with path values

--- terraform/modernisation-platform-account/lambda.tf
+++ terraform/modernisation-platform-account/lambda.tf
@@ -1,13 +1,13 @@
 data "archive_file" "instance_scheduler_zip" {
   type        = "zip"
-  source_dir  = "./lambda/golang-instance-scheduler"
-  output_path = "./lambda/files/golang_instance_scheduler.zip"
+  source_dir  = "${path.module}/lambda/golang-instance-scheduler"
+  output_path = "${path.module}/lambda/files/golang_instance_scheduler.zip"
 }
 
 resource "aws_lambda_function" "instance-scheduler" {
   #checkov:skip=CKV_AWS_116
   #checkov:skip=CKV_AWS_117
-  filename                       = "./lambda/files/golang-instance-scheduler.zip"
+  filename                       = "${path.module}/lambda/files/golang-instance-scheduler.zip"
   function_name                  = "golang-instance-scheduler"
   handler                        = "main"
   reserved_concurrent_executions = 0
