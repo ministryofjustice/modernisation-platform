019ea8cacfe5fe7c40a76ca476e521f6e6ef7052
David Sibley <david.sibley@digital.justice.gov.uk>
2022-08-08 13:57:56 +0100

    allow assumption of MemberInfrastructureAccess role by github-actions role

--- terraform/environments/bootstrap/delegate-access/iam.tf
+++ terraform/environments/bootstrap/delegate-access/iam.tf
@@ -188,6 +188,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
     principals {
       type = "AWS"
       identifiers = ["arn:aws:iam::${local.modernisation_platform_account.id}:root",
+        "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/github-actions",
         "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:user/testing-ci"
       ]
     }
