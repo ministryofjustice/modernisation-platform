ed942c9952a97c708da0f286ab606744ce615053
David Sibley <david.sibley@digital.justice.gov.uk>
2022-08-15 16:31:27 +0100

    switch underscore for hyphen

--- terraform/environments/bootstrap/delegate-access/locals.tf
+++ terraform/environments/bootstrap/delegate-access/locals.tf
@@ -11,7 +11,7 @@ locals {
   modernisation_platform_account = local.root_account.accounts[index(local.root_account.accounts[*].email, "aws+modernisation-platform@digital.justice.gov.uk")]
   environment_management         = jsondecode(data.aws_secretsmanager_secret_version.environment_management.secret_string)
   application_name               = try(regex("^bichard*.|^remote-supervisio*.", terraform.workspace), replace(terraform.workspace, "/-([[:alnum:]]+)$/", ""))
-  business_unit                  = jsondecode(data.http.environments_file.response_body).tags.business_unit
+  business_unit                  = jsondecode(data.http.environments_file.response_body).tags.business-unit
   application_environment        = substr(terraform.workspace, length(local.application_name)+1, -1)
 
   environments = {
