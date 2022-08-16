e94e17d0d74958c214c12363d505f2dc15e07304
dms1981 <david.sibley@digital.justice.gov.uk>
2022-08-16 11:06:13 +0100

    OIDC for all member accounts (#2149)
    * added OIDC into delegate-access, but still need an answer as to how to provider per-business-unit names for member-delegation roles
    
    * added tls provider as required by OIDC provider. Added locals to allow for string creation covering correct business unit and environment for member-delegation role
    
    * correctly reference tag for business_unit
    
    * switch underscore for hyphen
    
    * bump github-oidc module version, use output from module in member-access module to ensure order of operation
    
    * Commit changes made by code formatters
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- terraform/environments/bootstrap/delegate-access/locals.tf
+++ terraform/environments/bootstrap/delegate-access/locals.tf
@@ -2,10 +2,17 @@
 # (when we want to assume a role in the MP, for instance)
 data "aws_organizations_organization" "root_account" {}
 
+data "http" "environments_file" {
+  url = format("https://raw.githubusercontent.com/ministryofjustice/modernisation-platform/main/environments/%s.json", local.application_name)
+}
+
 locals {
   root_account                   = data.aws_organizations_organization.root_account
   modernisation_platform_account = local.root_account.accounts[index(local.root_account.accounts[*].email, "aws+modernisation-platform@digital.justice.gov.uk")]
   environment_management         = jsondecode(data.aws_secretsmanager_secret_version.environment_management.secret_string)
+  application_name               = try(regex("^bichard*.|^remote-supervisio*.", terraform.workspace), replace(terraform.workspace, "/-([[:alnum:]]+)$/", ""))
+  business_unit                  = jsondecode(data.http.environments_file.response_body).tags.business-unit
+  application_environment        = substr(terraform.workspace, length(local.application_name) + 1, -1)
 
   environments = {
     business-unit = "Platforms"
