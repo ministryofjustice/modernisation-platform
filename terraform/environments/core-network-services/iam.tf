1eebd1b38f3e81bca20242af80f6c4bdad8ff1f7
David Sibley <david.sibley@digital.justice.gov.uk>
2022-08-17 14:08:44 +0100

    add missing braces

--- terraform/environments/core-network-services/iam.tf
+++ terraform/environments/core-network-services/iam.tf
@@ -33,11 +33,14 @@ resource "aws_iam_role" "dns" {
           "Action" : "sts:AssumeRole",
           "Condition" : {
             "ForAnyValue:StringLike" : {
-              "aws:PrincipalOrgPaths" : ["${data.aws_organizations_organization.root_account.id}/*/${local.environment_management.modernisation_platform_organisation_unit_id}/*"]
+              "aws:PrincipalOrgPaths" : [
+                "${data.aws_organizations_organization.root_account.id}/*/${local.environment_management.modernisation_platform_organisation_unit_id}/*"
+              ]
             },
             "ForAnyValue:StringLike" : {
               "aws:PrincipalArn" : ["aws:arn:iam::*:role/github-actions"]
             }
+          }
         }
       ]
   })
