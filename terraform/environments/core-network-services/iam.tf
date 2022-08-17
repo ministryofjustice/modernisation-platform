db91f4d3648370f43faef6202fcff96525725909
David Sibley <david.sibley@digital.justice.gov.uk>
2022-08-17 13:57:05 +0100

    correctly populate IAM statement conditions

--- terraform/environments/core-network-services/iam.tf
+++ terraform/environments/core-network-services/iam.tf
@@ -34,13 +34,10 @@ resource "aws_iam_role" "dns" {
           "Condition" : {
             "ForAnyValue:StringLike" : {
               "aws:PrincipalOrgPaths" : ["${data.aws_organizations_organization.root_account.id}/*/${local.environment_management.modernisation_platform_organisation_unit_id}/*"]
-            }
-          },
-          "Condition" : {
+            },
             "ForAnyValue:StringLike" : {
               "aws:PrincipalArn" : ["aws:arn:iam::*:role/github-actions"]
             }
-          }
         }
       ]
   })
