bec93fbf842c91b3bc9c2c1a1a91de38bf5d7dd6
George F <george.fountopoulos@digital.justice.gov.uk>
2022-08-18 08:58:33 +0100

    Features/instance scheduler role (#2151)

--- terraform/environments/bootstrap/delegate-access/iam.tf
+++ terraform/environments/bootstrap/delegate-access/iam.tf
@@ -176,6 +176,47 @@ resource "aws_iam_policy" "member-access" {
   policy      = data.aws_iam_policy_document.member-access.json
 }
 
+module "instance-scheduler-access" {
+  count  = local.account_data.account-type == "member" ? 1 : 0
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-cross-account-access?ref=v2.2.0"
+  providers = {
+    aws = aws.workspace
+  }
+  account_id             = local.modernisation_platform_account.id
+  additional_trust_roles = [format("arn:aws:iam::%s:role/LambdaInstanceSchedulerPolicy", local.environment_management.account_ids["modernisation_platform_account_id"])]
+  policy_arn             = aws_iam_policy.instance-scheduler-access[0].id
+  role_name              = "InstanceSchedulerAccess"
+}
+
+#tfsec:ignore:aws-iam-no-policy-wildcards
+data "aws_iam_policy_document" "instance-scheduler-access" {
+  statement {
+    #checkov:skip=CKV_AWS_108
+    #checkov:skip=CKV_AWS_111
+    #checkov:skip=CKV_AWS_107
+    #checkov:skip=CKV_AWS_109
+    #checkov:skip=CKV_AWS_110
+    effect = "Allow"
+    actions = [
+      "ec2:DescribeInstances",
+      "ec2:DescribeInstanceStatus",
+      "ec2:DescribeTags",
+      "ec2:StartInstances",
+      "ec2:StopInstances"
+    ]
+    resources = ["*"] #tfsec:ignore:AWS099 tfsec:ignore:AWS097
+  }
+}
+
+resource "aws_iam_policy" "instance-scheduler-access" {
+  count    = local.account_data.account-type == "member" ? 1 : 0
+  provider = aws.workspace
+
+  name        = "InstanceSchedulerAccessActions"
+  description = "Restricted policy for use by the Instance Scheduler Lambda in member accounts"
+  policy      = data.aws_iam_policy_document.instance-scheduler-access.json
+}
+
 # Testing-test member access - separate as need the testing user created in the testing account to be able to access as well
 data "aws_iam_policy_document" "assume_role_policy" {
   version = "2012-10-17"
