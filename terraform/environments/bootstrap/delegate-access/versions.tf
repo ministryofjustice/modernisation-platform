0825aa2b9147acf59f04536224e1acb96e61bf36
David Elliott <david.elliott@digital.justice.gov.uk>
2022-09-21 15:31:39 +0100

    Add versions to fix TFlint warnings

--- terraform/environments/bootstrap/delegate-access/versions.tf
+++ terraform/environments/bootstrap/delegate-access/versions.tf
@@ -1,12 +1,16 @@
 terraform {
   required_providers {
     aws = {
-      version = ">= 4.0.0, < 5.0.0"
+      version = "~> 4.0"
       source  = "hashicorp/aws"
     }
     tls = {
       source  = "hashicorp/tls"
       version = "~>4.0"
+    }
+    http = {
+      version = "~> 3.0"
+      source  = "hashicorp/http"
     }
   }
   required_version = "~> 1.0"
