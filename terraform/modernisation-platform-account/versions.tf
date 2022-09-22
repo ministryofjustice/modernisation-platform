80a369e1e92afa48309fb106861b8b33ee801517
julialawrence <98329494+julialawrence@users.noreply.github.com>
2022-09-22 14:56:03 +0100

    fixed a merge conflict gone bad

--- terraform/modernisation-platform-account/versions.tf
+++ terraform/modernisation-platform-account/versions.tf
@@ -5,11 +5,7 @@ terraform {
       source  = "hashicorp/aws"
     }
     archive = {
-<<<<<<< HEAD
-      version = "~> 2.2"
-=======
       version = "~> 2.0"
->>>>>>> main
       source  = "hashicorp/archive"
     }
   }
