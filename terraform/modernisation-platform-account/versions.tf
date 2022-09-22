f979a7ccc5d7a56b6995ca076bbb884271993706
julialawrence <julia.lawrence@digital.justice.gov.uk>
2022-09-22 12:12:07 +0100

    Resolving tflint warnings in mod-platform-acct

--- terraform/modernisation-platform-account/versions.tf
+++ terraform/modernisation-platform-account/versions.tf
@@ -1,8 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      version = ">= 4.0.0, < 5.0.0"
+      version = "~> 4.0"
       source  = "hashicorp/aws"
+    }
+    archive = {
+      version = "~> 2.2"
+      source  = "hashicorp/archive"
     }
   }
   required_version = "~> 1.0"
