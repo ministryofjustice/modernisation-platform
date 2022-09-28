97dc3185f2b5193e8038da052ccee2fec02d4f20
julialawrence <julia.lawrence@digital.justice.gov.uk>
2022-09-28 07:51:16 +0100

    Revert "Changes to templates."
    This reverts commit 0ebe0f528653d02cb3306bb2104f1711552c2bca.

--- terraform/templates/member-providers.tf
+++ terraform/templates/member-providers.tf
@@ -1,14 +1,9 @@
-# ######################### Run Terraform via CICD ##################################
+######################### Run Terraform via CICD ##################################
 # AWS provider for the workspace you're working in (every resource will default to using this, unless otherwise specified)
-provider "aws" {
-  alias  = "oidc-session"
-  region = "eu-west-2"
-}
-
 provider "aws" {
   region = "eu-west-2"
   assume_role {
-    role_arn = "arn:aws:iam::${data.aws_caller_identity.oidc_session.id}:role/MemberInfrastructureAccess"
+    role_arn = "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/MemberInfrastructureAccess"
   }
 }
 
@@ -17,9 +12,6 @@ provider "aws" {
   alias                  = "modernisation-platform"
   region                 = "eu-west-2"
   skip_get_ec2_platforms = true
-  assume_role {
-    role_arn = "arn:aws:iam::${local.modernisation_platform_account_id}:role/modernisation-account-limited-read-member-access"
-  }
 }
 
 # AWS provider for core-vpc-<environment>, to share VPCs into this account
@@ -45,25 +37,10 @@ provider "aws" {
 
 
 ######################### Run Terraform Plan Locally Only ##################################
-# # To run a Terraform Plan locally, uncomment this bottom section of code and comment out the top section
+# To run a Terraform Plan locally, uncomment this bottom section of code and comment out the top section
 
 # provider "aws" {
 #   region = "eu-west-2"
-# }
-
-# provider "aws" {
-#   alias  = "oidc-session"
-#   region = "eu-west-2"
-# }
-
-# # AWS provider for the Modernisation Platform, to get things from there if required
-# provider "aws" {
-#   alias                  = "modernisation-platform"
-#   region                 = "eu-west-2"
-#   skip_get_ec2_platforms = true
-#   assume_role {
-#     role_arn = "arn:aws:iam::${local.modernisation_platform_account_id}:role/modernisation-account-limited-read-member-access"
-#   }
 # }
 
 # # AWS provider for core-vpc-<environment>, to share VPCs into this account
