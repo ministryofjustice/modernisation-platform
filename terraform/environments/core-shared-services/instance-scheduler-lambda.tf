5117a0bf8274ac0cd017185d197ee47d49de02b6
George Fountopoulos <george.fountopoulos@digital.justice.gov.uk>
2022-10-11 16:19:58 +0100

    ECR module name adjustment

--- terraform/environments/core-shared-services/instance-scheduler-lambda.tf
+++ terraform/environments/core-shared-services/instance-scheduler-lambda.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_function" "instance-scheduler-lambda-function" {
   handler                        = "main"
   reserved_concurrent_executions = 0
   runtime                        = "go1.x"
-  image_uri                      = "${module.instance_scheduler_ecr_repo.ecr_repository_name}/instance-scheduler-lambda-function:latest"
+  image_uri                      = "${module.instance_scheduler_ecr_repo.ecr_repository_name}:latest"
   package_type                   = "Image"
   role                           = aws_iam_role.instance-scheduler-lambda-function.arn
 }
