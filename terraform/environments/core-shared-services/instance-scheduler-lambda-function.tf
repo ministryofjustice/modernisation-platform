b8f9bf35f547b1c4994c014d40b7da5ee210794b
George Fountopoulos <george.fountopoulos@digital.justice.gov.uk>
2022-10-13 17:55:37 +0100

    Check: CKV_AWS_50 AWS Lambda functions with tracing not enabled

--- terraform/environments/core-shared-services/instance-scheduler-lambda-function.tf
+++ terraform/environments/core-shared-services/instance-scheduler-lambda-function.tf
@@ -9,4 +9,7 @@ resource "aws_lambda_function" "instance-scheduler-lambda-function" {
   image_uri                      = "${module.instance_scheduler_ecr_repo.ecr_repository_name}:latest"
   package_type                   = "Image"
   role                           = aws_iam_role.instance-scheduler-lambda-function.arn
+  tracing_config {
+    mode = "Active"
+  }
 }
