cbed42d76d285895809c6c11473c7b2935211278
David Sibley <david.sibley@digital.justice.gov.uk>
2022-10-18 13:40:22 +0100

    appended 'live' to fixngo name for clarity

--- terraform/environments/core-network-services/transit_gateway_connections.tf
+++ terraform/environments/core-network-services/transit_gateway_connections.tf
@@ -58,12 +58,12 @@ locals {
   }
 
   egress_pttp_routing_cidrs_live_data = {
-    "azure-fixngo"    = "10.40.0.0/16",
-    "azure-noms-test" = "10.101.0.0/16",
-    "azure-noms-mgmt" = "10.102.0.0/16",
-    "global-protect"  = "10.184.0.0/16",
-    "cloud-platform"  = "172.20.0.0/16",
-    "ppud-psn"        = "51.247.0.0/16",
+    "azure-fixngo-live" = "10.40.0.0/16",
+    "azure-noms-test"   = "10.101.0.0/16",
+    "azure-noms-mgmt"   = "10.102.0.0/16",
+    "global-protect"    = "10.184.0.0/16",
+    "cloud-platform"    = "172.20.0.0/16",
+    "ppud-psn"          = "51.247.0.0/16",
   }
 
   tgw_live_data_attachments = {
