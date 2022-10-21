485ce083ed203f028b8dde1961eb5409ebdd76c7
George F <george.fountopoulos@digital.justice.gov.uk>
2022-10-21 14:05:22 +0100

    Add the lambda role to trust roles for the InstanceSchedulerAccess role (#2464)
    * Add the lambda role to trust roles for the InstanceSchedulerAccess role
    
    * Commit changes made by code formatters
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- terraform/environments/bootstrap/delegate-access/iam.tf
+++ terraform/environments/bootstrap/delegate-access/iam.tf
@@ -191,12 +191,10 @@ module "instance-scheduler-access" {
   providers = {
     aws = aws.workspace
   }
-  # All roles under the core-shared-services, including the InstanceSchedulerLambdaFunctionPolicy, can assume the role
-  # InstanceSchedulerAccess in all member accounts, therefore, we don't need to explicitly specify
-  # InstanceSchedulerLambdaFunctionPolicy as part of the additional_trust_roles variable
-  account_id = local.environment_management.account_ids["core-shared-services-production"]
-  policy_arn = aws_iam_policy.instance-scheduler-access[0].id
-  role_name  = "InstanceSchedulerAccess"
+  account_id             = local.environment_management.account_ids["core-shared-services-production"]
+  additional_trust_roles = [format("arn:aws:iam::%s:role/LambdaInstanceSchedulerPolicy", local.environment_management.account_ids["core-shared-services-production"])]
+  policy_arn             = aws_iam_policy.instance-scheduler-access[0].id
+  role_name              = "InstanceSchedulerAccess"
 }
 
 #tfsec:ignore:aws-iam-no-policy-wildcards
