a86dee64a7c7a136d6ed426d7ef9d968f3f66cc5
julialawrence <julia.lawrence@digital.justice.gov.uk>
2022-10-26 11:55:09 +0100

    correcting checkov ref.

--- terraform/environments/core-shared-services/instance-scheduler-lambda-function.tf
+++ terraform/environments/core-shared-services/instance-scheduler-lambda-function.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "instance-scheduler-lambda-function" {
   #checkov:skip=CKV_AWS_116
   #checkov:skip=CKV_AWS_117
   #checkov:skip=CKV_AWS_272 "Code signing not required"
-  #checkov:skip=CKV_AWS_117 "These lambda envvars aren't sensitive and don't need a cmk. Default AWS KMS key is sufficient"
+  #checkov:skip=CKV_AWS_173 "These lambda envvars aren't sensitive and don't need a cmk. Default AWS KMS key is sufficient"
   function_name                  = "instance-scheduler-lambda-function"
   reserved_concurrent_executions = 0
   image_uri                      = "${local.environment_management.account_ids[terraform.workspace]}.dkr.ecr.eu-west-2.amazonaws.com/${module.instance_scheduler_ecr_repo.ecr_repository_name}:latest"
