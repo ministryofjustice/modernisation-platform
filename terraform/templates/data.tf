2512006d1e12ebc6828d8a7e4748b2b2a6526ff0
David Elliott <david.elliott@digital.justice.gov.uk>
2022-10-28 12:04:26 +0100

    Add documentation for shared KMS keys
    Also adds the required data sources to the data.tf, so users don't need
    to add this themselves.
    
    A further PR to add this data to the existing environments in the
    environments repo will be required.

--- terraform/templates/data.tf
+++ terraform/templates/data.tf
@@ -125,6 +125,19 @@ data "aws_route53_zone" "network-services" {
   private_zone = false
 }
 
+# Shared KMS keys (per business unit)
+data "aws_kms_key" "general_shared" {
+	key_id = "arn:aws:kms:eu-west-2:${local.environment_management.account_ids["core-shared-services-production"]}:alias/general-${var.networking[0].business-unit}"
+}
+
+data "aws_kms_key" "ebs_shared" {
+	key_id = "arn:aws:kms:eu-west-2:${local.environment_management.account_ids["core-shared-services-production"]}:alias/ebs-${var.networking[0].business-unit}"
+}
+
+data "aws_kms_key" "rds_shared" {
+	key_id = "arn:aws:kms:eu-west-2:${local.environment_management.account_ids["core-shared-services-production"]}:alias/rds-${var.networking[0].business-unit}"
+}
+
 # State for core-network-services resource information
 data "terraform_remote_state" "core_network_services" {
   backend = "s3"
