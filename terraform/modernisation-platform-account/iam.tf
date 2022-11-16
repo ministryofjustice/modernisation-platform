d42a1bf648244e69c974db04f1b4884292d8e4c1
julialawrence <julia.lawrence@digital.justice.gov.uk>
2022-11-16 07:22:11 +0000

    Correcting repo name passed to the OIDC module to include the org name. This is a bug fix.

--- terraform/modernisation-platform-account/iam.tf
+++ terraform/modernisation-platform-account/iam.tf
@@ -372,7 +372,7 @@ module "github-oidc" {
   source                      = "github.com/ministryofjustice/modernisation-platform-github-oidc-provider?ref=v2.0.0"
   additional_permissions      = data.aws_iam_policy_document.oidc-deny-specific-actions.json
   additional_managed_policies = ["arn:aws:iam::aws:policy/AdministratorAccess"]
-  github_repositories         = ["ministryofjustice/modernisation-platform:*", "modernisation-platform-ami-builds:*"]
+  github_repositories         = ["ministryofjustice/modernisation-platform:*", "ministryofjustice/modernisation-platform-ami-builds:*"]
   tags_common                 = { "Name" = format("%s-oidc", terraform.workspace) }
   tags_prefix                 = ""
 }
