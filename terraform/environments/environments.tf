7503f59069c2b80a24c2b1d9eda48a84147fbb87
julialawrence <julia.lawrence@digital.justice.gov.uk>
2022-12-06 11:48:08 +0000

    Environment file and environments terraform modifications for awsnuke

--- terraform/environments/environments.tf
+++ terraform/environments/environments.tf
@@ -1,5 +1,5 @@
 module "environments" {
-  source                             = "github.com/ministryofjustice/modernisation-platform-terraform-environments?ref=v4.0.0"
+  source                             = "github.com/ministryofjustice/modernisation-platform-terraform-environments?ref=v5.0.0"
   environment_directory              = "../../environments"
   environment_parent_organisation_id = local.environment_management.modernisation_platform_organisation_unit_id
   environment_prefix                 = "modernisation-platform"
