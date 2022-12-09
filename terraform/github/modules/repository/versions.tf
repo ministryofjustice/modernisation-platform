e24feedaa7b86761947ed7991d9fda06e75828a5
David Elliott <david.elliott@digital.justice.gov.uk>
2022-12-09 09:52:04 +0000

    Pinning until security block issue fixed

--- terraform/github/modules/repository/versions.tf
+++ terraform/github/modules/repository/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.0"
   required_providers {
     github = {
-      version = "~> 5.2"
+      version = "5.8.0"
       source  = "integrations/github"
     }
   }
