757a1308752ee3c1cd066b883d39f954c7b9b15c
julialawrence <julia.lawrence@digital.justice.gov.uk>
2022-12-12 12:42:39 +0000

    Testing fix in module local name.

--- terraform/environments/environments.tf
+++ terraform/environments/environments.tf
@@ -1,5 +1,5 @@
 module "environments" {
-  source                             = "github.com/ministryofjustice/modernisation-platform-terraform-environments?ref=v5.0.1"
+  source                             = "github.com/ministryofjustice/modernisation-platform-terraform-environments?ref=30f855778a45a5b7446c033c67bcc1347c3cdbdd"
   environment_directory              = "../../environments"
   environment_parent_organisation_id = local.environment_management.modernisation_platform_organisation_unit_id
   environment_prefix                 = "modernisation-platform"
