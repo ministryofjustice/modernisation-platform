3432b86b86e4343fcfad26fa95024a70b4de28fa
jakemulley <jakemulley@users.noreply.github.com>
2022-12-29 15:03:48 +0000

    Capture the aws-cli output as a variable, rather than waiting for input

--- scripts/update-sso-permission-sets.sh
+++ scripts/update-sso-permission-sets.sh
@@ -12,6 +12,9 @@ for permission_set in $permission_sets; do
 	arn=$(echo $description | jq -r '.PermissionSet.PermissionSetArn')
 	if [[ $name == modernisation-platform* ]];then
 		echo "Updating permission set $name"
-		aws sso-admin provision-permission-set --instance-arn $instance_arn --permission-set-arn $arn --target-type ALL_PROVISIONED_ACCOUNTS
+
+		# Capture the aws-cli output as a variable, rather than blocking the next task
+		output=$(aws sso-admin provision-permission-set --instance-arn $instance_arn --permission-set-arn $arn --target-type ALL_PROVISIONED_ACCOUNTS --output json)
+		echo $output
 	fi
 done
