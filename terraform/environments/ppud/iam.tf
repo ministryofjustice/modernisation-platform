9b645cafc5e74a85a9a2e73217c750dfe8eb6ccc
David Sibley <david.sibley@digital.justice.gov.uk>
2023-01-09 11:28:39 +0000

    added IAM user for SES email sending

--- terraform/environments/ppud/iam.tf
+++ terraform/environments/ppud/iam.tf
@@ -12,3 +12,33 @@ resource "aws_iam_user_policy_attachment" "mgn_attach_policy" {
   user       = aws_iam_user.mgn_user[0].name
   policy_arn = "arn:aws:iam::aws:policy/AWSApplicationMigrationFullAccess"
 }
+
+#tfsec:ignore:aws-iam-no-user-attached-policies
+resource "aws_iam_user" "email" {
+  name = format("%s-%s-email_user", local.application_name, local.environment)
+  tags = merge(local.tags,
+    { Name = format("%s-%s-email_user", local.application_name, local.environment) }
+  )
+}
+
+resource "aws_iam_access_key" "email" {
+  user = aws_iam_user.email.name
+}
+
+#tfsec:ignore:aws-iam-no-policy-wildcards
+resource "aws_iam_user_policy" "email_policy" {
+  name   = "AmazonSesSendingAccess"
+  user   = aws_iam_user.email.name
+  policy = data.aws_iam_policy_document.email.json
+}
+
+#tfsec:ignore:aws-iam-no-policy-wildcards
+data "aws_iam_policy_document" "email" {
+  #checkov:skip=CKV_AWS_111
+  statement {
+    actions = [
+      "ses:SendRawEmail"
+    ]
+    resources = ["*"]
+  }
+}
