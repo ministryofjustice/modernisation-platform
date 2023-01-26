bab324f0976610638900ab5ef91a60e6036ccfde
mark roberts <mark.roberts@digital.justice.gov.uk>
2023-01-26 15:39:17 +0000

    all soo errors resolved

--- terraform/environments/ppud/iam.tf
+++ terraform/environments/ppud/iam.tf
@@ -16,6 +16,7 @@ resource "aws_iam_user_policy_attachment" "mgn_attach_policy" {
 
 #tfsec:ignore:aws-iam-user
 resource "aws_iam_user" "email" {
+  #checkov:skip=CKV_AWS_273: "Skipping"
   name = format("%s-%s-email_user", local.application_name, local.environment)
   tags = merge(local.tags,
     { Name = format("%s-%s-email_user", local.application_name, local.environment) }
