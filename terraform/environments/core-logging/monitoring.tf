fd096031a17b4d06a703ee3e7b381f5d2e4f9376
Stephen Linden <stephen.linden@digital.justice.gov.uk>
2023-01-31 16:23:41 +0000

    Pt 2 of initial commit

--- terraform/environments/core-logging/monitoring.tf
+++ terraform/environments/core-logging/monitoring.tf
@@ -2,3 +2,15 @@ module "core_monitoring" {
   source                     = "../../modules/core-monitoring"
   pagerduty_integration_keys = local.pagerduty_integration_keys
 }
+
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
+
+resource "aws_cloudwatch_log_group" "aws_route53_logs_com" {
+  provider = aws.us-east-1
+
+  name              = "aws_route53_zone.logs_com.name"
+  retention_in_days = 365
+}
---
+++ terraform/modules/route53-logging/variables.tf
@@ -0,0 +1,6 @@
+tags = {
+    business-unit = "Platforms"
+    application   = "Modernisation Platform: ${terraform.workspace}"
+    is-production = local.is-production
+    owner         = "Modernisation Platform: modernisation-platform@digital.justice.gov.uk"
+  }
