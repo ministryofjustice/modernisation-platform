0d9d3e23ff138bdf000a7dce540d617cde94b60d
Stephen Linden <stephen.linden@digital.justice.gov.uk>
2023-02-01 10:29:10 +0000

    Initial module build

--- terraform/environments/core-logging/monitoring.tf
+++ terraform/environments/core-logging/monitoring.tf
@@ -14,3 +14,32 @@ resource "aws_cloudwatch_log_group" "aws_route53_logs_com" {
   name              = "aws_route53_zone.logs_com.name"
   retention_in_days = 365
 }
+
+data "aws_iam_policy_document" "route53-query-logging-policy" {
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:*:*:log-group:/aws/route53/*"]
+
+    principals {
+      identifiers = ["route53.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
+resource "aws_cloudwatch_log_resource_policy" "route53-query-logging-policy" {
+  provider = aws.us-east-1
+
+  policy_document = data.aws_iam_policy_document.route53-query-logging-policy.json
+  policy_name     = "route53-query-logging-policy"
+}
+
+# Example Route53 zone with query logging
+
+resource "aws_route53_zone" "logs_com" {
+  name = "logs.com"
+}
--- terraform/modules/route53-logging/main.tf
+++ terraform/modules/route53-logging/main.tf
@@ -12,34 +12,34 @@
 # Example CloudWatch log resource policy to allow Route53 to write logs
 # to any log group under /aws/route53/*
 
-data "aws_iam_policy_document" "route53-query-logging-policy" {
-  statement {
-    actions = [
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-    ]
+# data "aws_iam_policy_document" "route53-query-logging-policy" {
+#   statement {
+#     actions = [
+#       "logs:CreateLogStream",
+#       "logs:PutLogEvents",
+#     ]
 
-    resources = ["arn:aws:logs:*:*:log-group:/aws/route53/*"]
+#     resources = ["arn:aws:logs:*:*:log-group:/aws/route53/*"]
 
-    principals {
-      identifiers = ["route53.amazonaws.com"]
-      type        = "Service"
-    }
-  }
-}
+#     principals {
+#       identifiers = ["route53.amazonaws.com"]
+#       type        = "Service"
+#     }
+#   }
+# }
 
-resource "aws_cloudwatch_log_resource_policy" "route53-query-logging-policy" {
-  provider = aws.us-east-1
+# resource "aws_cloudwatch_log_resource_policy" "route53-query-logging-policy" {
+#   provider = aws.us-east-1
 
-  policy_document = data.aws_iam_policy_document.route53-query-logging-policy.json
-  policy_name     = "route53-query-logging-policy"
-}
+#   policy_document = data.aws_iam_policy_document.route53-query-logging-policy.json
+#   policy_name     = "route53-query-logging-policy"
+# }
 
-# Example Route53 zone with query logging
+# # Example Route53 zone with query logging
 
-resource "aws_route53_zone" "logs_com" {
-  name = "logs.com"
-}
+# resource "aws_route53_zone" "logs_com" {
+#   name = "logs.com"
+# }
 
 # resource "aws_route53_query_log" "example_com" {
 #   depends_on = [aws_cloudwatch_log_resource_policy.route53-query-logging-policy]
