bc9e5b7c66ad61c5c2a071143c04ebe9d7d31578
David Sibley <david.sibley@digital.justice.gov.uk>
2023-02-01 14:21:41 +0000

    adjusted the flow to go back inline with original syntax but refer to new PAT value

--- terraform/environments/core-vpc/vpc.tf
+++ terraform/environments/core-vpc/vpc.tf
@@ -105,7 +105,7 @@ module "vpc" {
 module "vpc_nacls" {
   source           = "../../modules/vpc-nacls"
   for_each         = local.vpcs[terraform.workspace]
-  additional_cidrs      = each.value.options.additional_cidrs
+  additional_cidrs     = each.value.options.additional_cidrs
   additional_vpcs       = each.value.options.additional_vpcs
   tags             = local.tags
   tags_prefix      = each.key
