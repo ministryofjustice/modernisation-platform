6c2fe3b46f84f8975a97fac6944b1f18b12a40b1
dms1981 <david.sibley@digital.justice.gov.uk>
2023-02-13 16:01:11 +0000

    Refactor R53 resolver logs (#3114)
    * cleaned up old resource declarations, logs should now write to a per-VPC cloudwatch log group
    
    * Commit changes made by code formatters
    
    * changed to use VPC names in some resources for readability
    
    * removed leftover data call from when I was trying to get ARNs a different way
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- terraform/modules/r53-resolver-logs/main.tf
+++ terraform/modules/r53-resolver-logs/main.tf
@@ -1,10 +1,36 @@
 resource "aws_route53_resolver_query_log_config" "main" {
-  name            = "dns_logs"
-  destination_arn = var.logging_destination_arn
+  name            = format("%s-r53-resolver-logs", var.vpc_name)
+  destination_arn = aws_cloudwatch_log_group.main.arn
   tags            = var.tags_common
 }
 
 resource "aws_route53_resolver_query_log_config_association" "main" {
   resolver_query_log_config_id = aws_route53_resolver_query_log_config.main.id
   resource_id                  = var.vpc_id
+}
+
+resource "aws_cloudwatch_log_group" "main" {
+  name              = format("%s-r53-resolver-logs", var.vpc_name)
+  retention_in_days = 365
+}
+
+resource "aws_cloudwatch_log_resource_policy" "route53-query-logging-policy" {
+  policy_document = data.aws_iam_policy_document.route53-query-logging-policy.json
+  policy_name     = format("%s-r53-query-logging-policy", var.vpc_name)
+}
+
+data "aws_iam_policy_document" "route53-query-logging-policy" {
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = [aws_cloudwatch_log_group.main.arn]
+
+    principals {
+      identifiers = ["route53resolver.amazonaws.com"]
+      type        = "Service"
+    }
+  }
 }
