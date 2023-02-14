8ab1ddd1d3e3ff7776b8f5c123bd7863e3ce5810
Ewa Stempel <ewa.stempel@digital.justice.gov.uk>
2023-02-14 12:54:27 +0000

    Updating default files for analytical platform accounts

--- terraform/environments/analytical-platform-management/platform_versions.tf
+++ terraform/environments/analytical-platform-management/platform_versions.tf
@@ -1,8 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      version = ">= 4.0.0, < 5.0.0"
+      version = "~> 4.0"
       source  = "hashicorp/aws"
+    }
+    http = {
+      version = "~> 3.0"
+      source  = "hashicorp/http"
     }
   }
   required_version = "~> 1.0"
