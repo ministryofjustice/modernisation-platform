5814e53931e66693d36782223faea2bac4377407
ewastempel <52969218+ewastempel@users.noreply.github.com>
2023-02-14 14:06:32 +0000

    Updating default files for analytical platform accounts (#3120)
    Skipping subnet_share for unrestricted accounts and updating default files for analytical platform

--- terraform/environments/analytical-platform-data/platform_backend.tf
+++ terraform/environments/analytical-platform-data/platform_backend.tf
@@ -9,5 +9,6 @@ terraform {
     key                  = "terraform.tfstate"
     region               = "eu-west-2"
     workspace_key_prefix = "environments/accounts/analytical-platform-data" # This will store the object as environments/accounts/analytical-platform-data/${workspace}/terraform.tfstate
+    dynamodb_table       = "modernisation-platform-terraform-state-lock"
   }
 }
