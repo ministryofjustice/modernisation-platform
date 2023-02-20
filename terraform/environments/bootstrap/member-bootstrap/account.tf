f63a80462c2f5d3316c6716afe9acd18ba57a60c
David Elliott <david.elliott@digital.justice.gov.uk>
2023-02-20 16:18:17 +0000

    Remove security alternate contact
    This is managed centrally and you can only have one security alternate
    contact.

--- terraform/environments/bootstrap/member-bootstrap/account.tf
+++ terraform/environments/bootstrap/member-bootstrap/account.tf
@@ -7,13 +7,3 @@ resource "aws_account_alternate_contact" "infrastructure_support_operations" {
   email_address = local.application_tags.infrastructure-support
   phone_number  = "+0000000000"
 }
-
-resource "aws_account_alternate_contact" "infrastructure_support_security" {
-
-  alternate_contact_type = "SECURITY"
-
-  name          = "Infrastructure Support"
-  title         = "Infrastructure Support Security"
-  email_address = local.application_tags.infrastructure-support
-  phone_number  = "+0000000000"
-}
