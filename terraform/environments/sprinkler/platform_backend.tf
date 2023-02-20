ca1377abd10755a6415ba6d4a42032bc5b09a159
ewastempel <52969218+ewastempel@users.noreply.github.com>
2023-02-20 09:41:55 +0000

    Refactoring default files for sprinkler account (#3157)

--- terraform/environments/sprinkler/platform_backend.tf
+++ terraform/environments/sprinkler/platform_backend.tf
@@ -8,7 +8,7 @@ terraform {
     encrypt              = true
     key                  = "terraform.tfstate"
     region               = "eu-west-2"
-    workspace_key_prefix = "environments/accounts/sprinkler" # This will store the object as environments/sprinkler/${workspace}/terraform.tfstate
+    workspace_key_prefix = "environments/accounts/sprinkler" # This will store the object as environments/accounts/sprinkler/${workspace}/terraform.tfstate
     dynamodb_table       = "modernisation-platform-terraform-state-lock"
   }
 }
