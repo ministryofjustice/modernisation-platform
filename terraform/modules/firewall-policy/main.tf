0eee24ba2a40f6a3b1f83d8395f68b299c9ba0b1
mark roberts <mark.roberts@digital.justice.gov.uk>
2023-02-28 16:46:18 +0000

    priority change

--- terraform/modules/firewall-policy/main.tf
+++ terraform/modules/firewall-policy/main.tf
@@ -9,7 +9,6 @@ resource "aws_networkfirewall_firewall_policy" "main" {
       rule_order = "DEFAULT_ACTION_ORDER"
     }
     stateful_rule_group_reference {
-      priority     = 1
       resource_arn = aws_networkfirewall_rule_group.stateful.arn
     }
     stateless_default_actions          = ["aws:forward_to_sfe"]
