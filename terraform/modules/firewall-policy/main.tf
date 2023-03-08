3a9dc32ed8f8d3663a480a7aeb278364f030f217
mark roberts <mark.roberts@digital.justice.gov.uk>
2023-03-08 15:26:45 +0000

    add new rulegroup to the firewall policy

--- terraform/modules/firewall-policy/main.tf
+++ terraform/modules/firewall-policy/main.tf
@@ -11,6 +11,9 @@ resource "aws_networkfirewall_firewall_policy" "main" {
     stateful_rule_group_reference {
       resource_arn = aws_networkfirewall_rule_group.stateful.arn
     }
+    stateful_rule_group_reference {
+      resource_arn = aws_networkfirewall_rule_group.fqdn-stateful.arn
+    }
     stateless_default_actions          = ["aws:forward_to_sfe"]
     stateless_fragment_default_actions = ["aws:drop"]
   }
