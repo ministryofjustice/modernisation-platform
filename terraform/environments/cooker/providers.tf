161415af9dea9e4cdfbe795177ae2f854e867c47
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2023-03-10 09:56:40 +0000

    Initial commit for us-east-1 test on cooker.

--- terraform/environments/cooker/providers.tf
+++ terraform/environments/cooker/providers.tf
@@ -31,3 +31,11 @@ provider "aws" {
     role_arn = "arn:aws:iam::${local.environment_management.account_ids["core-network-services-production"]}:role/ModernisationPlatformAccess"
   }
 }
+
+provider "aws" {
+  alias  = "us-east"
+  region = "us-east-1"
+  assume_role {
+    role_arn = "arn:aws:iam::${local.environment_management.account_ids[local.provider_name]}:role/MemberInfrastructureAccessUSEast"
+  }
+}
