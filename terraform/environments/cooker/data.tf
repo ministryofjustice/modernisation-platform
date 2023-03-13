28d3e30cd67e0143fe928728f7ffc02789f881e2
ep-93 <109581241+ep-93@users.noreply.github.com>
2023-03-13 17:14:53 +0000

    Update data.tf

--- terraform/environments/cooker/data.tf
+++ terraform/environments/cooker/data.tf
@@ -2,3 +2,13 @@
 data "http" "environments_file" {
   url = "https://raw.githubusercontent.com/ministryofjustice/modernisation-platform/main/environments/${local.application_name}.json"
 }
+
+resource "aws_acm_certificate" "cloudfront" {
+  domain_name       = "http://modernisation-platform.service.justice.gov.uk/"
+  validation_method = "DNS"
+  provider          = aws.us-east-1
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
