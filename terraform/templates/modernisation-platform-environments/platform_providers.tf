8a4907e7fb96c168d68b720f64413f02d50051b0
David Elliott <david.elliott@digital.justice.gov.uk>
2023-04-05 16:12:34 +0100

    Add dynamic provider for us-east-1
    This will allow plans locally for resources created in us-east-1 which
    were failing previously.

--- terraform/templates/modernisation-platform-environments/platform_providers.tf
+++ terraform/templates/modernisation-platform-environments/platform_providers.tf
@@ -44,6 +44,6 @@ provider "aws" {
   alias  = "us-east-1"
   region = "us-east-1"
   assume_role {
-    role_arn = "arn:aws:iam::${data.aws_caller_identity.original_session.id}:role/MemberInfrastructureAccessUSEast"
+    role_arn = !can(regex("githubactionsrolesession|AdministratorAccess|user", data.aws_caller_identity.original_session.arn)) ? null : can(regex("user", data.aws_caller_identity.original_session.arn)) ? "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/${var.collaborator_access}" : "arn:aws:iam::${data.aws_caller_identity.original_session.id}:role/MemberInfrastructureAccessUSEast"
   }
 }
