f2cd9a71a7ef213eed9e608993b180adba9fabf3
David Elliott <david.elliott@digital.justice.gov.uk>
2023-04-06 16:37:53 +0100

    Move to service orientated structure
    First step in refactoring are alerts, creating the new services,
    business services and dependancies.

--- terraform/pagerduty/business-services.tf
+++ terraform/pagerduty/business-services.tf
@@ -8,35 +8,70 @@ resource "pagerduty_business_service" "modernisation_platform" {
   team             = pagerduty_team.modernisation_platform.id
 }
 
-resource "pagerduty_service_dependency" "modernisation_platform_high_priority" {
+resource "pagerduty_business_service" "modernisation_platform_networking" {
+  name             = "Networking - Modernisation Platform"
+  description      = "Networking"
+  point_of_contact = "#ask-modernisation-platform"
+  team             = pagerduty_team.modernisation_platform.id
+}
+
+resource "pagerduty_business_service" "modernisation_platform_security" {
+  name             = "Security - Modernisation Platform"
+  description      = "Security"
+  point_of_contact = "#ask-modernisation-platform"
+  team             = pagerduty_team.modernisation_platform.id
+}
+
+resource "pagerduty_business_service" "modernisation_platform_operations" {
+  name             = "Operations - Modernisation Platform"
+  description      = "Operations"
+  point_of_contact = "#ask-modernisation-platform"
+  team             = pagerduty_team.modernisation_platform.id
+}
+
+resource "pagerduty_service_dependency" "modernisation_platform_networking" {
   dependency {
     dependent_service {
       id   = pagerduty_business_service.modernisation_platform.id
       type = "business_service"
     }
     supporting_service {
-      id   = pagerduty_service.high_priority.id
-      type = "service"
+      id   = pagerduty_business_service.modernisation_platform_networking.id
+      type = "business_service"
     }
   }
 }
 
-resource "pagerduty_service_dependency" "modernisation_platform_low_priority" {
+resource "pagerduty_service_dependency" "modernisation_platform_security" {
   dependency {
     dependent_service {
       id   = pagerduty_business_service.modernisation_platform.id
       type = "business_service"
     }
     supporting_service {
-      id   = pagerduty_service.low_priority.id
-      type = "service"
+      id   = pagerduty_business_service.modernisation_platform_security.id
+      type = "business_service"
     }
   }
 }
-resource "pagerduty_service_dependency" "modernisation_platform_core_alerts" {
+
+resource "pagerduty_service_dependency" "modernisation_platform_operations" {
   dependency {
     dependent_service {
       id   = pagerduty_business_service.modernisation_platform.id
+      type = "business_service"
+    }
+    supporting_service {
+      id   = pagerduty_business_service.modernisation_platform_operations.id
+      type = "business_service"
+    }
+  }
+}
+
+resource "pagerduty_service_dependency" "modernisation_platform_core_alerts" {
+  dependency {
+    dependent_service {
+      id   = pagerduty_business_service.modernisation_platform_security.id
       type = "business_service"
     }
     supporting_service {
@@ -48,11 +83,37 @@ resource "pagerduty_service_dependency" "modernisation_platform_core_alerts" {
 resource "pagerduty_service_dependency" "modernisation_platform_on_call" {
   dependency {
     dependent_service {
-      id   = pagerduty_business_service.modernisation_platform.id
+      id   = pagerduty_business_service.modernisation_platform_operations.id
       type = "business_service"
     }
     supporting_service {
       id   = pagerduty_service.contact_on_call.id
+      type = "service"
+    }
+  }
+}
+
+resource "pagerduty_service_dependency" "modernisation_platform_ddos" {
+  dependency {
+    dependent_service {
+      id   = pagerduty_business_service.modernisation_platform_security.id
+      type = "business_service"
+    }
+    supporting_service {
+      id   = pagerduty_service.ddos.id
+      type = "service"
+    }
+  }
+}
+
+resource "pagerduty_service_dependency" "modernisation_platform_tgw" {
+  dependency {
+    dependent_service {
+      id   = pagerduty_business_service.modernisation_platform_networking.id
+      type = "business_service"
+    }
+    supporting_service {
+      id   = pagerduty_service.tgw.id
       type = "service"
     }
   }
--- terraform/pagerduty/services.tf
+++ terraform/pagerduty/services.tf
@@ -42,8 +42,8 @@ resource "pagerduty_service_integration" "low_priority_cloudwatch" {
 
 ### Low priority core alerts
 resource "pagerduty_service" "core_alerts" {
-  name                    = "Modernisation Platform Core Alerts"
-  description             = "Modernisation Platform Core Infrastructure Alerts"
+  name                    = "Core Alerts - Modernisation Platform"
+  description             = "Core Infrastructure Alerts"
   auto_resolve_timeout    = 14400
   acknowledgement_timeout = "null"
   escalation_policy       = pagerduty_escalation_policy.low_priority.id
@@ -93,7 +93,7 @@ resource "pagerduty_service_event_rule" "mfa-console-access" {
 # contact-on-call-modernisation-platform email
 # note email integration must be configured manually in PagerDuty
 resource "pagerduty_service" "contact_on_call" {
-  name                    = "Contact On Call Email Address"
+  name                    = "Contact On Call Email Address -  Modernisation Platform"
   description             = "Emails received to the on call email address"
   auto_resolve_timeout    = 345600
   acknowledgement_timeout = "null"
@@ -103,4 +103,42 @@ resource "pagerduty_service" "contact_on_call" {
     type    = "constant"
     urgency = "high"
   }
+}
+
+resource "pagerduty_service" "ddos" {
+  name                    = "DDOS Protection - Modernisation Platform"
+  description             = "Member DDOS Protection"
+  auto_resolve_timeout    = 345600
+  acknowledgement_timeout = "null"
+  escalation_policy       = pagerduty_escalation_policy.on_call.id
+  alert_creation          = "create_alerts_and_incidents"
+  incident_urgency_rule {
+    type    = "constant"
+    urgency = "high"
+  }
+}
+
+resource "pagerduty_service_integration" "ddos_cloudwatch" {
+  name    = data.pagerduty_vendor.cloudwatch.name
+  service = pagerduty_service.ddos.id
+  vendor  = data.pagerduty_vendor.cloudwatch.id
+}
+
+resource "pagerduty_service" "tgw" {
+  name                    = "Transit Gateway - Modernisation Platform"
+  description             = "Transit Gateway"
+  auto_resolve_timeout    = 345600
+  acknowledgement_timeout = "null"
+  escalation_policy       = pagerduty_escalation_policy.on_call.id
+  alert_creation          = "create_alerts_and_incidents"
+  incident_urgency_rule {
+    type    = "constant"
+    urgency = "high"
+  }
+}
+
+resource "pagerduty_service_integration" "tgw" {
+  name    = data.pagerduty_vendor.cloudwatch.name
+  service = pagerduty_service.tgw.id
+  vendor  = data.pagerduty_vendor.cloudwatch.id
 }
