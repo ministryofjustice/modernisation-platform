338cfcb198dd951d3584cfde80d22355ea55c00a
David Sibley <david.sibley@digital.justice.gov.uk>
2023-04-24 14:37:40 +0100

    moved firewall logging into separate module to prevent cyclic dependency issues

---
+++ terraform/modules/firewall-logging/variables.tf
@@ -0,0 +1,14 @@
+variable "cloudwatch_log_group_name" {
+  description = "Name of CloudWatch log group to ship logs to"
+  type        = string
+}
+
+variable "fw_arn" {
+  description = "ARN of firewall for logging configuration"
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of keys and values used to create resource metadata tags"
+  type        = map(any)
+}
