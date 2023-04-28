affe5cff47e250c87fd9adf994eebbc0e7d2c9f5
SteveLinden <stephen.linden@digital.justice.gov.uk>
2023-04-28 09:42:26 +0100

    Correcting spelling mistake and adding remove tags

--- terraform/environments/bootstrap/delegate-access/policies.tf
+++ terraform/environments/bootstrap/delegate-access/policies.tf
@@ -102,7 +102,7 @@ data "aws_iam_policy_document" "developer_additional" {
     effect = "Allow"
     actions = [
       "acm:ImportCertificate",
-      "acm.AddTagsToCertificate",
+      "acm:AddTagsToCertificate",
       "autoscaling:UpdateAutoScalingGroup",
       "autoscaling:SetDesiredCapacity",
       "aws-marketplace:ViewSubscriptions",
