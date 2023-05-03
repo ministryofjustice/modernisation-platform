135b54fe659da68f1b81f78b57f10c679da45c65
Dominic Robinson <65237317+drobinson-moj@users.noreply.github.com>
2023-05-03 11:12:19 +0100

    nomis: DSOS-1833: add new pager duty integrations for Sheffield DBA team (#3774)
    * add new pager duty integrations for Sheffield DBA team
    
    * whitespace and comments

--- terraform/pagerduty/member-services-integrations.tf
+++ terraform/pagerduty/member-services-integrations.tf
@@ -181,3 +181,49 @@ resource "pagerduty_service_integration" "laa_mojfin_prod_cloudwatch" {
 }
 
 # # Slack channel: #laa-alerts-mojfin-prod
+
+# NOTE: Update escalation_policy once alarms have been tested
+resource "pagerduty_service" "hmpps_shef_dba_high_priority" {
+  name                    = "HMPPS Sheffield DBA High Priority Alarms"
+  description             = "Production alarms requiring immediate attention by Sheffield DBAs, i.e. worthy of overnight callout"
+  auto_resolve_timeout    = 345600
+  acknowledgement_timeout = "null"
+  escalation_policy       = pagerduty_escalation_policy.member_policy.id
+  alert_creation          = "create_alerts_and_incidents"
+}
+resource "pagerduty_service_integration" "hmpps_shef_dba_high_priority" {
+  name    = data.pagerduty_vendor.cloudwatch.name
+  service = pagerduty_service.hmpps_shef_dba_high_priority.id
+  vendor  = data.pagerduty_vendor.cloudwatch.id
+}
+# Slack channel: dba_alerts_prod
+
+resource "pagerduty_service" "hmpps_shef_dba_low_priority" {
+  name                    = "HMPPS Sheffield DBA Low Priority Alarms"
+  description             = "Low priority production alarms for attention of Sheffield DBAs"
+  auto_resolve_timeout    = 345600
+  acknowledgement_timeout = "null"
+  escalation_policy       = pagerduty_escalation_policy.member_policy.id
+  alert_creation          = "create_alerts_and_incidents"
+}
+resource "pagerduty_service_integration" "hmpps_shef_dba_low_priority" {
+  name    = data.pagerduty_vendor.cloudwatch.name
+  service = pagerduty_service.hmpps_shef_dba_low_priority.id
+  vendor  = data.pagerduty_vendor.cloudwatch.id
+}
+# Slack channel: dba_alerts_prod
+
+resource "pagerduty_service" "hmpps_shef_dba_non_prod" {
+  name                    = "HMPPS Sheffield DBA Non-Production Alarms"
+  description             = "Non-production alarms for attention of Sheffield DBAs"
+  auto_resolve_timeout    = 345600
+  acknowledgement_timeout = "null"
+  escalation_policy       = pagerduty_escalation_policy.member_policy.id
+  alert_creation          = "create_alerts_and_incidents"
+}
+resource "pagerduty_service_integration" "hmpps_shef_dba_non_prod" {
+  name    = data.pagerduty_vendor.cloudwatch.name
+  service = pagerduty_service.hmpps_shef_dba_non_prod.id
+  vendor  = data.pagerduty_vendor.cloudwatch.id
+}
+# Slack channel: dba_alerts_devtest
