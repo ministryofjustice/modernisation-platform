ee8bbb3ad70cee84405afd39d3329e75636884e5
David Sibley <david.sibley@digital.justice.gov.uk>
2023-05-17 14:36:34 +0100

    rewrote variable names for readability

--- terraform/environments/core-network-services/test/go_test.go
+++ terraform/environments/core-network-services/test/go_test.go
@@ -79,66 +79,66 @@ func TestInspectionVPCs(t *testing.T) {
 	terraform.Plan(t, terraformOptions)
 
 	// Retrieve the VPC output as a map
-    inspectionVPCs := terraform.OutputMap(t, terraformOptions, "inspection_vpc_ids")
+	vpcIDs := terraform.OutputMap(t, terraformOptions, "inspection_vpc_ids")
 
 	// Define a regular expression pattern to match the VPC IDs
-    vpcIDPattern := regexp.MustCompile(`^vpc-[0-9a-f]{17}$`)
+	vpcIDPattern := regexp.MustCompile(`^vpc-[0-9a-f]{17}$`)
 
 	// Assert that the vpc_id values in the map match the regular expression pattern
-	assert.Regexp(t, vpcIDPattern, inspectionVPCs["non_live_data"], "non_live_data vpc_id does not match the expected pattern")
-	assert.Regexp(t, vpcIDPattern, inspectionVPCs["live_data"], "live_data vpc_id does not match the expected pattern")
+	assert.Regexp(t, vpcIDPattern, vpcIDs["non_live_data"], "non_live_data vpc_id does not match the expected pattern")
+	assert.Regexp(t, vpcIDPattern, vpcIDs["live_data"], "live_data vpc_id does not match the expected pattern")
 
 	// Retrieve the subnet outputs
-	inspectionTgwSubnets := terraform.OutputMap(t, terraformOptions, "inspection_tgw_subnets")
-	inspectionInspectionSubnets := terraform.OutputMap(t, terraformOptions, "inspection_inspection_subnets")
-	inspectionPublicSubnets := terraform.OutputMap(t, terraformOptions, "inspection_public_subnets")
+	tgwSubnets := terraform.OutputMap(t, terraformOptions, "inspection_tgw_subnets")
+	inspectionSubnets := terraform.OutputMap(t, terraformOptions, "inspection_inspection_subnets")
+	publicSubnets := terraform.OutputMap(t, terraformOptions, "inspection_public_subnets")
 
-    // Assert that non_live_data has all of its subnets
-	assert.Equal(t, inspectionTgwSubnets["non_live_data"], "3")
-	assert.Equal(t, inspectionInspectionSubnets["non_live_data"], "3")
-	assert.Equal(t, inspectionPublicSubnets["non_live_data"], "3")
+	// Assert that non_live_data has all of its subnets
+	assert.Equal(t, tgwSubnets["non_live_data"], "3")
+	assert.Equal(t, inspectionSubnets["non_live_data"], "3")
+	assert.Equal(t, publicSubnets["non_live_data"], "3")
 
-    // Assert that live_data has all of its subnets
-	assert.Equal(t, inspectionTgwSubnets["live_data"], "3")
-	assert.Equal(t, inspectionInspectionSubnets["live_data"], "3")
-	assert.Equal(t, inspectionPublicSubnets["live_data"], "3")
+	// Assert that live_data has all of its subnets
+	assert.Equal(t, tgwSubnets["live_data"], "3")
+	assert.Equal(t, inspectionSubnets["live_data"], "3")
+	assert.Equal(t, publicSubnets["live_data"], "3")
 
 	// Retrieve firewall ARNs as a map
-	inspectionFirewalls := terraform.OutputMap(t, terraformOptions, "firewall_arn")
+	firewallARNs := terraform.OutputMap(t, terraformOptions, "firewall_arn")
 	// Define a regular expression pattern to match the VPC IDs
-    firewallARNPattern := regexp.MustCompile(`^arn:aws:network-firewall:eu-west-2:[0-9]{12}:firewall/(live-data|non-live-data)-inline-inspection$`)
-    // Assert that the firewall_arn values in the map match the regular expression pattern
-	assert.Regexp(t, firewallARNPattern, inspectionFirewalls["non_live_data"], "non_live_data firewall_arn does not match the expected pattern")
-	assert.Regexp(t, firewallARNPattern, inspectionFirewalls["live_data"], "live_data firewall_arn does not match the expected pattern")
+	firewallARNPattern := regexp.MustCompile(`^arn:aws:network-firewall:eu-west-2:[0-9]{12}:firewall/(live-data|non-live-data)-inline-inspection$`)
+	// Assert that the firewall_arn values in the map match the regular expression pattern
+	assert.Regexp(t, firewallARNPattern, firewallARNs["non_live_data"], "non_live_data firewall_arn does not match the expected pattern")
+	assert.Regexp(t, firewallARNPattern, firewallARNs["live_data"], "live_data firewall_arn does not match the expected pattern")
 
 	// Retrieve Internet Gateway IDs
-	inspectionIGWs := terraform.OutputMap(t, terraformOptions, "inspection_igw_id")
+	igwIDs := terraform.OutputMap(t, terraformOptions, "inspection_igw_id")
 	// Define a regular expression pattern to match the VPC IDs
-    igwIDPattern := regexp.MustCompile(`^igw-[0-9a-f]{17}$`)
-	// Assert that both live_data and non_live_data have an IGW matching the regext
-	assert.Regexp(t, igwIDPattern, inspectionIGWs["non_live_data"], "non_live_data igw_id does not match the expected pattern")
-	assert.Regexp(t, igwIDPattern, inspectionIGWs["live_data"], "live_data igw_id does not match the expected pattern")
+	igwIDPattern := regexp.MustCompile(`^igw-[0-9a-f]{17}$`)
+	// Assert that both live_data and non_live_data have an IGW matching the regex
+	assert.Regexp(t, igwIDPattern, igwIDs["non_live_data"], "non_live_data igw_id does not match the expected pattern")
+	assert.Regexp(t, igwIDPattern, igwIDs["live_data"], "live_data igw_id does not match the expected pattern")
 
 	// Retrieve a map of NAT Gateway outputs
-	inspectionNATGWs := terraform.OutputMap(t, terraformOptions, "inspection_natgw_ids")
+	natGWs := terraform.OutputMap(t, terraformOptions, "inspection_natgw_ids")
 
-    // Assert that each VPC has all of its NAT Gateways
-	assert.Equal(t, inspectionNATGWs["non_live_data"], "3")
-	assert.Equal(t, inspectionNATGWs["non_live_data"], "3")
+	// Assert that each VPC has all of its NAT Gateways
+	assert.Equal(t, natGWs["non_live_data"], "3")
+	assert.Equal(t, natGWs["non_live_data"], "3")
 
 	// Run `terraform output` to get the value of the output variable as a string
 	terraformOutput := terraform.OutputJson(t, terraformOptions, "inspection_default_routes")
 
 	// Parse the string value as JSON to obtain the map structure
-	var inspectionDefaultRoutes map[string]interface{}
-	err := json.Unmarshal([]byte(terraformOutput), &inspectionDefaultRoutes)
+	var defaultRoutes map[string]interface{}
+	err := json.Unmarshal([]byte(terraformOutput), &defaultRoutes)
 	if err != nil {
 		t.Fatalf("Failed to parse inspection_default_routes output as JSON: %s", err)
 	}
 
 	// Check the number of keys in `inspection_default_routes`
-	liveDataRoutes, _ := inspectionDefaultRoutes["live_data"].(map[string]interface{})
-	nonLiveDataRoutes, _ := inspectionDefaultRoutes["non_live_data"].(map[string]interface{})
+	liveDataRoutes, _ := defaultRoutes["live_data"].(map[string]interface{})
+	nonLiveDataRoutes, _ := defaultRoutes["non_live_data"].(map[string]interface{})
 
 	assert.Len(t, liveDataRoutes, 9)
 	assert.Len(t, nonLiveDataRoutes, 9)
