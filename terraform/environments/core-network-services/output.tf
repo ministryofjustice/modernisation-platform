bcb205d8a666ccba143b752fb838c558d9c32cb6
David Sibley <david.sibley@digital.justice.gov.uk>
2023-05-19 18:59:16 +0100

    removed references to vpc_hub, removed vpc_hub module, pruned tests

--- terraform/environments/core-network-services/output.tf
+++ terraform/environments/core-network-services/output.tf
@@ -23,36 +23,6 @@ output "transit_gateway_ram_share" {
   value = aws_ram_resource_share.transit-gateway.name
 }
 
-output "non_live_data_private_route_tables" {
-  value = module.vpc_hub["non_live_data"].private_route_tables
-}
-
-output "public_route_tables" {
-  value = {
-    for key, value in local.networking :
-    key => module.vpc_hub[key].public_route_tables.tags["Name"]
-  }
-}
-
-output "live_data_private_route_tables" {
-  value = module.vpc_hub["live_data"].private_route_tables
-}
-
-output "public_igw_route" {
-  value = {
-    for key, value in local.networking :
-    key => module.vpc_hub[key].public_igw_route.destination_cidr_block
-  }
-}
-
-output "non_tgw_subnet_ids" {
-  value = length(module.vpc_hub["non_live_data"].non_tgw_subnet_ids)
-}
-
-output "tgw_subnet_ids" {
-  value = length(module.vpc_hub["non_live_data"].tgw_subnet_ids)
-}
-
 ###
 
 output "inspection_tgw_subnets" {
