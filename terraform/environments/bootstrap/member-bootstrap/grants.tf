bf13dce41f43712e7cc7b357be914173cc768a1d
David Sibley <david.sibley@digital.justice.gov.uk>
2023-06-01 21:53:15 +0100

    solved problem of improper data call, but have to figure out how to set both providers to reference us-east-1 as location for licensemanager grants

--- terraform/environments/bootstrap/member-bootstrap/grants.tf
+++ terraform/environments/bootstrap/member-bootstrap/grants.tf
@@ -6,5 +6,5 @@ module "grant-rhel-els" {
   }
   account_to_grant       = local.environment_management.account_ids[terraform.workspace]
   destination_grant_name = "RHEL-6-ELS"
-  source_license_arn     = "arn:aws:license-manager::294406891311:license:l-d82b05a0dc724e79a1d164704144d38b"
+  source_license_sku     = "02e38931-bf68-46a4-925d-5fdf598c92d2"
 }
--- terraform/modules/license-manager/variables.tf
+++ terraform/modules/license-manager/variables.tf
@@ -20,13 +20,7 @@ variable "destination_grant_name" {
   type = string
 }
 
-variable "source_license_arn" {
-  description = "ARN of source license - eg. \"arn:aws:license-manager::$account-id:license:$license-id\"."
-  type        = string
-}
-
-variable "source_grant_home_region" {
-  description = "Home AWS region of source grant - eg. \"us-east-1\"."
-  default     = "us-east-1"
+variable "source_license_sku" {
+  description = "SKU of source license visible through CLI tools query"
   type        = string
 }
