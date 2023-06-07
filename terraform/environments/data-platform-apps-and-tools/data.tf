2327ad51b9690b462996faf73aeac31c1db92877
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-06-07 05:53:13 +0000

    Removed already declared aws_caller_identity Removed SES, will create in environments repository

--- terraform/environments/data-platform-apps-and-tools/data.tf
+++ terraform/environments/data-platform-apps-and-tools/data.tf
@@ -5,6 +5,4 @@ data "http" "environments_file" {
 
 data "aws_region" "current" {}
 
-data "aws_caller_identity" "current" {}
-
 data "aws_availability_zones" "available" {}
--- terraform/environments/data-platform-apps-and-tools/ses-domain-identities.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_ses_domain_identity" "main" {
-  domain = local.environment_configuration.ses_domain_identity
-}
