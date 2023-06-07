79d06b09fa815ad06ee1995f60a2951e595de300
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-06-07 07:12:30 +0000

    Bumped CIDR Added SMTP VPC endpoint

--- terraform/environments/data-platform-apps-and-tools/environment-configuration.tf
+++ terraform/environments/data-platform-apps-and-tools/environment-configuration.tf
@@ -3,9 +3,9 @@ locals {
   environment_configurations = {
     development = {
       ses_domain_identity        = "apps-tools.${local.environment}.data-platform.service.justice.gov.uk"
-      vpc_cidr                   = "10.27.128.0/24"
-      vpc_private_subnets        = ["10.26.128.0/26", "10.26.128.16/26", "10.26.128.32/26"]
-      vpc_public_subnets         = ["10.26.128.48/26", "10.26.128.64/26", "10.26.128.80/26"]
+      vpc_cidr                   = "10.27.128.0/23"
+      vpc_private_subnets        = ["10.26.128.0/26", "10.26.128.64/26", "10.26.128.128/26"]
+      vpc_public_subnets         = ["10.26.129.0/26", "10.26.129.64/26", "10.26.129.128/26"]
       vpc_enable_nat_gateway     = true
       vpc_one_nat_gateway_per_az = false
     }
