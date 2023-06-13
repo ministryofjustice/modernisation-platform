112c0c2540b0d1fdce68cd4fd23a2cb9335c1688
David Sibley <david.sibley@digital.justice.gov.uk>
2023-06-13 14:32:03 +0100

    added rules for laa-appstream to mp-laa VPCs

--- terraform/environments/core-network-services/cidr-ranges.tf
+++ terraform/environments/core-network-services/cidr-ranges.tf
@@ -53,7 +53,6 @@ locals {
     laa-lz-production              = "10.205.0.0/20"
     laa-lz-shared-services-nonprod = "10.200.0.0/20"
     laa-lz-shared-services-prod    = "10.200.16.0/20"
-    laa-appstream-subnets          = "10.200.32.0/23"
     laa-appstream-vpc              = "10.200.32.0/19"
     laa-appstream-vpc_additional   = "10.200.68.0/22"
 
