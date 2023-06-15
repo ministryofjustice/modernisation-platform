aa0cae90d54f59846d72c6a82ecae35e18b37a2c
Matt <38562764+LavMatt@users.noreply.github.com>
2023-06-15 09:37:06 +0100

    close bracket

--- terraform/environments/core-shared-services/ecr_repos.tf
+++ terraform/environments/core-shared-services/ecr_repos.tf
@@ -191,7 +191,7 @@ module "data_platform_athena_load_ecr_repo" {
     local.environment_management.account_ids["data-platform-development"],
   ]
 
-  enable_retrieval_policy_for_lambdas = ["arn:aws:lambda:eu-west-2:${local.environment_management.account_ids["data-platform-development"]}:function:data_product_athena_load*",
+  enable_retrieval_policy_for_lambdas = ["arn:aws:lambda:eu-west-2:${local.environment_management.account_ids["data-platform-development"]}:function:data_product_athena_load*"]
   
   # Tags
   tags_common = local.tags
