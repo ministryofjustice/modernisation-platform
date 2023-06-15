df29e72f788eed980ea58bd20c73efa334525a63
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-06-15 16:53:20 +0100

    Fix a very silly mistake (#4276)

--- terraform/environments/data-platform-apps-and-tools/environment-configuration.tf
+++ terraform/environments/data-platform-apps-and-tools/environment-configuration.tf
@@ -4,8 +4,8 @@ locals {
     development = {
       ses_domain_identity        = "apps-tools.${local.environment}.data-platform.service.justice.gov.uk"
       vpc_cidr                   = "10.27.128.0/23"
-      vpc_private_subnets        = ["10.26.128.0/26", "10.26.128.64/26", "10.26.128.128/26"]
-      vpc_public_subnets         = ["10.26.129.0/26", "10.26.129.64/26", "10.26.129.128/26"]
+      vpc_private_subnets        = ["10.27.128.0/26", "10.27.128.64/26", "10.27.128.128/26"]
+      vpc_public_subnets         = ["10.27.129.0/26", "10.27.129.64/26", "10.27.129.128/26"]
       vpc_enable_nat_gateway     = true
       vpc_one_nat_gateway_per_az = false
     }
