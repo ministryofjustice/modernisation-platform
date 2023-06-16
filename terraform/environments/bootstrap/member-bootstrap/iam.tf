e7662545136cec30c6cf0cd1fa8b85663d0b8421
ep-93 <109581241+ep-93@users.noreply.github.com>
2023-06-16 15:15:18 +0100

    Update iam.tf

--- terraform/environments/bootstrap/member-bootstrap/iam.tf
+++ terraform/environments/bootstrap/member-bootstrap/iam.tf
@@ -37,6 +37,7 @@ data "aws_iam_policy_document" "member-access" {
       "backup-storage:MountCapsule",
       "cloudfront:*",
       "cloudwatch:*",
+      "cloudtrail:*",
       "codebuild:*",
       "codedeploy:*",
       "codepipeline:*",
