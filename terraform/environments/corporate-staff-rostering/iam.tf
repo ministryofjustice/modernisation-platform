992f8d3c26e2a149d078070ac152ceb971437956
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2023-06-19 16:19:56 +0100

    Added new mgm user

---
+++ terraform/environments/corporate-staff-rostering/iam.tf
@@ -0,0 +1,17 @@
+# Create user for MGN
+
+#tfsec:ignore:aws-iam-no-user-attached-policies
+#tfsec:ignore:AWS273
+resource "aws_iam_user" "mgn_user" {
+  #checkov:skip=CKV_AWS_273: "Skipping as tfsec check is also set to ignore"
+  name = "MGN-Test"
+  tags = local.tags
+}
+#tfsec:ignore:aws-iam-no-user-attached-policies
+resource "aws_iam_user_policy_attachment" "mgn_attach_policy" {
+  #tfsec:ignore:aws-iam-no-user-attached-policies
+  #checkov:skip=CKV_AWS_40: "Skipping as tfsec check is also ignored"
+  user       = aws_iam_user.mgn_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSApplicationMigrationAgentInstallationPolicy"
+}
+
