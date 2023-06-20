108ca91fee91007916d42e8372bb8df9155c0f2a
Aaron Robinson <aaron.robinson@digital.justice.gov.uk>
2023-06-20 12:11:49 +0100

    core-security v5 update

--- terraform/modules/core-monitoring/main.tf
+++ terraform/modules/core-monitoring/main.tf
@@ -1,5 +1,5 @@
 module "pagerduty_core_alerts" {
-  source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=v1.0.0"
+  source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=v2.0.0"
   sns_topics                = ["config", "securityhub-alarms", "cloudtrail"]
   pagerduty_integration_key = var.pagerduty_integration_keys["core_alerts_cloudwatch"]
 }
