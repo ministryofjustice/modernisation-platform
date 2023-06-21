712707197f203c8b4af0afe7a617d49f833cc5ec
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2023-06-21 13:58:34 +0100

    Missed upgrades for v 5

--- terraform/environments/environments.tf
+++ terraform/environments/environments.tf
@@ -1,5 +1,5 @@
 module "environments" {
-  source                             = "github.com/ministryofjustice/modernisation-platform-terraform-environments?ref=v5.0.3"
+  source                             = "github.com/ministryofjustice/modernisation-platform-terraform-environments?ref=v6.0.0"
   environment_directory              = "../../environments"
   environment_parent_organisation_id = local.environment_management.modernisation_platform_organisation_unit_id
   environment_prefix                 = "modernisation-platform"
