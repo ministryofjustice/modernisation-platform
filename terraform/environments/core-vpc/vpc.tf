96b2323930db705bfadbe67e42571278cf7adbea
dms1981 <david.sibley@digital.justice.gov.uk>
2023-06-22 11:14:12 +0100

    Revert "allow VPCs to be associated with private zones (#4383)" (#4387)
    This reverts commit 9247d04d232792ae5743f036b3350be6c3f3b10e.

--- terraform/environments/core-vpc/vpc.tf
+++ terraform/environments/core-vpc/vpc.tf
@@ -233,7 +233,6 @@ resource "aws_iam_role_policy" "member-delegation" {
       {
         "Effect" : "Allow",
         "Action" : [
-          "route53:AssociateVPCWithHostedZone",
           "route53:List*",
           "route53:Get*",
           "route53resolver:CreateResolverEndpoint",
