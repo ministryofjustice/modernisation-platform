9b6e4cbcfbcfe12ed09a45eef0d911aa370b6444
David Elliott <david.elliott@digital.justice.gov.uk>
2023-06-26 11:02:24 +0100

    Switch to auto for minor versions

--- terraform/github/modules/repository/versions.tf
+++ terraform/github/modules/repository/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.0"
   required_providers {
     github = {
-      version = "5.28.1"
+      version = "~> 5.0"
       source  = "integrations/github"
     }
   }
