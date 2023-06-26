9d1bfb7f154f94bb113e0aeeb6d2fbb7dd1ede5d
David Sibley <david.sibley@digital.justice.gov.uk>
2023-06-26 09:40:31 +0100

    added key alias and policy based off of AWS example

--- terraform/environments/core-network-services/secrets.tf
+++ terraform/environments/core-network-services/secrets.tf
@@ -26,5 +26,43 @@ resource "aws_kms_key" "environment_logging" {
   description             = "environment-logging"
   enable_key_rotation     = true
   deletion_window_in_days = 30
+  policy                  = data.aws_iam_policy_document.environment_logging.json
 }
 
+resource "aws_kms_alias" "environment_logging" {
+  name          = "alias/environment-logging"
+  target_key_id = aws_kms_key.environment_logging.key_id
+}
+
+data "aws_iam_policy_document" "environment_logging" {
+  statement {
+    sid    = "Enable IAM User Permissions"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "Allow CloudWatch access"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["logs.eu-west-2.amazonaws.com"]
+    }
+    actions = [
+      "kms:Encrypt*",
+      "kms:Decrypt*",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:Describe*"
+    ]
+    condition {
+      test     = "ArnLike"
+      values   = ["arn:aws:logs:region:${data.aws_caller_identity.current.account_id}:*"]
+      variable = "kms:EncryptionContext:aws:logs:arn"
+    }
+  }
+}
