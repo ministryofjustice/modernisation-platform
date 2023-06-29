25632a8628f004178417f3070b459d8fcf3e98cf
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2023-06-29 09:42:38 +0100

    Typo

--- terraform/modernisation-platform-account/s3.tf
+++ terraform/modernisation-platform-account/s3.tf
@@ -87,7 +87,7 @@ resource "aws_kms_alias" "s3_state_bucket_eu-west-1_replication" {
 }
 
 module "state-bucket-s3-replication-role" {
-  source             = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=3b8a2945c1d266cc0ec2b21edb7f186b6574bda7"
+  source             = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket-replication-role?ref=3b8a2945c1d266cc0ec2b21edb7f186b6574bda7"
   buckets            = [module.state-bucket.bucket.arn]
   replication_bucket = "modernisation-platform-terraform-state-replication"
   suffix_name        = "-terraform-state"
