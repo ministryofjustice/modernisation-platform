f1c86655615fb47371ae84fddcc704c76b82300d
ep-93 <109581241+ep-93@users.noreply.github.com>
2023-07-03 08:21:05 +0100

    Update environments.tf

--- terraform/environments/environments.tf
+++ terraform/environments/environments.tf
@@ -1,5 +1,5 @@
 module "environments" {
-  source                             = "github.com/ministryofjustice/modernisation-platform-terraform-environments?ref=v6.0.0"
+  source                             = "github.com/ministryofjustice/modernisation-platform-terraform-environments?ref=b17ca8d6c23a2c22d2efc6c5116a1328492c32bd"
   environment_directory              = "../../environments"
   environment_parent_organisation_id = local.environment_management.modernisation_platform_organisation_unit_id
   environment_prefix                 = "modernisation-platform"
