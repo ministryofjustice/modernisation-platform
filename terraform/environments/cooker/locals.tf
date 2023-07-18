5f3ad40122635f2f3078274df87af6aa828e7379
David Sibley <david.sibley@digital.justice.gov.uk>
2023-07-18 12:26:36 +0100

    added local to cooker to trigger git diff in workflow

--- terraform/environments/cooker/locals.tf
+++ terraform/environments/cooker/locals.tf
@@ -27,6 +27,7 @@ locals {
 
   is_live       = [substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production" || substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-preproduction" ? "live" : "non-live"]
   provider_name = "core-vpc-${local.environment}"
+  is_cooker     = true
 
   # environment specfic variables
   # example usage:
