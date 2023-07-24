6c34533986fe85a97cb0d5c185b5ea3ed8e171fb
mark roberts <mark.roberts@digital.justice.gov.uk>
2023-07-24 22:34:06 +0100

    added back the providers to the module

--- terraform/modules/private-dns-zone-extend/main.tf
+++ terraform/modules/private-dns-zone-extend/main.tf
@@ -1,9 +1,11 @@
 data "aws_route53_zone" "private" {
+ provider = aws.core-network-services
  name = var.business_unit_name
  private_zone = true
 }
 
 resource "aws_route53_vpc_association_authorization" "private_zone_vpc_auth" {
+provider = aws.core-network-services
 for_each = data.aws_route53_zone.private.zone_id
 vpc_id   = var.vpc_id
 zone_id  = each.key.value
