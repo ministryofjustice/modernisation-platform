8fc96e5a07ff04e1dc56fc7ae95b830296fe32c8
mark roberts <mark.roberts@digital.justice.gov.uk>
2023-07-24 13:55:32 +0100

    module code update

--- terraform/modules/private-dns-zone-extend/main.tf
+++ terraform/modules/private-dns-zone-extend/main.tf
@@ -1,18 +1,18 @@
 
 data "aws_route53_zone" "private" {
- provider = core-network-services
+ provider = aws.core-network-services
  name = var.business_unit_name
 }
 
 
 resource "aws_route53_vpc_association_authorization" "vpcauth" {
-provider = core-network-services
+provider = aws.core-network-services
 vpc_id   = var.vpc_id
 zone_id  = data.aws_route53_zone.private.zone_id
 }
 
 resource "aws_route53_zone_association" "extend"  {
-provider = core-network-services
+provider = aws.core-vpc
 vpc_id   = aws_route53_vpc_association_authorization.vpcauth.zone_id
 zone_id  = aws_route53_vpc_association_authorization.vpcauth.vpc_id
 }
