cda99c7e2cbce6964313f3d56310ae3b010e5b7f
mark roberts <mark.roberts@digital.justice.gov.uk>
2023-07-24 22:36:47 +0100

    provider update on vpc

--- terraform/environments/core-vpc/vpc.tf
+++ terraform/environments/core-vpc/vpc.tf
@@ -197,9 +197,8 @@ locals {
 
 
 module "private_dns_zone_extend" {
-  providers = {
-    aws = aws.core-network-services
-  }
+  providers = [aws.core-network-services]
+  
   for_each = local.vpcs[terraform.workspace]
   source = "../../modules/private-dns-zone-extend"
   name = each.key
