f4e86e2b91f49a6e7a14ef7a3dd78bd1e8f43124
mark roberts <mark.roberts@digital.justice.gov.uk>
2023-07-25 09:51:17 +0100

    correcting posible fault in the module

--- terraform/modules/private-dns-zone-extend/main.tf
+++ terraform/modules/private-dns-zone-extend/main.tf
@@ -8,7 +8,7 @@ resource "aws_route53_vpc_association_authorization" "private_zone_vpc_auth" {
 provider = aws.core-network-services
 for_each = data.aws_route53_zone.private.zone_id
 vpc_id   = var.vpc_id
-zone_id  = each.key.value
+zone_id  = each.key
 }
 
 resource "aws_route53_zone_association" "private_zone_assoc"  {
