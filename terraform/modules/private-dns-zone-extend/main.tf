8d870b6f83a0be8f4778ffe094d23f232fccc673
mark roberts <mark.roberts@digital.justice.gov.uk>
2023-07-25 10:04:44 +0100

    plan worked but nothing was changed data added

--- terraform/modules/private-dns-zone-extend/main.tf
+++ terraform/modules/private-dns-zone-extend/main.tf
@@ -1,14 +1,14 @@
-# data "aws_route53_zone" "private" {
-#  provider = aws.core-network-services
-#  name = var.business_unit_name
-#  private_zone = true
-# }
+data "aws_route53_zone" "private" {
+ provider = aws.core-network-services
+ name = var.business_unit_name
+ private_zone = true
+}
 
 resource "aws_route53_vpc_association_authorization" "private_zone_vpc_auth" {
 provider = aws.core-network-services
 #for_each = data.aws_route53_zone.private.zone_id
 vpc_id   = var.vpc_id
-zone_id  = var.business_unit_name
+zone_id  = data.aws_route53_zone.private.zone_id
 }
 
 resource "aws_route53_zone_association" "private_zone_assoc"  {
