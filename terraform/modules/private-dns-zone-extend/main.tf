c6671a930b58c66499ec0813871e624d091c404a
mark roberts <mark.roberts@digital.justice.gov.uk>
2023-07-25 09:59:46 +0100

    surgested change to the module

--- terraform/modules/private-dns-zone-extend/main.tf
+++ terraform/modules/private-dns-zone-extend/main.tf
@@ -1,17 +1,17 @@
-data "aws_route53_zone" "private" {
- provider = aws.core-network-services
- name = var.business_unit_name
- private_zone = true
-}
+# data "aws_route53_zone" "private" {
+#  provider = aws.core-network-services
+#  name = var.business_unit_name
+#  private_zone = true
+# }
 
 resource "aws_route53_vpc_association_authorization" "private_zone_vpc_auth" {
 provider = aws.core-network-services
-for_each = data.aws_route53_zone.private.zone_id
+#for_each = data.aws_route53_zone.private.zone_id
 vpc_id   = var.vpc_id
-zone_id  = each.key
+zone_id  = var.business_unit_name
 }
 
 resource "aws_route53_zone_association" "private_zone_assoc"  {
-vpc_id   = aws_route53_vpc_association_authorization.private_zone_vpc_auth.zone_id[each.key]
-zone_id  = aws_route53_vpc_association_authorization.private_zone_vpc_auth.vpc_id[each.key]
+vpc_id   = aws_route53_vpc_association_authorization.private_zone_vpc_auth.zone_id
+zone_id  = aws_route53_vpc_association_authorization.private_zone_vpc_auth.vpc_id
 }
