eca627c8972980e4b58a91b36327ea3eb78404a1
mark roberts <mark.roberts@digital.justice.gov.uk>
2023-07-27 14:23:47 +0100

    added output to see what is being passed

--- terraform/modules/private-dns-zone-extend/main.tf
+++ terraform/modules/private-dns-zone-extend/main.tf
@@ -14,3 +14,8 @@ resource "aws_route53_zone_association" "private_zone_assoc"  {
 vpc_id   = aws_route53_vpc_association_authorization.private_zone_vpc_auth.zone_id
 zone_id  = aws_route53_vpc_association_authorization.private_zone_vpc_auth.vpc_id
 }
+
+output "zonenametest" {
+    value = data.aws_route53_zone.private.zone_id
+  
+}
