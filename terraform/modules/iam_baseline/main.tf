9617e4a790d68b009cab071d0923863800dc3bd4
George Taylor <george.taylor@digital.justice.gov.uk>
2023-07-28 12:06:10 +0100

    Add policy for SSM usage under cicd-member-user role (#4670)
    * ssm policy
    
    * attach policy
    
    * wildcard suffix match
    
    * reorder
    
    ---------
    
    Co-authored-by: dms1981 <david.sibley@digital.justice.gov.uk>

--- terraform/modules/iam_baseline/main.tf
+++ terraform/modules/iam_baseline/main.tf
@@ -106,9 +106,57 @@ resource "aws_iam_policy" "policy" {
   })
 }
 
+resource "aws_iam_policy" "ssm_policy" {
+  name        = "cicd-member-ssm-policy"
+  description = "IAM Policy for CICD member user"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ssm:StartSession",
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:eu-west-2:${data.aws_caller_identity.current.account_id}:instance/*}"
+      },
+    ]
+    Statement = [
+      {
+        Action = [
+          "ssm:DescribeSessions",
+          "ssm:GetConnectionStatus",
+          "ssm:DescribeInstanceInformation",
+          "ssm:DescribeInstanceProperties",
+          "ssm:GetParameter",
+          "ssm:SendCommand",
+          "ec2:DescribeInstances"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+    Statement = [
+      {
+        Action = [
+          "ssm:TerminateSession",
+          "ssm:ResumeSession"
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:ssm:eu-west-2:${data.aws_caller_identity.current.account_id}:session/${aws_iam_user.cicd_member_user.name}*"
+      },
+    ]
+  })
+}
+
+
 resource "aws_iam_group_policy_attachment" "aws_cicd_member_attach" {
   group      = aws_iam_group.cicd_member_group.name
   policy_arn = aws_iam_policy.policy.arn
+}
+
+resource "aws_iam_group_policy_attachment" "aws_cicd_member_attach_ssm" {
+  group      = aws_iam_group.cicd_member_group.name
+  policy_arn = aws_iam_policy.ssm_policy.arn
 }
 
 resource "aws_iam_group_policy_attachment" "aws_ec2_readonly_attach" {
