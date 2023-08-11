168cc6b2637c3826a2285a983b2bbeabcc61e5fb
David Sibley <david.sibley@digital.justice.gov.uk>
2023-08-11 08:47:37 +0100

    adjusted secrets for pra-register to fix SCA errors

--- terraform/environments/pra-register/data.tf
+++ terraform/environments/pra-register/data.tf
@@ -2,3 +2,7 @@
 data "http" "environments_file" {
   url = "https://raw.githubusercontent.com/ministryofjustice/modernisation-platform/main/environments/${local.application_name}.json"
 }
+
+data "aws_kms_key" "general_shared" {
+  key_id = "arn:aws:kms:eu-west-2:${local.environment_management.account_ids["core-shared-services-production"]}:alias/general-${var.networking[0].business-unit}"
+}
