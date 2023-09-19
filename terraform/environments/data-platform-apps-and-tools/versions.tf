2c5bfa56204bdd0c7f767791bd31aed1e29334d0
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-09-19 18:25:53 +0000

    add cloudinit and kubernetes
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/data-platform-apps-and-tools/versions.tf
+++ terraform/environments/data-platform-apps-and-tools/versions.tf
@@ -4,9 +4,17 @@ terraform {
       version = "~> 5.0"
       source  = "hashicorp/aws"
     }
+    cloudinit = {
+      version = "~> 2.0"
+      source  = "hashicorp/cloudinit"
+    }
     http = {
       version = "~> 3.0"
       source  = "hashicorp/http"
+    }
+    kubernetes = {
+      version = "~> 2.0"
+      source  = "hashicorp/kubernetes"
     }
     time = {
       version = "~> 0.9"
