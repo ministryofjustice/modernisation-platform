24af8b1261487a0e2a404f629bc4422daca69561
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-09-19 17:01:36 +0000

    Comment out IRSA roles
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/data-platform-apps-and-tools/eks-iam-roles.tf
+++ terraform/environments/data-platform-apps-and-tools/eks-iam-roles.tf
@@ -1,37 +1,37 @@
-module "vpc_cni_role" {
-  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+# module "vpc_cni_role" {
+#   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
 
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "~> 5.0"
+#   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+#   version = "~> 5.0"
 
-  role_name_prefix      = "vpc-cni"
-  attach_vpc_cni_policy = true
+#   role_name_prefix      = "vpc-cni"
+#   attach_vpc_cni_policy = true
 
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:aws-node"]
-    }
-  }
+#   oidc_providers = {
+#     main = {
+#       provider_arn               = module.eks.oidc_provider_arn
+#       namespace_service_accounts = ["kube-system:aws-node"]
+#     }
+#   }
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 
-module "ebs_csi_driver_role" {
-  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+# module "ebs_csi_driver_role" {
+#   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
 
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "~> 5.0"
+#   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+#   version = "~> 5.0"
 
-  role_name_prefix      = "ebs-csi-driver"
-  attach_ebs_csi_policy = true
+#   role_name_prefix      = "ebs-csi-driver"
+#   attach_ebs_csi_policy = true
 
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
-    }
-  }
+#   oidc_providers = {
+#     main = {
+#       provider_arn               = module.eks.oidc_provider_arn
+#       namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+#     }
+#   }
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
