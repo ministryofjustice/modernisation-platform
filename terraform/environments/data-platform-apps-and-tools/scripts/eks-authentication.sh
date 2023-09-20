640de2629184d6bfb1f836536d0e23f1d076fccb
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-09-20 10:19:37 +0000

    Make AWS_ROLE again but pass a default
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/data-platform-apps-and-tools/scripts/eks-authentication.sh
+++ terraform/environments/data-platform-apps-and-tools/scripts/eks-authentication.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-# This scripts exists because the Terraform Lubernetes provider does not pass assumed credentials from the default AWS provider
-
+# This scripts exists because the Terraform Kubernetes provider does not pass assumed credentials from the default AWS provider
 
 AWS_ACCOUNT_ID=${1}
 EKS_CLUSTER_NAME=${2}
-AWS_ROLE="ModernisationPlatformAccess"
+AWS_ROLE=${3:-ModernisationPlatformAccess}
 
 assumeRole=$(aws sts assume-role \
   --role-arn arn:aws:iam::${AWS_ACCOUNT_ID}:role/${AWS_ROLE} \
