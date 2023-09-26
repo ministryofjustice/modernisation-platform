b620dccec6657f7082a5c435604355772b59bfa4
dms1981 <david.sibley@digital.justice.gov.uk>
2023-09-26 15:08:29 +0100

    Refactor core-shared-services VPC endpoints (#5100)
    * started by cleaning up how things are referenced locally, removing unneccsary locals
    
    * updated aws_eip reference in vpc module
    
    * first pass on creating extra endpoints across VPCs
    
    * refactored some more to create security groups in both VPCs
    
    * refactored output. will probably need to refactor tests
    
    * refactored to use a consolidated map of vpc names and endpoint names

--- terraform/environments/core-shared-services/output.tf
+++ terraform/environments/core-shared-services/output.tf
@@ -57,9 +57,9 @@ output "tgw_subnet_ids" {
 }
 
 output "image_builder_security_group_id" {
-  value = aws_security_group.image_builder_security_group.id
+  value = { for k, v in aws_security_group.image_builder_security_group : k => v.id }
 }
 
 output "non_live_private_subnet_ids" {
-  value = local.non_live_private_subnet_ids
+  value = module.vpc["non_live_data"].non_tgw_subnet_ids_map["private"]
 }
