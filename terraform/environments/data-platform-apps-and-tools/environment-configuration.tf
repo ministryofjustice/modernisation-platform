87107a6f7d3b743bfe946427b5aa59764f95a307
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-09-26 15:39:55 +0000

    test
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/data-platform-apps-and-tools/environment-configuration.tf
+++ terraform/environments/data-platform-apps-and-tools/environment-configuration.tf
@@ -20,16 +20,16 @@ locals {
         "webserver.warn_deployment_exposure" = 0
       }
       airflow_weekly_maintenance_window_start = "SAT:00:00"
-      target_eks_cluster_arn                  = module.eks.cluster_arn
-      vpc_cidr                                = "10.26.128.0/21"
-      vpc_private_subnets                     = ["10.26.130.0/23", "10.26.132.0/23", "10.26.134.0/23"]
-      vpc_public_subnets                      = ["10.26.128.0/27", "10.26.128.32/27", "10.26.128.64/27"]
-      vpc_database_subnets                    = ["10.26.128.96/27", "10.26.128.128/27", "10.26.128.160/27"]
-      vpc_enable_nat_gateway                  = true
-      vpc_one_nat_gateway_per_az              = false
-      route53_zone                            = "apps-tools.development.data-platform.service.justice.gov.uk"
-      ses_domain_identity                     = "apps-tools.development.data-platform.service.justice.gov.uk"
-      eks_sso_access_role                     = "modernisation-platform-sandbox"
+      # target_eks_cluster_arn                  = module.eks.cluster_arn
+      vpc_cidr                   = "10.26.128.0/21"
+      vpc_private_subnets        = ["10.26.130.0/23", "10.26.132.0/23", "10.26.134.0/23"]
+      vpc_public_subnets         = ["10.26.128.0/27", "10.26.128.32/27", "10.26.128.64/27"]
+      vpc_database_subnets       = ["10.26.128.96/27", "10.26.128.128/27", "10.26.128.160/27"]
+      vpc_enable_nat_gateway     = true
+      vpc_one_nat_gateway_per_az = false
+      route53_zone               = "apps-tools.development.data-platform.service.justice.gov.uk"
+      ses_domain_identity        = "apps-tools.development.data-platform.service.justice.gov.uk"
+      eks_sso_access_role        = "modernisation-platform-sandbox"
     }
     production = {
       airflow_s3_bucket         = "moj-data-platform-airflow-production20230908140747954800000002"
@@ -42,16 +42,16 @@ locals {
         "webserver.warn_deployment_exposure" = 0
       }
       airflow_weekly_maintenance_window_start = "SAT:00:00"
-      target_eks_cluster_arn                  = "arn:aws:eks:eu-west-1:525294151996:cluster/development-aWrhyc0m"
-      vpc_cidr                                = "10.27.128.0/21"
-      vpc_private_subnets                     = ["10.27.130.0/23", "10.27.132.0/23", "10.27.134.0/23"]
-      vpc_public_subnets                      = ["10.27.128.0/27", "10.27.128.32/27", "10.27.128.64/27"]
-      vpc_database_subnets                    = ["10.27.128.96/27", "10.27.128.128/27", "10.27.128.160/27"]
-      vpc_enable_nat_gateway                  = true
-      vpc_one_nat_gateway_per_az              = false
-      route53_zone                            = "apps-tools.data-platform.service.justice.gov.uk"
-      ses_domain_identity                     = "apps-tools.data-platform.service.justice.gov.uk"
-      eks_sso_access_role                     = "modernisation-platform-developer"
+      # target_eks_cluster_arn                  = "arn:aws:eks:eu-west-1:525294151996:cluster/development-aWrhyc0m"
+      vpc_cidr                   = "10.27.128.0/21"
+      vpc_private_subnets        = ["10.27.130.0/23", "10.27.132.0/23", "10.27.134.0/23"]
+      vpc_public_subnets         = ["10.27.128.0/27", "10.27.128.32/27", "10.27.128.64/27"]
+      vpc_database_subnets       = ["10.27.128.96/27", "10.27.128.128/27", "10.27.128.160/27"]
+      vpc_enable_nat_gateway     = true
+      vpc_one_nat_gateway_per_az = false
+      route53_zone               = "apps-tools.data-platform.service.justice.gov.uk"
+      ses_domain_identity        = "apps-tools.data-platform.service.justice.gov.uk"
+      eks_sso_access_role        = "modernisation-platform-developer"
     }
   }
 }
