042f7bfa104188db9e65bf3cb1e4a2f06633adeb
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-09-27 16:50:35 +0000

    remove old bucket
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/data-platform-apps-and-tools/s3-buckets.tf
+++ terraform/environments/data-platform-apps-and-tools/s3-buckets.tf
@@ -1,16 +1,3 @@
-// TODO: clean up this resource
-module "eks_backup_bucket" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v7.0.0"
-
-  providers = {
-    aws.bucket-replication = aws
-  }
-
-  bucket_prefix = "moj-dp-${local.environment}-velero"
-
-  tags = local.tags
-}
-
 module "velero_s3_bucket" {
   source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v7.0.0"
 
