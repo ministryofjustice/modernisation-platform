c3c51257add5ecd2341ad91ed7307c868bb2c892
Gary H <26419401+Gary-H9@users.noreply.github.com>
2023-09-29 15:51:38 +0100

    :wrench: Add persistent volumes etc

---
+++ terraform/environments/data-platform-apps-and-tools/kubernetes-pv.tf
@@ -0,0 +1,45 @@
+resource "kubernetes_persistent_volume" "openmetadata_airflow_dags" {
+  metadata {
+    name = "openmetadata-dependencies-dags-pv"
+    labels = {
+      app = "openmetadata-airflow-dags"
+    }
+  }
+  spec {
+    capacity = {
+      storage = "10Gi"
+    }
+    access_modes                     = ["ReadWriteMany"]
+    persistent_volume_reclaim_policy = "Retain"
+    storage_class_name               = "efs-sc"
+    persistent_volume_source {
+      csi {
+        driver        = "efs.csi.aws.com"
+        volume_handle = "${module.efs.id}::${module.efs.access_points["airflow_dags"].id}"
+      }
+    }
+  }
+}
+
+resource "kubernetes_persistent_volume" "openmetadata_airflow_logs" {
+  metadata {
+    name = "openmetadata-dependencies-logs-pv"
+    labels = {
+      app = "openmetadata-airflow-logs"
+    }
+  }
+  spec {
+    capacity = {
+      storage = "50Gi"
+    }
+    access_modes                     = ["ReadWriteMany"]
+    persistent_volume_reclaim_policy = "Retain"
+    storage_class_name               = "efs-sc"
+    persistent_volume_source {
+      csi {
+        driver        = "efs.csi.aws.com"
+        volume_handle = "${module.efs.id}::${module.efs.access_points["airflow_logs"].id}"
+      }
+    }
+  }
+}
