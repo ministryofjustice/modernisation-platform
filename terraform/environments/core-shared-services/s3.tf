958ae072f3c1c6b98c846a73b7913efcb298e4d2
George Taylor <george.taylor@digital.justice.gov.uk>
2023-10-02 10:33:55 +0100

    delius artefact s3

--- terraform/environments/core-shared-services/s3.tf
+++ terraform/environments/core-shared-services/s3.tf
@@ -163,3 +163,80 @@ data "aws_iam_policy_document" "software_bucket_policy" {
   }
 
 }
+
+module "s3-delius-artefact-bucket" {
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=8688bc15a08fbf5a4f4eef9b7433c5a417df8df1" # v7.0.0
+
+  providers = {
+    aws.bucket-replication = aws.bucket-replication
+  }
+  bucket_prefix       = "delius-artefact"
+  bucket_policy       = [data.aws_iam_policy_document.delius_artefact_policy.json]
+  replication_enabled = false
+  versioning_enabled  = true
+  force_destroy       = false
+  lifecycle_rule = [
+    {
+      id      = "main"
+      enabled = "Enabled"
+      prefix  = ""
+
+      tags = {
+        rule      = "log"
+        autoclean = "true"
+      }
+
+      transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      expiration = {
+        days = 730
+      }
+
+      noncurrent_version_transition = [
+        {
+          days          = 90
+          storage_class = "STANDARD_IA"
+          }, {
+          days          = 365
+          storage_class = "GLACIER"
+        }
+      ]
+
+      noncurrent_version_expiration = {
+        days = 730
+      }
+    }
+  ]
+
+  tags = local.tags
+}
+
+
+data "aws_iam_policy_document" "delius_artefact_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+      "s3:ListBucket"
+    ]
+
+    resources = [
+      "${module.s3-delius-artefact-bucket.bucket.arn}/*",
+      module.s3-delius-artefact-bucket.bucket.arn
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::*:user/delius-jitbit-development-s3_user"]
+    }
+  }
+}
