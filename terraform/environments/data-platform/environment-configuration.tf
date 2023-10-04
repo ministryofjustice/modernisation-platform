4a09dd77536d883a0ce64107b9860f3e874eeb80
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-10-04 14:25:57 +0000

    Adding test and preprod
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/data-platform/environment-configuration.tf
+++ terraform/environments/data-platform/environment-configuration.tf
@@ -5,6 +5,16 @@ locals {
       apps_tools_eks_oidc_url                  = "https://oidc.eks.eu-west-2.amazonaws.com/id/BEE86BED6494692D4ED31C2ED2319E13"
       apps_tools_openmetadata_airflow_role_arn = "arn:aws:iam::335889174965:role/openmetadata-airflow20231002155028097300000001"
     }
+    test = {
+      // TODO: Replace with test values, keeping these as a placeholder
+      apps_tools_eks_oidc_url                  = "https://oidc.eks.eu-west-2.amazonaws.com/id/BEE86BED6494692D4ED31C2ED2319E13"
+      apps_tools_openmetadata_airflow_role_arn = "arn:aws:iam::335889174965:role/openmetadata-airflow20231002155028097300000001"
+    }
+    preproduction = {
+      // TODO: Replace with preproduction values, keeping these as a placeholder
+      apps_tools_eks_oidc_url                  = "https://oidc.eks.eu-west-2.amazonaws.com/id/BEE86BED6494692D4ED31C2ED2319E13"
+      apps_tools_openmetadata_airflow_role_arn = "arn:aws:iam::335889174965:role/openmetadata-airflow20231002155028097300000001"
+    }
     production = {
       // TODO: Replace with production values, keeping these as a placeholder
       apps_tools_eks_oidc_url                  = "https://oidc.eks.eu-west-2.amazonaws.com/id/BEE86BED6494692D4ED31C2ED2319E13"
