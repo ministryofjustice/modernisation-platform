47e90b6fc1b3f7dadb6f8c799c7b7edfc9a4afa8
Emterry <emma.terry@digital.justice.gov.uk>
2023-10-04 12:07:04 +0100

    bug

--- terraform/environments/bootstrap/member-bootstrap/iam.tf
+++ terraform/environments/bootstrap/member-bootstrap/iam.tf
@@ -255,7 +255,7 @@ resource "aws_iam_role_policy_attachment" "testing_member_infrastructure_access_
 }
 
 resource "aws_iam_role_policy_attachment" "testing_member_grafana_workspace" {
-  role       = aws_iam_role.testing_member_infrastructure_access_role[0].id
+  role       = aws_iam_role.testing_member_infrastructure_access_role.id
   policy_arn = "arn:aws:iam::aws:policy/AWSGrafanaWorkspacePermissionManagement"
 }
 
