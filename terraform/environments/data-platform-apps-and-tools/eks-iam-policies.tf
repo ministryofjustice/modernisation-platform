5377c381fe1a4a6aa38e74a0f9d33982079d3d42
BrianEllwood <brian.ellwood@digital.justice.gov.uk>
2023-10-05 10:08:49 +0100

    amend iam policy

--- terraform/environments/data-platform-apps-and-tools/eks-iam-policies.tf
+++ terraform/environments/data-platform-apps-and-tools/eks-iam-policies.tf
@@ -4,8 +4,7 @@ data "aws_iam_policy_document" "openmetadata_airflow" {
     effect  = "Allow"
     actions = ["sts:AssumeRole"]
     resources = [
-      "arn:aws:iam::593291632749:role/openmetadata-airflow20230623105404064900000001", // analytical-platform-data-production
-      "arn:aws:iam::013433889002:role/openmetadata-airflow20230623105404064900000001"  // data-platform-development
+      "arn:aws:iam::013433889002:role/openmetadata20231004152712710000000002"
     ]
   }
 }
