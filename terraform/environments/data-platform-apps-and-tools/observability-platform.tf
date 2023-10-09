430d8f1c3f1394db750de81e10928583c08325ba
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-10-09 19:40:34 +0000

    Fix argument
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/data-platform-apps-and-tools/observability-platform.tf
+++ terraform/environments/data-platform-apps-and-tools/observability-platform.tf
@@ -7,8 +7,8 @@ module "observability_platform_iam_role" {
   role_name         = "observability-platform"
   role_requires_mfa = false
 
-  trusted_arns = [
-    "arn:aws:iam::${local.environment_configuration.observability_platform_account_id}:root",
+  trusted_role_arns = [
+    "arn:aws:iam::${local.environment_configuration.observability_platform_account_id}:root"
   ]
 
   custom_role_policy_arns = ["arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"]
