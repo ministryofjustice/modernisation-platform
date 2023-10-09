da106dac4233ba36ba6a5906748d62db9ff8ba39
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-10-09 19:37:10 +0000

    Add OP role
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

---
+++ terraform/environments/data-platform-apps-and-tools/observability-platform.tf
@@ -0,0 +1,17 @@
+module "observability_platform_iam_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = "~> 5.0"
+
+  create_role = true
+
+  role_name         = "observability-platform"
+  role_requires_mfa = false
+
+  trusted_arns = [
+    "arn:aws:iam::${local.environment_configuration.observability_platform_account_id}:root",
+  ]
+
+  custom_role_policy_arns = ["arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"]
+
+  tags = local.tags
+}
