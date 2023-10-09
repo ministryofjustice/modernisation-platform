b6851db120fd3ea1759ff7934becb9a4a454424f
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-10-09 16:05:24 +0000

    Set role_requires_mfa to false
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/observability-platform/iam-roles.tf
+++ terraform/environments/observability-platform/iam-roles.tf
@@ -6,6 +6,7 @@ module "data_platform_apps_tools_iam_role" {
   role_name               = "data-platform-apps-and-tools"
   trusted_role_arns       = ["arn:aws:iam::${local.environment_configuration.data_platform_apps_tools_account_id}:root"]
   custom_role_policy_arns = [module.amazon_managed_prometheus_iam_policy.arn]
+  role_requires_mfa       = false
 
   tags = local.tags
 }
