b10150b482fe69a6a3effa46d374631db57530bd
Richard Baguley <20583399+bagg3rs@users.noreply.github.com>
2023-10-09 11:30:44 +0000

    🔎 Adds managed Grafana
    Co-authored-by: Jacob Woffenden <jacob@woffenden.io>
    Co-authored-by: Gary <Gary-H9@users.noreply.github.com>
    Signed-off-by: Richard Baguley <20583399+bagg3rs@users.noreply.github.com>

---
+++ terraform/environments/observability-platform/managed-grafana.tf
@@ -0,0 +1,20 @@
+module "managed_grafana" {
+  source  = "terraform-aws-modules/managed-service-grafana/aws"
+  version = "~> 2.0"
+
+  name = local.application_name
+
+  license_type = "ENTERPRISE_FREE_TRIAL"
+
+  account_access_type       = "CURRENT_ACCOUNT"
+  authentication_providers  = ["AWS_SSO"]
+  permission_type           = "SERVICE_MANAGED"
+  data_sources              = ["CLOUDWATCH", "PROMETHEUS"]
+  notification_destinations = ["SNS"]
+
+  role_associations = {
+    "ADMIN" = {
+      "group_ids" = ["16a2d234-1031-70b5-2657-7f744c55e48f"] # observability-platform
+    }
+  }
+}
