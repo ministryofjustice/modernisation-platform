cc1a39e5beecbf068406791ef5ab494256192e34
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-10-13 14:52:52 +0000

    Remove versions
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/data-platform-apps-and-tools/versions.tf
+++ terraform/environments/data-platform-apps-and-tools/versions.tf
@@ -1,36 +1,24 @@
 terraform {
   required_providers {
     aws = {
-      version = "~> 5.0"
       source  = "hashicorp/aws"
-    }
-    cloudinit = {
-      version = "~> 2.0"
-      source  = "hashicorp/cloudinit"
+      version = "~> 5.0"
     }
     helm = {
-      version = "~> 2.0"
       source  = "hashicorp/helm"
+      version = "~> 2.0"
     }
     http = {
-      version = "~> 3.0"
       source  = "hashicorp/http"
+      version = "~> 3.0"
     }
     kubernetes = {
-      version = "~> 2.0"
       source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
     }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.0"
-    }
-    time = {
-      version = "~> 0.9"
-      source  = "hashicorp/time"
-    }
-    tls = {
-      version = "~> 4.0"
-      source  = "hashicorp/tls"
     }
   }
   required_version = "~> 1.0"
