7b38773a5d02e35e51057e7d4a42ad11744dfc37
dms1981 <david.sibley@digital.justice.gov.uk>
2023-10-30 14:08:57 +0000

    corrected resource statemet to deny all access to S3 resources (#5318)

--- terraform/modernisation-platform-account/iam.tf
+++ terraform/modernisation-platform-account/iam.tf
@@ -150,7 +150,7 @@ data "aws_iam_policy_document" "modernisation_account_limited_read" {
   statement {
     effect    = "Deny"
     actions   = [ "s3:*" ]
-    resources = [ "arn:aws:s3:eu-west-2:${data.aws_caller_identity.current.account_id}:*" ]
+    resources = [ "arn:aws:s3:::*" ]
   }
 }
 resource "aws_iam_policy" "modernisation_account_limited_read" {
