d371967ec1e7ebf092cd244d51111c871203dc3d
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2023-10-31 09:21:24 +0000

    Removing github tls certificate

--- terraform/environments/digital-prison-reporting/data.tf
+++ terraform/environments/digital-prison-reporting/data.tf
@@ -3,10 +3,6 @@ data "http" "environments_file" {
   url = "https://raw.githubusercontent.com/ministryofjustice/modernisation-platform/main/environments/${local.application_name}.json"
 }
 # TLS certificate data
-data "tls_certificate" "github" {
-  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
-}
-# TLS certificate data
 data "tls_certificate" "circleci" {
   url = "https://oidc.circleci.com/org/${data.aws_secretsmanager_secret_version.circleci.secret_string}/.well-known/openid-configuration"
 }
