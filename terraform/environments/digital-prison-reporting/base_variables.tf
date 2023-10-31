de97895283b4b98fa7e13e7461e78b1a78f68db7
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2023-10-31 09:09:45 +0000

    Removed github thumbprints

--- terraform/environments/digital-prison-reporting/base_variables.tf
+++ terraform/environments/digital-prison-reporting/base_variables.tf
@@ -4,15 +4,6 @@ variable "networking" {
 
 }
 
-variable "github_known_thumbprints" {
-  type        = list(string)
-  description = "The known intermediary thumbprints for the GitHub OIDC provider"
-  default     = [
-    "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
-    "6938fd4d98bab03faadb97b34396831e3780aea1"
-  ]
-}
-
 variable "circleci_known_thumbprints" {
   type        = list(string)
   description = "The known intermediary thumbprints for the circle ci provider"
