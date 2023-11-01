a84fcbde54989c22fdfb7746f94e9becac2977e2
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2023-11-01 09:06:14 +0000

    Reduced permissions

--- terraform/environments/digital-prison-reporting/iam.tf
+++ terraform/environments/digital-prison-reporting/iam.tf
@@ -17,31 +17,122 @@ resource "aws_iam_role" "circleci_iam_role" {
   })
 }
 
-
+#tfsec:ignore:aws-iam-no-policy-wildcards
 data "aws_iam_policy_document" "circleci_iam_policy" {
+  #checkov:skip=CKV_AWS_289
+  #checkov:skip=CKV_AWS_288
+  #checkov:skip=CKV_AWS_290
+  #checkov:skip=CKV_AWS_355: Allows access to multiple unknown resources
   statement {
+    #checkov:skip=CKV_AWS_108
+    #checkov:skip=CKV_AWS_111
+    #checkov:skip=CKV_AWS_107
+    #checkov:skip=CKV_AWS_109
+    #checkov:skip=CKV_AWS_110
+    #checkov:skip=CKV2_AWS_40
+    #checkov:skip=CKV_AWS_356: Needs to access multiple resources
+    effect = "Allow"
     actions = [
-        "ec2:RunInstances",
-        "ec2:DescribeInstances",
-        "ec2:TerminateInstances",
-        "ec2:CreateTags",
-        "s3:PutObject",
-        "s3:GetObject",
-        "s3:ListBucket",
-        "iam:PassRole",
-        "elasticbeanstalk:*",
-        "rds:*",
-        "lambda:*",
-        "cloudformation:*",
-        "ecs:*",
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents",
-        "elasticache:*",
-        "kms:Decrypt*",
-        "kms:GenerateDataKey"
+      "athena:StartQueryExecution",
+      "athena:GetQueryExecution",
+      "backup:Start*",
+      "codebuild:Start*",
+      "codebuild:StartBuild",
+      "codebuild:BatchGetBuilds",
+      "codepipeline:StartPipelineExecution",
+      "dms:StartReplicationTask",
+      "dms:StopReplicationTask",
+      "dms:TestConnection",
+      "dms:StartReplicationTaskAssessment",
+      "dms:StartReplicationTaskAssessmentRun",
+      "dms:DescribeEndpoints",
+      "dms:DescribeEndpointSettings",
+      "dms:RebootReplicationInstance",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "datasync:Create*",
+      "datasync:Delete*",
+      "datasync:Describe*",
+      "datasync:List*",
+      "datasync:TagResource",
+      "datasync:StartTaskExecution",
+      "ecs:RegisterTaskDefinition",
+      "ecs:UpdateService",
+      "ecs:deregisterTaskDefinition",
+      "ecs:DescribeServices",
+      "ecs:DescribeTaskDefinition",
+      "ecs:*Tasks",
+      "ecs:ListServices",
+      "ecs:CreateService",
+      "ecs:*Task",
+      "ecs:ListTaskDefinitions",
+      "ecs:*TaskSet",
+      "ecs:TagResource",
+      "ecr:DescribeImages",
+      "ecr:DescribeRepositories",
+      "ecr:ListImages",
+      "ecr:GetAuthorizationToken",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:UploadLayerPart",
+      "ec2:DescribeInstances",
+      "ec2:CreateNetworkInterface",
+      "ec2:CreateNetworkInterfacePermission",
+      "ec2:AttachNetworkInterface",
+      "ec2:DeleteNetworkInterface",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeVpcEndpoints",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:ModifyNetworkInterfaceAttribute",
+      "elasticfilesystem:Describe*",
+      "elasticfilesystem:Create*",
+      "elasticfilesystem:Delete*",
+      "elasticfilesystem:restore",
+      "glue:GetJobRuns",
+      "glue:StartJobRun",
+      "glue:GetJobs",
+      "glue:GetTable",
+      "glue:BatchGetJobs",
+      "glue:ListJobs",
+      "glue:StartJobRun",
+      "glue:StartTrigger",
+      "glue:StopSession",
+      "glue:StopTrigger",
+      "glue:BatchStopJobRun",
+      "glue:StopWorkflowRun",
+      "glue:UpdateJob",
+      "iam:getRole",
+      "iam:getRolePolicy",
+      "iam:listAttachedRolePolicies",
+      "iam:listInstanceProfilesForRole",
+      "iam:listRolePolicies",
+      "iam:ListRoles",
+      "iam:PassRole",
+      "kinesis:PutRecord",
+      "kms:DescribeKey",
+      "kms:Decrypt",
+      "kms:GenerateDataKey",
+      "logs:CreateLogGroup",
+      "logs:DescribeLogGroups",
+      "logs:DescribeResourcePolicies",
+      "logs:GetLogEvents",
+      "s3:GetBucketLocation",
+      "s3:ListBucket",
+      "s3:ListAllMyBuckets",
+      "s3:*Object*",
+      "secretsmanager:ListSecrets",
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:GetResourcePolicy",
+      "ssm:GetParameter",
+      "ssm:PutParameter"
     ]
-    resources = ["*"]
+    resources = ["*"] #tfsec:ignore:AWS099 tfsec:ignore:AWS097
   }
 }
 
