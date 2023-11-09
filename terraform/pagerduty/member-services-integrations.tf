b4569d3ac761f08bfd8b9bec226f9567859287ad
matt-k1998 <mateusz.kolakowski@justice.gov.uk>
2023-11-09 16:16:48 +0000

    NCAS: Add PagerDuty

--- terraform/pagerduty/member-services-integrations.tf
+++ terraform/pagerduty/member-services-integrations.tf
@@ -1108,3 +1108,50 @@ resource "pagerduty_slack_connection" "planetfm_connection" {
   }
 }
 # Slack channel: #planetfm_alerts_modernisation_platform
+
+# NCAS non prod
+resource "pagerduty_service" "ncas_non_prod" {
+  name                    = "NCAS non prod alarms"
+  description             = "NCAS non prod alarms (dev for now)"
+  auto_resolve_timeout    = 345600
+  acknowledgement_timeout = "null"
+  escalation_policy       = pagerduty_escalation_policy.member_policy.id
+  alert_creation          = "create_alerts_and_incidents"
+}
+
+resource "pagerduty_service_integration" "ncas_non_prod_cloudwatch" {
+  name    = data.pagerduty_vendor.cloudwatch.name
+  service = pagerduty_service.ncas_non_prod.id
+  vendor  = data.pagerduty_vendor.cloudwatch.id
+}
+
+resource "pagerduty_slack_connection" "my_connection" {
+  source_id = pagerduty_service.ncas_non_prod.id
+  source_type = "service_reference"
+  workspace_id = local.slack_workspace_id
+  channel_id = "C0399KTB88G"
+  notification_type = "responder"
+  config {
+    events = [
+      "incident.triggered",
+      "incident.acknowledged",
+      "incident.escalated",
+      "incident.resolved",
+      "incident.reassigned",
+      "incident.annotated",
+      "incident.unacknowledged",
+      "incident.delegated",
+      "incident.priority_updated",
+      "incident.responder.added",
+      "incident.responder.replied",
+      "incident.action_invocation.created",
+      "incident.action_invocation.terminated",
+      "incident.action_invocation.updated",
+      "incident.status_update_published",
+      "incident.reopened"
+    ]
+    priorities = ["*"]
+  }
+}
+
+# Slack channel: #dts-legacy-apps-system-alerts
