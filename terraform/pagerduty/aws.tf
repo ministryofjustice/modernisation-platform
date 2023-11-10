3b84d00c51b371afe52803a9119cada8f3432a6d
Mateusz Kolakowski <mateusz.kolakowski@justice.gov.uk>
2023-11-10 09:13:21 +0000

    NCAS: Add PagerDuty (#5433)

--- terraform/pagerduty/aws.tf
+++ terraform/pagerduty/aws.tf
@@ -46,6 +46,7 @@ resource "aws_secretsmanager_secret_version" "pagerduty_integration_keys" {
     csr_alarms                   = pagerduty_service_integration.csr_cloudwatch.integration_key,
     dpr_nonprod_alarms           = pagerduty_service_integration.dpr_nonprod_cloudwatch.integration_key,
     planetfm_alarms              = pagerduty_service_integration.planetfm_cloudwatch.integration_key,
+    ncas_non_prod_alarms         = pagerduty_service_integration.ncas_non_prod_cloudwatch.integration_key,
   })
 }
 
