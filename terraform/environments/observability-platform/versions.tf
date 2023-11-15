e176e2cbed6dc5ba618e3029994d2952329f3b70
Emterry <emma.terry@digital.justice.gov.uk>
2023-11-15 14:18:23 +0000

    add provider

--- terraform/environments/observability-platform/versions.tf
+++ terraform/environments/observability-platform/versions.tf
@@ -8,6 +8,10 @@ terraform {
       version = "~> 3.0"
       source  = "hashicorp/http"
     }
+    grafana = {
+      source  = "grafana/grafana"
+      version = "2.6.1"
+    }
   }
   required_version = "~> 1.0"
 }
