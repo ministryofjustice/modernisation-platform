77651e8b07fa43ac0678a9e18faee8c773de86f3
Emterry <emma.terry@digital.justice.gov.uk>
2023-11-15 14:27:43 +0000

    add region

--- terraform/environments/observability-platform/data.tf
+++ terraform/environments/observability-platform/data.tf
@@ -2,3 +2,5 @@
 data "http" "environments_file" {
   url = "https://raw.githubusercontent.com/ministryofjustice/modernisation-platform/main/environments/${local.application_name}.json"
 }
+
+data "aws_region" "current" {}
