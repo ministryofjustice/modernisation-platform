78110626889ff7c76f6c4de58a3bbf0af993d8de
Aafreen Ansari <aafreen.ansari@digital.justice.gov.uk>
2023-11-15 17:38:51 +0000

    Create main.tf

---
+++ terraform/environments/cooker/test-ec2/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
