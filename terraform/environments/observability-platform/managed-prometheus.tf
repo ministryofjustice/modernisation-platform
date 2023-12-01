1b94e34406bcc04b7e62d06550731c5ba954f9ab
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2023-12-01 11:25:00 +0000

    Removing code migrated to environments

--- terraform/environments/observability-platform/managed-prometheus.tf
+++
@@ -1,9 +0,0 @@
-module "managed_prometheus" {
-  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
-  source  = "terraform-aws-modules/managed-service-prometheus/aws"
-  version = "~> 2.0"
-
-  workspace_alias = local.application_name
-
-  tags = local.tags
-}
