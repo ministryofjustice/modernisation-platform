3e2270c79b5480c67c33d44bd9aeca82f46dc384
ewastempel <52969218+ewastempel@users.noreply.github.com>
2023-12-07 15:48:42 +0000

    Addressing checkov failures (#5738)
    * Addressing checkov failures
    
    * Adding fix to the deployment errors

--- terraform/environments/core-shared-services/instance-scheduler-lambda-function.tf
+++ terraform/environments/core-shared-services/instance-scheduler-lambda-function.tf
@@ -9,6 +9,7 @@ module "instance_scheduler" {
   description                    = "Lambda to automatically start and stop instances on member accounts"
   role_name                      = "InstanceSchedulerLambdaFunctionPolicy"
   policy_json                    = data.aws_iam_policy_document.instance-scheduler-lambda-function-policy.json
+  policy_json_attached           = true
   function_name                  = "instance-scheduler-lambda-function"
   create_role                    = true
   reserved_concurrent_executions = 1
@@ -81,10 +82,12 @@ resource "aws_cloudwatch_event_target" "instance_scheduler_weekly_start_in_the_m
 
 # sns topics that are the instance scheduler lambda function's destination configuration
 resource "aws_sns_topic" "on_failure" {
+  #checkov:skip=CKV_AWS_26:"encrypted topics do not work with pagerduty subscription"
   name = "instance-scheduler-event-notification-topic-on-failure"
 }
 
 resource "aws_sns_topic" "on_success" {
+  #checkov:skip=CKV_AWS_26:"encrypted topics do not work with pagerduty subscription"
   name = "instance-scheduler-event-notification-topic-on-success"
 }
 
@@ -93,7 +96,7 @@ module "pagerduty_core_alerts" {
   depends_on = [
     aws_sns_topic.on_failure, aws_sns_topic.on_success
   ]
-  source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=v2.0.0"
+  source                    = "github.com/ministryofjustice/modernisation-platform-terraform-pagerduty-integration?ref=0179859e6fafc567843cd55c0b05d325d5012dc4" # v2.0.0
   sns_topics                = [aws_sns_topic.on_failure.name, aws_sns_topic.on_success.name]
   pagerduty_integration_key = local.pagerduty_integration_keys["core_alerts_cloudwatch"]
 }
