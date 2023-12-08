2f6f7036a47e2c7603e7c80de39b9c7c40928e54
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-12-08 14:22:08 +0000

    ♻️ Switch to Observability Platform Terraform module (#5740)
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/data-platform-apps-and-tools/observability-platform.tf
+++ terraform/environments/data-platform-apps-and-tools/observability-platform.tf
@@ -1,18 +1,8 @@
-module "observability_platform_iam_role" {
+module "observability_platform_tenant" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
-  version = "~> 5.0"
 
-  create_role = true
+  source  = "ministryofjustice/observability-platform-tenant/aws"
+  version = "0.0.2"
 
-  role_name         = "observability-platform"
-  role_requires_mfa = false
-
-  trusted_role_arns = [
-    "arn:aws:iam::${local.environment_configuration.observability_platform_account_id}:root"
-  ]
-
-  custom_role_policy_arns = ["arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"]
-
-  tags = local.tags
+  observability_platform_account_id = local.environment_configuration.observability_platform_account_id
 }
