e4a65daa370e9072d721c473392f01d035b5dcff
harichintala1 <109067588+harichintala1@users.noreply.github.com>
2023-12-14 15:21:25 +0000

    Update s3 permissions

--- terraform/environments/digital-prison-reporting/iam.tf
+++ terraform/environments/digital-prison-reporting/iam.tf
@@ -30,7 +30,6 @@ data "aws_iam_policy_document" "circleci_iam_policy" {
       "ec2:DescribeInstances",
       "ec2:TerminateInstances",
       "ec2:CreateTags",
-      "s3:PutObject",
       "s3:ListBucket",
       "s3:*Object",
       "iam:PassRole",
