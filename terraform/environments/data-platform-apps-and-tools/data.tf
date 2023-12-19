406f6be3bb8a4438791d781e184502e062b588e7
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2023-12-19 12:03:52 +0000

    Removed tenant code

--- terraform/environments/data-platform-apps-and-tools/data.tf
+++ terraform/environments/data-platform-apps-and-tools/data.tf
@@ -6,39 +6,3 @@ data "http" "environments_file" {
 data "aws_region" "current" {}
 
 data "aws_availability_zones" "available" {}
-
-
-##################################################
-# Data Platform Apps and Tools Route 53
-##################################################
-
-data "aws_route53_zone" "apps_tools" {
-  name         = local.environment_configuration.route53_zone
-  private_zone = false
-}
-
-##################################################
-# Data Platform Apps and Tools Airflow S3
-##################################################
-
-data "aws_s3_bucket" "airflow" {
-  bucket = local.environment_configuration.airflow_s3_bucket
-}
-
-##################################################
-# Data Platform Apps and Tools IAM
-##################################################
-
-
-
-##################################################
-# Data Platform Apps and Tools Open Metadata
-##################################################
-
-data "aws_secretsmanager_secret_version" "openmetadata_entra_id_client_id" {
-  secret_id = "openmetadata/entra-id/client-id"
-}
-
-data "aws_secretsmanager_secret_version" "openmetadata_entra_id_tenant_id" {
-  secret_id = "openmetadata/entra-id/tenant-id"
-}
--- terraform/environments/data-platform-apps-and-tools/versions.tf
+++ terraform/environments/data-platform-apps-and-tools/versions.tf
@@ -4,20 +4,8 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.0"
-    }
     http = {
       source  = "hashicorp/http"
-      version = "~> 3.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-    random = {
-      source  = "hashicorp/random"
       version = "~> 3.0"
     }
   }
