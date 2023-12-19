951ffdb805c4255e3bb6d0a5febe5f1bb21407c9
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-12-19 15:49:47 +0000

    🚚 Migrate DPAT (#5825)
    * Halfway through migration, pus
    hed changes, do not merge
    
    * Removed tenant code
    
    ---------
    
    Co-authored-by: Edward Proctor <edward.proctor@digital.justice.gov.uk>

--- terraform/environments/data-platform-apps-and-tools/mwaa-iam-user-policy-attachments.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_iam_user_policy_attachment" "airflow_ses" {
-  user       = module.airflow_iam_user.iam_user_name
-  policy_arn = module.airflow_ses_policy.arn
-}
--- terraform/environments/data-platform-apps-and-tools/observability-platform.tf
+++
@@ -1,8 +0,0 @@
-module "observability_platform_tenant" {
-  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
-
-  source  = "ministryofjustice/observability-platform-tenant/aws"
-  version = "0.0.3"
-
-  observability_platform_account_id = local.environment_configuration.observability_platform_account_id
-}
