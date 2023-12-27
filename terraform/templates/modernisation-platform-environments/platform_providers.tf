50c65e6fd31a30f1afbf03f59485720ee0e43402
ep-93 <109581241+ep-93@users.noreply.github.com>
2023-12-27 13:20:04 +0000

    Update platform_providers.tf

--- terraform/templates/modernisation-platform-environments/platform_providers.tf
+++ terraform/templates/modernisation-platform-environments/platform_providers.tf
@@ -47,3 +47,12 @@ provider "aws" {
     role_arn = !can(regex("githubactionsrolesession|AdministratorAccess|user", data.aws_caller_identity.original_session.arn)) ? null : can(regex("user", data.aws_caller_identity.original_session.arn)) ? "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/${var.collaborator_access}" : "arn:aws:iam::${data.aws_caller_identity.original_session.id}:role/MemberInfrastructureAccessUSEast"
   }
 }
+
+# Provider for creating resources in eu-central-1, eg Bedrock resources
+provider "aws" {
+  alias  = "eu-central-1"
+  region = "eu-central-1"
+  assume_role {
+    role_arn = !can(regex("githubactionsrolesession|AdministratorAccess|user", data.aws_caller_identity.original_session.arn)) ? null : can(regex("user", data.aws_caller_identity.original_session.arn)) ? "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/${var.collaborator_access}" : "arn:aws:iam::${data.aws_caller_identity.original_session.id}:role/MemberInfrastructureBedrockEuCentral"
+  }
+}
