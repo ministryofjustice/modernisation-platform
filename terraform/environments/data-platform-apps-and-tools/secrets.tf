c8d414e7391989db91b7296f86b5db5cf13fef3b
murad-ali-MoJ <murad.ali@justice.gov.uk>
2024-01-10 09:32:38 +0000

    added secret for jml

--- terraform/environments/data-platform-apps-and-tools/secrets.tf
+++ terraform/environments/data-platform-apps-and-tools/secrets.tf
@@ -3,6 +3,17 @@ data "aws_ssm_parameter" "modernisation_platform_account_id" {
   name = "modernisation_platform_account_id"
 }
 
+# Create a new secret in AWS SecretsManager for Gov.UK Notify
+resource "aws_secretsmanager_secret" "gov_uk_notify_secret" {
+  name = "gov_uk_notify_secret"
+}
+
+# Add the API key to the secret
+resource "aws_secretsmanager_secret_version" "gov_uk_notify_secret_version" {
+  secret_id     = aws_secretsmanager_secret.gov_uk_notify_secret.id
+  secret_string = "API_KEY_FROM_1PASSWORD"
+}
+
 # Get secret by arn for environment management
 data "aws_secretsmanager_secret" "environment_management" {
   provider = aws.modernisation-platform
