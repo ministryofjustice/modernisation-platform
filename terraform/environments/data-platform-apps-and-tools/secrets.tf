65d1087ee6eb4a050f883a3725c30b9304eb19b3
murad-ali-MoJ <murad.ali@justice.gov.uk>
2024-01-11 09:16:49 +0000

    add secret for the api-key and email

--- terraform/environments/data-platform-apps-and-tools/secrets.tf
+++ terraform/environments/data-platform-apps-and-tools/secrets.tf
@@ -3,15 +3,29 @@ data "aws_ssm_parameter" "modernisation_platform_account_id" {
   name = "modernisation_platform_account_id"
 }
 
-# Create a new secret in AWS SecretsManager for Gov.UK Notify
-resource "aws_secretsmanager_secret" "gov_uk_notify_secret" {
-  name = "gov_uk_notify_secret"
+# Create a new secret in AWS SecretsManager for Gov.UK Notify API key
+resource "aws_secretsmanager_secret" "govuk_notify_api_key" {
+  count = terraform.workspace == "data-platform-apps-and-tools-production" ? 1 : 0
+
+  name = "gov-uk-notify/production/api-key"
 }
 
-# Add the API key to the secret
-resource "aws_secretsmanager_secret_version" "gov_uk_notify_secret_version" {
-  secret_id     = aws_secretsmanager_secret.gov_uk_notify_secret.id
-  secret_string = "API_KEY_FROM_1PASSWORD"
+# Add the Gov.UK Notify API key to the secret
+resource "aws_secretsmanager_secret_version" "gov_uk_notify_api_key_version" {
+  count       = terraform.workspace == "data-platform-apps-and-tools-production" ? 1 : 0
+  secret_id   = aws_secretsmanager_secret.govuk_notify_api_key[0].id
+  secret_string = "API_KEY_PLACEHOLDER"
+}
+
+# Email secret for Lambda function
+resource "aws_secretsmanager_secret" "email_secret" {
+  name = "jml/email"
+}
+
+# Add the email value to the secret
+resource "aws_secretsmanager_secret_version" "email_secret_version" {
+  secret_id     = aws_secretsmanager_secret.email_secret.id
+  secret_string = "EMAIL_VALUE"
 }
 
 # Get secret by arn for environment management
