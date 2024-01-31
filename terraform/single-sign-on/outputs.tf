46a20307fecd9816bb4cf423c1bffd78f48eb479
modernisation-platform-ci <modernisation-platform+github@digital.justice.gov.uk>
2024-01-31 04:50:25 +0000

    Updates from GitHub Actions Format Code workflow

--- terraform/single-sign-on/outputs.tf
+++ terraform/single-sign-on/outputs.tf
@@ -1,23 +1,23 @@
 output "sandbox" {
-    value = aws_ssoadmin_permission_set.modernisation_platform_sandbox.arn
+  value = aws_ssoadmin_permission_set.modernisation_platform_sandbox.arn
 }
 
 output "migration" {
-    value = aws_ssoadmin_permission_set.modernisation_platform_migration.arn
+  value = aws_ssoadmin_permission_set.modernisation_platform_migration.arn
 }
 
 output "instance_management" {
-    value = aws_ssoadmin_permission_set.modernisation_platform_instance_management.arn
+  value = aws_ssoadmin_permission_set.modernisation_platform_instance_management.arn
 }
 
 output "data_engineer" {
-    value = aws_ssoadmin_permission_set.modernisation_platform_data_engineer.arn
+  value = aws_ssoadmin_permission_set.modernisation_platform_data_engineer.arn
 }
 
 output "reporting_operations" {
-    value = aws_ssoadmin_permission_set.modernisation_platform_reporting_operations.arn
+  value = aws_ssoadmin_permission_set.modernisation_platform_reporting_operations.arn
 }
 
 output "mwaa_user" {
-    value = aws_ssoadmin_permission_set.modernisation_platform_data_mwaa_user.arn
+  value = aws_ssoadmin_permission_set.modernisation_platform_data_mwaa_user.arn
 }
