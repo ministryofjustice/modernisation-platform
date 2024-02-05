ab2b6eed41c0bee43233f07b5aea9ff56929d290
Alistair Curtis <alistair.curtis@digital.justice.gov.uk>
2024-02-05 14:42:11 +0000

    create cdpt-ifs-ecr-repo

--- terraform/environments/core-shared-services/ecr_repos.tf
+++ terraform/environments/core-shared-services/ecr_repos.tf
@@ -792,6 +792,27 @@ module "cdpt_chaps_ecr_repo" {
   tags_common = local.tags
 }
 
+module "cdpt_ifs_ecr_repo" {
+  source = "../../modules/app-ecr-repo"
+
+  app_name = "cdpt-ifs"
+
+  push_principals = [
+    "arn:aws:iam::${local.environment_management.account_ids["cdpt-ifs-development"]}:role/modernisation-platform-oidc-cicd",
+    "arn:aws:iam::${local.environment_management.account_ids["cdpt-ifs-preproduction"]}:role/modernisation-platform-oidc-cicd",
+    "arn:aws:iam::${local.environment_management.account_ids["cdpt-ifs-production"]}:role/modernisation-platform-oidc-cicd"
+  ]
+
+  pull_principals = [
+    local.environment_management.account_ids["cdpt-ifs-development"],
+    local.environment_management.account_ids["cdpt-ifs-preproduction"],
+    local.environment_management.account_ids["cdpt-ifs-production"]
+  ]
+
+  # Tags
+  tags_common = local.tags
+}
+
 # Delius Core
 module "delius_core_ansible_aws_ecr_repo" {
   source = "../../modules/app-ecr-repo"
