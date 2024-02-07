303eb7c45c63dd0c99d628b0c2d4899a0e5067ad
Sukesh <sukesh.reddygade@digital.justice.gov.uk>
2024-02-07 16:38:34 +0000

    Added modernisation_platform_account local variable

--- terraform/environments/bootstrap/single-sign-on/locals.tf
+++ terraform/environments/bootstrap/single-sign-on/locals.tf
@@ -1,3 +1,8 @@
+# This data sources allows us to get the Modernisation Platform account information for use elsewhere
+data "aws_caller_identity" "modernisation-platform" {
+
+}
+
 locals {
 
   app_name = try(regex("^bichard*.|^remote-supervisio*.", terraform.workspace), replace(terraform.workspace, "/-([[:alnum:]]+)$/", ""))
@@ -6,7 +11,8 @@ locals {
   #app_name = replace(terraform.workspace, "/-([[:alnum:]]+)$/", "")
   env_name = replace(terraform.workspace, "${local.app_name}-", "")
 
-  environment_management = jsondecode(data.aws_secretsmanager_secret_version.environment_management.secret_string)
+  modernisation_platform_account = data.aws_caller_identity.modernisation-platform
+  environment_management         = jsondecode(data.aws_secretsmanager_secret_version.environment_management.secret_string)
 
   defname = jsondecode(file("../../../../environments/${local.app_name}.json"))
 
