01b60316250fe6271992b6969d14ff22893eea02
Hope Aitchison <hope.aitchison@digital.justice.gov.uk>
2024-02-09 12:51:47 +0000

    adding AD ports outbound to all firewall rules

--- terraform/environments/core-network-services/cidr-ranges.tf
+++ terraform/environments/core-network-services/cidr-ranges.tf
@@ -77,6 +77,7 @@ locals {
     hmpps-production-general-private-subnets    = "10.27.10.0/22"
 
     mojo-end-user-devices = "10.0.0.0/8"
+    outbound-to-all       = "0.0.0.0/0"
   }
 
   all_cidr_ranges = merge(
