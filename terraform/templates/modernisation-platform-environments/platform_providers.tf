e2d192b621710f77481d5e56e9d210175c62b4ad
David Sibley <david.sibley@digital.justice.gov.uk>
2024-02-12 11:56:13 +0000

    added sso-readonly provider to platform_providers template

--- terraform/templates/modernisation-platform-environments/platform_providers.tf
+++ terraform/templates/modernisation-platform-environments/platform_providers.tf
@@ -47,3 +47,12 @@ provider "aws" {
     role_arn = !can(regex("githubactionsrolesession|AdministratorAccess|user", data.aws_caller_identity.original_session.arn)) ? null : can(regex("user", data.aws_caller_identity.original_session.arn)) ? "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/${var.collaborator_access}" : "arn:aws:iam::${data.aws_caller_identity.original_session.id}:role/MemberInfrastructureAccessUSEast"
   }
 }
+
+# Provider for reading resources from root account IdentityStore
+provider "aws" {
+  region = "eu-west-2"
+  alias  = "sso-readonly"
+  assume_role {
+    role_arn = "arn:aws:iam::${local.environment_management.aws_organizations_root_account_id}:role/ModernisationPlatformSSOReadOnly"
+  }
+}
