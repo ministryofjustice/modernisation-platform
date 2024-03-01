1de210392e498e1055c26ed94724725d64e3039c
mikereiddigital <mike.reid@digital.justice.gov.uk>
2024-03-01 11:21:21 +0000

    Big fixes

--- terraform/environments/core-vpc/locals.tf
+++ terraform/environments/core-vpc/locals.tf
@@ -8,7 +8,7 @@ locals {
   application_name           = "core-vpc"
   environment_management     = jsondecode(data.aws_secretsmanager_secret_version.environment_management.secret_string)
   pagerduty_integration_keys = jsondecode(data.aws_secretsmanager_secret_version.pagerduty_integration_keys.secret_string)
-  firehose_preprod_network_secret = tostring(jsondecode(data.aws_secretsmanager_secret_version.preprod_network_secret_arn_version.secret_string["second_key"]))
+  firehose_preprod_network_secret = tostring(jsondecode(data.aws_secretsmanager_secret_version.preprod_network_secret_arn_version.secret_string["xsiam_preprod_network_secret"]))
 
   # This takes the name of the Terraform workspace (e.g. core-vpc-production), strips out the application name (e.g. core-vpc), and checks if
   # the string leftover is `-production`, if it isn't (e.g. core-vpc-non-production => -non-production) then it sets the var to false.
