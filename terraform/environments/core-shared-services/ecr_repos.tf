4d662ffb025e19e61a956d012e37534b2ffd3b7f
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-03-12 12:53:17 +0000

    Add push principal for data-platform-apps-and-tools-development
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- terraform/environments/core-shared-services/ecr_repos.tf
+++ terraform/environments/core-shared-services/ecr_repos.tf
@@ -755,6 +755,7 @@ module "data_platform_jml_ecr_repo" {
 
   push_principals = [
     "arn:aws:iam::${local.environment_management.account_ids["data-platform-development"]}:role/modernisation-platform-oidc-cicd",
+    "arn:aws:iam::${local.environment_management.account_ids["data-platform-apps-and-tools-development"]}:role/modernisation-platform-oidc-cicd",
     local.environment_management.account_ids["data-platform-apps-and-tools-production"],
   ]
 
