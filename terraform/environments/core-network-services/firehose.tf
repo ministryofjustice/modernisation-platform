03fbe45bcab3adde9704c4ef3a2e8f010ca7704f
mikereiddigital <mike.reid@digital.justice.gov.uk>
2024-03-19 14:27:49 +0000

    Easier to read the module call.

--- terraform/environments/core-network-services/firehose.tf
+++ terraform/environments/core-network-services/firehose.tf
@@ -2,9 +2,19 @@
 # The purpose of this module call is to generate firehose & related resources for each cloudwatch log group created for the inline & external inspection firewalls.
 # This uses the cloudwatch log names outputted from the vpc inspection firewall module as well as the log group created for the external inspection firewall
 
+locals {
+    live_data = module.vpc_inspection["live_data"].fw_cloudwatch_name
+    non_live_data = module.vpc_inspection["non_live_data"].fw_cloudwatch_name
+    external_inspection = module.firewall_logging.cloudwatch_log_group_name
+
+    firewall_logs = toset([local.live_data, local.non_live_data, local.external_inspection])
+
+}
+
+
 module "external_inspection_firehose" {
   source          = "../../modules/firehose"
-  for_each        = toset([module.vpc_inspection["live_data"].fw_cloudwatch_name, module.vpc_inspection["non_live_data"].fw_cloudwatch_name, module.firewall_logging.cloudwatch_log_group_name])
+  for_each        = local.firewall_logs
   resource_prefix = each.value
   log_group_name  = each.value
   tags            = local.tags
