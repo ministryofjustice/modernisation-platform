0cd6ce63e34f1d4277e38f455f5d0ba7fbb9403b
mikereiddigital <mike.reid@digital.justice.gov.uk>
2024-03-22 14:50:46 +0000

    Fixes error whereby secrets were swapped.

--- terraform/environments/core-vpc/vpc.tf
+++ terraform/environments/core-vpc/vpc.tf
@@ -100,9 +100,10 @@ module "vpc" {
 
   build_firehose = local.build_firehose
 
-  kinesis_endpoint_secret_string = local.is-production ? tostring(local.xsiam["xsiam_prod_network_endpoint"]) : tostring(local.xsiam["xsiam_preprod_network_endpoint"])
+  kinesis_endpoint_url = local.is-production ? tostring(local.xsiam["xsiam_prod_network_endpoint"]) : tostring(local.xsiam["xsiam_preprod_network_endpoint"])
 
-  kinesis_endpoint_url = local.is-production ? tostring(local.xsiam["xsiam_prod_network_secret"]) : tostring(local.xsiam["xsiam_preprod_network_secret"])
+  kinesis_endpoint_secret_string = local.is-production ? tostring(local.xsiam["xsiam_prod_network_secret"]) : tostring(local.xsiam["xsiam_preprod_network_secret"])
+
 
   # Tags
   tags_common = local.tags
