7d14c87d4065e8b333b69e227eb18277276b845c
Rich Green <richard.green@digital.justice.gov.uk>
2024-03-25 14:51:10 +0000

    add max_retries 100

--- terraform/environments/bootstrap/secure-baselines/versions.tf
+++ terraform/environments/bootstrap/secure-baselines/versions.tf
@@ -1,9 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      version     = "~> 5.0"
-      source      = "hashicorp/aws"
-      max_retries = 100
+      version = "~> 5.0"
+      source  = "hashicorp/aws"
     }
   }
   required_version = "~> 1.0"
