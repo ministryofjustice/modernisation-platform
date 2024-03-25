f7dadfcff92f1f71dbc7ec2d5e35753a6e91856f
Rich Green <richard.green@digital.justice.gov.uk>
2024-03-25 13:33:06 +0000

    try increasing API retry limit from 25 to 100

--- terraform/environments/bootstrap/secure-baselines/versions.tf
+++ terraform/environments/bootstrap/secure-baselines/versions.tf
@@ -1,8 +1,9 @@
 terraform {
   required_providers {
     aws = {
-      version = "~> 5.0"
-      source  = "hashicorp/aws"
+      version     = "~> 5.0"
+      source      = "hashicorp/aws"
+      max_retries = 100
     }
   }
   required_version = "~> 1.0"
