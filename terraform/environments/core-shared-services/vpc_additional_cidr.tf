fd037f80d11d8c06ffe043269dd9dac39398af3e
David Sibley <david.sibley@digital.justice.gov.uk>
2024-03-28 17:21:45 +0000

    wrote basic terraform for NACL rules in new subnets

--- terraform/environments/core-shared-services/vpc_additional_cidr.tf
+++ terraform/environments/core-shared-services/vpc_additional_cidr.tf
@@ -6,6 +6,12 @@ locals {
     for az in data.aws_availability_zones.available.names :
     az => cidrsubnet(aws_vpc_ipv4_cidr_block_association.live-data-additional.cidr_block, 3, index(data.aws_availability_zones.available.names, az))
   }
+  bidirectional_nacl_rules = {
+    # Because NACLs offer two possible values for the egress attribute, egress=true means outbound, and egress=false means inbound
+    # So to save duplicating the rules I've got two keys here to ensure inbound and outbound are rules can be done quickly
+    "outbound" = true
+    "inbound"  = false
+  }
 }
 
 resource "aws_vpc_ipv4_cidr_block_association" "live-data-additional" {
@@ -34,4 +40,95 @@ resource "aws_route" "default-via-tgw" {
   route_table_id         = aws_route_table.live-data-additional.id
   destination_cidr_block = "0.0.0.0/0"
   transit_gateway_id     = data.aws_ec2_transit_gateway.transit-gateway.id
+}
+
+resource "aws_network_acl" "live-data-additional" {
+  vpc_id     = module.vpc["live_data"].vpc_id
+  subnet_ids = values(aws_subnet.live-data-additional)[*].id
+}
+
+resource "aws_network_acl_rule" "allow-vpc" {
+  for_each       = local.bidirectional_nacl_rules
+  cidr_block     = module.vpc["live_data"].vpc_cidr_block
+  egress         = each.value
+  network_acl_id = aws_network_acl.live-data-additional.id
+  protocol       = "-1"
+  rule_action    = "allow"
+  rule_number    = 1000
+}
+
+resource "aws_network_acl_rule" "allow-vpc-additional" {
+  for_each       = local.bidirectional_nacl_rules
+  cidr_block     = aws_vpc_ipv4_cidr_block_association.live-data-additional.cidr_block
+  egress         = each.value
+  network_acl_id = aws_network_acl.live-data-additional.id
+  protocol       = "-1"
+  rule_action    = "allow"
+  rule_number    = 1100
+}
+
+# There's no deny east/west here because a number of VPCs will need to connect in.
+# The DCs will be secured with security groups.
+
+resource "aws_network_acl_rule" "allow-10-0-0-0-8" {
+  for_each       = local.bidirectional_nacl_rules
+  cidr_block     = "10.0.0.0/8"
+  egress         = each.value
+  network_acl_id = aws_network_acl.live-data-additional.id
+  protocol       = "-1"
+  rule_action    = "allow"
+  rule_number    = 4000
+}
+
+resource "aws_network_acl_rule" "allow-172-16-0-0-12" {
+  for_each       = local.bidirectional_nacl_rules
+  cidr_block     = "172.16.0.0/12"
+  egress         = each.value
+  network_acl_id = aws_network_acl.live-data-additional.id
+  protocol       = "-1"
+  rule_action    = "allow"
+  rule_number    = 4100
+}
+
+resource "aws_network_acl_rule" "allow-192-168-0-0-16" {
+  for_each       = local.bidirectional_nacl_rules
+  cidr_block     = "192.168.0.0/16"
+  egress         = each.value
+  network_acl_id = aws_network_acl.live-data-additional.id
+  protocol       = "-1"
+  rule_action    = "allow"
+  rule_number    = 4200
+}
+
+resource "aws_network_acl_rule" "high-ports-in-from-internet" {
+  cidr_block     = "0.0.0.0/0"
+  egress         = false
+  from_port      = 49152
+  network_acl_id = aws_network_acl.live-data-additional.id
+  protocol       = "tcp"
+  rule_action    = "allow"
+  rule_number    = 5300
+  to_port        = 65535
+}
+
+resource "aws_network_acl_rule" "https-out-to-internet" {
+  cidr_block     = "0.0.0.0/0"
+  egress         = true
+  from_port      = 443
+  network_acl_id = aws_network_acl.live-data-additional.id
+  protocol       = "tcp"
+  rule_action    = "allow"
+  rule_number    = 5000
+  to_port        = 443
+}
+
+resource "aws_network_acl_rule" "http-out-to-internet" {
+  cidr_block     = "0.0.0.0/0"
+  egress         = true
+  from_port      = 80
+  network_acl_id = aws_network_acl.live-data-additional.id
+  protocol       = "tcp"
+  rule_action    = "allow"
+  rule_number    = 5100
+  to_port        = 80
 }
