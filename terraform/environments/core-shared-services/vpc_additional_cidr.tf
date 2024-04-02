9afb821dbd922d7a02d3c0d7b94db766238efdb5
dms1981 <david.sibley@digital.justice.gov.uk>
2024-04-02 15:45:36 +0100

    adds Name tag to NACL (#6668)

--- terraform/environments/core-shared-services/vpc_additional_cidr.tf
+++ terraform/environments/core-shared-services/vpc_additional_cidr.tf
@@ -52,7 +52,10 @@ resource "aws_route" "default-via-tgw" {
 resource "aws_network_acl" "live-data-additional" {
   vpc_id     = module.vpc["live_data"].vpc_id
   subnet_ids = values(aws_subnet.live-data-additional)[*].id
-  tags       = local.tags
+  tags = merge({
+    "Name" = "live_data-additional"
+  },
+  local.tags)
 }
 
 #trivy:ignore:AVD-AWS-0102
