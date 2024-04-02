65a03c7fe4cea5ae5b46939dd4fa8d197de45eb4
modernisation-platform-ci <modernisation-platform+github@digital.justice.gov.uk>
2024-04-02 11:03:55 +0000

    Workflow: created files in terraform/environments

---
+++ terraform/environments/delete-account/backend.tf
@@ -0,0 +1,14 @@
+# Backend
+terraform {
+  # `backend` blocks do not support variables, so the following are hard-coded here:
+  # - S3 bucket name, which is created in modernisation-platform-account/s3.tf
+  backend "s3" {
+    acl                  = "bucket-owner-full-control"
+    bucket               = "modernisation-platform-terraform-state"
+    encrypt              = true
+    key                  = "terraform.tfstate"
+    region               = "eu-west-2"
+    workspace_key_prefix = "environments/accounts/delete-account" # This will store the object as environments/accounts/delete-account/${workspace}/terraform.tfstate
+    dynamodb_table       = "modernisation-platform-terraform-state-lock"
+  }
+}
