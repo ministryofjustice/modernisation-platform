42351d3c4c7ebc2178bd27560324b6c0e259a379
mikereiddigital <mike.reid@digital.justice.gov.uk>
2024-04-09 11:24:19 +0100

    Though with sprinkler the notification was created with the .bucket property, this is changing it to .id as per the documentation.

--- terraform/environments/core-logging/sqs.tf
+++ terraform/environments/core-logging/sqs.tf
@@ -39,7 +39,7 @@ data "aws_iam_policy_document" "queue_policy_document" {
 
 # S3 bucket event notification for updates to the cloudtrail logging bucket
 resource "aws_s3_bucket_notification" "logging_bucket_notification" {
-  bucket = module.s3-bucket-cloudtrail-logging.bucket.bucket
+  bucket = module.s3-bucket-cloudtrail-logging.bucket.id
   queue {
     queue_arn = aws_sqs_queue.mp_cloudtrail_log_queue.arn
     events    = ["s3:ObjectCreated:*"] # Events to trigger the notification
