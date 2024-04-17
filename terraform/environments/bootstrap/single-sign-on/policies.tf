c316294320d019b7b39dce38f82061b825d677ff
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2024-04-17 13:32:38 +0100

    fix

--- terraform/environments/bootstrap/single-sign-on/policies.tf
+++ terraform/environments/bootstrap/single-sign-on/policies.tf
@@ -614,6 +614,7 @@ resource "aws_iam_policy" "instance-access" {
 #   "full"            = s3 put; read-write secret; ec2 ssm full access; ec2 sso local admin
 #tfsec:ignore:aws-iam-no-policy-wildcards
 data "aws_iam_policy_document" "instance-access-document" {
+  #checkov:skip=CKV_AWS_107
   #checkov:skip=CKV_AWS_108
   #checkov:skip=CKV_AWS_109
   #checkov:skip=CKV_AWS_111
