3254f34f2d6c4ae16f242fae2d7b254ed4e24982
George Taylor <george.taylor@digital.justice.gov.uk>
2024-04-30 10:52:31 +0100

    Update provision-member-directories.sh

--- scripts/provision-member-directories.sh
+++ scripts/provision-member-directories.sh
@@ -172,6 +172,7 @@ EOL
 **/subnet_share.tf @ministryofjustice/modernisation-platform
 **/networking.auto.tfvars.json @ministryofjustice/modernisation-platform
 **/platform_*.tf @ministryofjustice/modernisation-platform
+/terraform/modules/probation-webops @ministryofjustice/modernisation-platform @ministryofjustice/hmpps-migration
 /terraform/modules
 .devcontainer @ministryofjustice/devcontainer-community
 EOL
