0938455e017dc7a3d97f516b226016665483b331
Rich Green <richard.green@digital.justice.gov.uk>
2024-05-14 12:39:09 +0100

    update script and add workflow

--- scripts/monitor-ssm-instances.sh
+++ scripts/monitor-ssm-instances.sh
@@ -1,22 +1,52 @@
 #!/bin/bash
+
+#Set Values
 export AWS_PAGER=""
+regions="eu-central-1 eu-west-1 eu-west-2 eu-west-3 us-east-1"
+ROOT_AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+ROOT_AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+ROOT_AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN
 
 #Create csv file
 echo "Account ID,Account Name, Instance ID,Instance Name,SSM Status" > ssm-managed-instances.csv
 
-#Set Values
-account_name=$(aws iam list-account-aliases --query 'AccountAliases[*]' --output text)
-account_id=$(aws sts get-caller-identity --query Account --output text)
+#Assume Role Function
+getAssumeRoleCfg() {
+    account_id=$1
+    aws sts assume-role --role-arn "arn:aws:iam::${account_id}:role/ModernisationPlatformAccess" --role-session-name "test" --output json > credentials.json
+    export AWS_ACCESS_KEY_ID=$(jq -r '.Credentials.AccessKeyId' credentials.json)
+    export AWS_SECRET_ACCESS_KEY=$(jq -r '.Credentials.SecretAccessKey' credentials.json)
+    export AWS_SESSION_TOKEN=$(jq -r '.Credentials.SessionToken' credentials.json)
+}
 
-#Check for SSM managed instances
-for instance in $(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --output text)
-do
-    instance_name=$(aws ec2 describe-instances --instance-ids $instance --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value]' --output text)
-    managed=$(aws ssm describe-instance-information  --filters "Key=InstanceIds,Values=$instance" --query 'InstanceInformationList[*].[AssociationStatus]' --output text)
-    if [[ "$managed" != "Success" ]]; then 
-        managed="Not Managed";
-    else
-        managed="Managed"
-    fi
-    echo "$account_id,$account_name,$instance,$instance_name,$managed" >> ssm-managed-instances.csv
+for account_id in $(jq -r '.account_ids | to_entries[] | "\(.value)"' <<< $ENVIRONMENT_MANAGEMENT); do
+    echo "account: $account_id"
+    getAssumeRoleCfg "$account_id"
+    for region in $regions; do
+        #Set Values
+        AWS_REGION=$region
+        account_name=$(aws iam list-account-aliases --query 'AccountAliases[*]' --output text)
+        account_id=$(aws sts get-caller-identity --query Account --output text)
+
+        #Check for SSM managed instances
+        echo "Searching for SSM managed instances in $account_name $region"
+        for instance in $(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --output text)
+        do
+            instance_name=$(aws ec2 describe-instances --instance-ids $instance --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value]' --output text)
+            managed=$(aws ssm describe-instance-information  --filters "Key=InstanceIds,Values=$instance" --query 'InstanceInformationList[*].[AssociationStatus]' --output text)
+            if [[ "$managed" != "Success" ]]; then 
+                managed="Not Managed";
+            else
+                managed="Managed"
+            fi
+            echo "$account_id,$account_name,$instance,$instance_name,$managed" >> ssm-managed-instances.csv
+        done
+    done
+    export AWS_ACCESS_KEY_ID=$ROOT_AWS_ACCESS_KEY_ID
+    export AWS_SECRET_ACCESS_KEY=$ROOT_AWS_SECRET_ACCESS_KEY
+    export AWS_SESSION_TOKEN=$ROOT_AWS_SESSION_TOKEN
+    rm credentials.json
 done
+
+# # Print csv file to console
+# cat ssm-managed-instances.csv
