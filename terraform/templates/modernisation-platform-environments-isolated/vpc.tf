e0905d1fce69cd12162a571d38063a50e4ff89ba
SteveLinden <stephen.linden@digital.justice.gov.uk>
2024-05-17 10:43:47 +0100

    Correcting issue on vpc.tf code

--- terraform/templates/modernisation-platform-environments-isolated/vpc.tf
+++ terraform/templates/modernisation-platform-environments-isolated/vpc.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
 
-  source  = "github.com/terraform-aws-modules/vpc/aws?ref=25322b6b6be69db6cca7f167d7b0e5327156a595" # v5.8.1
+  source  = "github.com/terraform-aws-modules/terraform-aws-vpc//modules/vpc-endpoints?ref=25322b6b6be69db6cca7f167d7b0e5327156a595" # v5.8.1
 
   name            = "${local.application_name}-${local.environment}"
   azs             = local.availability_zones
@@ -20,7 +20,7 @@ module "vpc" {
 module "vpc_endpoints" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
 
-  source  = "github.com/terraform-aws-modules/vpc/aws//modules/vpc-endpoints?ref=25322b6b6be69db6cca7f167d7b0e5327156a595" # v5.8.1
+  source  = "github.com/terraform-aws-modules/terraform-aws-vpc//modules/vpc-endpoints?ref=25322b6b6be69db6cca7f167d7b0e5327156a595" # v5.8.1
 
   security_group_ids = [aws_security_group.vpc_endpoints.id]
   subnet_ids         = module.vpc.private_subnets
