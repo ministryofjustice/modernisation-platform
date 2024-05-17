5d43878ef3eb6c425b32dd2719c2c3fda32e212a
SteveLinden <stephen.linden@digital.justice.gov.uk>
2024-05-17 10:20:19 +0100

    Changed to correct vpc-endpoint code.

--- terraform/templates/modernisation-platform-environments-isolated/vpc.tf
+++ terraform/templates/modernisation-platform-environments-isolated/vpc.tf
@@ -20,7 +20,7 @@ module "vpc" {
 module "vpc_endpoints" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
 
-  source  = "terraform-aws-modules/vpc/aws/modules/vpc-endpoints?ref=9163310db647ed98094319980bd8eef72bee492b"
+  source  = "github.com/terraform-aws-modules/vpc/aws//modules/vpc-endpoints?ref=25322b6b6be69db6cca7f167d7b0e5327156a595" # v5.8.1
 
   security_group_ids = [aws_security_group.vpc_endpoints.id]
   subnet_ids         = module.vpc.private_subnets
