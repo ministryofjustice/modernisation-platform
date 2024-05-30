f5e1b08aebc2845dd416a531404ee89e4403625d
mark roberts <mark.roberts@digital.justice.gov.uk>
2024-05-30 14:29:05 +0100

    conditional change

--- terraform/environments/bootstrap/single-sign-on/policies.tf
+++ terraform/environments/bootstrap/single-sign-on/policies.tf
@@ -851,7 +851,9 @@ data "aws_iam_policy_document" "instance-management-document" {
       variable = "aws:PrincipalAccount"
 
       values = [
-        local.environment_management.account_ids["core-shared-services-production"]
+        local.environment_management.account_ids["core-shared-services-production"],
+        local.environment_management.account_ids["sprinkler-development"],
+        local.environment_management.account_ids["cooker-development"]
       ]
     }
     
