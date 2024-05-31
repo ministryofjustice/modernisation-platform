edff57427105dc84dbd492394fb0d339ee0cdaa6
pav <pav.gainda@digital.justice.gov.uk>
2024-05-31 10:58:32 +0100

    test patch module

--- terraform/environments/core-shared-services/ad-fixngo.tf
+++ terraform/environments/core-shared-services/ad-fixngo.tf
@@ -1039,7 +1039,7 @@ resource "aws_ssm_parameter" "ad_fixngo" {
 
 module "core-shared-service-ad-azure-dc-a" {
   source = "github.com/ministryofjustice/modernisation-platform-terraform-ssm-patching.git?ref=v3.0.0"
-  count  = local.is-production == true ? 1 : 0
+#  count  = local.is-production == true ? 1 : 0
   providers = {
     aws.bucket-replication = aws
   }
