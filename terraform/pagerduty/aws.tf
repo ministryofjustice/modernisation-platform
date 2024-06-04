88ebee4bb32501e52d237c7975d53beb89a434eb
Vincent.Cheung <vincent.cheung@digital.justice.gov.uk>
2024-06-04 11:07:38 +0100

    Correct integration key name

--- terraform/pagerduty/aws.tf
+++ terraform/pagerduty/aws.tf
@@ -67,7 +67,7 @@ resource "aws_secretsmanager_secret_version" "pagerduty_integration_keys" {
     delius_nextcloud_nonprod_alarms      = pagerduty_service_integration.delius_nextcloud_nonprod_cloudwatch.integration_key
     delius_nextcloud_prod_alarms         = pagerduty_service_integration.delius_nextcloud_prod_cloudwatch.integration_key
     laa_cwa_nonprod_alarms               = pagerduty_service_integration.cwa_non_prod.integration_key
-    laa_cwa_prod_alarm                   = pagerduty_service_integration.cwa_prod.integration_key
+    laa_cwa_prod_alarms                  = pagerduty_service_integration.cwa_prod.integration_key
   })
 }
 
