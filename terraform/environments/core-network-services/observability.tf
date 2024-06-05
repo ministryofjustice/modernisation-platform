3ec53e46b7e03b48bc0eb4444845c8d1252beaf6
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2024-06-05 15:13:55 +0100

    Added observability platform

---
+++ terraform/environments/core-network-services/observability.tf
@@ -0,0 +1,11 @@
+module "observability_platform_tenant" {
+  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
+  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
+
+  source  = "ministryofjustice/observability-platform-tenant/aws"
+  version = "1.2.0"
+
+  observability_platform_account_id = < a reference to 319748487814>
+
+  tags = local.tags
+}
