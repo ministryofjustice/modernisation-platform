e828ee57eab0a7c8f7777329668b4bfc1ce640ec
mikereiddigital <mike.reid@digital.justice.gov.uk>
2024-06-05 11:13:24 +0100

    Further testing

--- terraform/environments/bootstrap/member-bootstrap/iam.tf
+++ terraform/environments/bootstrap/member-bootstrap/iam.tf
@@ -245,7 +245,7 @@ data "aws_iam_policy_document" "member-access" {
     resources = ["*"] #tfsec:ignore:AWS099 tfsec:ignore:AWS097
     condition {
       test     = "ForAllValues:StringEquals"
-      variable = "cloudformation:TemplateResourceTypes"
+      variable = "cloudformation:ResourceTypes"
       values   = ["AWS::WAFv2::WebACL"]
     }
   }
@@ -259,7 +259,7 @@ data "aws_iam_policy_document" "member-access" {
     resources = ["*"] #tfsec:ignore:AWS099 tfsec:ignore:AWS097
     condition {
       test     = "ForAllValues:StringNotEquals"
-      variable = "cloudformation:TemplateResourceTypes"
+      variable = "cloudformation:ResourceTypes"
       values   = ["AWS::WAFv2::WebACL"]
     }
   }
