eb32fdfe24b1fe9d13e2f71b35fc062efc3bba73
mikereiddigital <mike.reid@digital.justice.gov.uk>
2024-06-11 14:40:31 +0100

    further testing

--- terraform/environments/bootstrap/member-bootstrap/iam.tf
+++ terraform/environments/bootstrap/member-bootstrap/iam.tf
@@ -258,9 +258,9 @@ data "aws_iam_policy_document" "member-access" {
       ]
       resources = ["*"] #tfsec:ignore:AWS099 tfsec:ignore:AWS097
       condition {
-        test     = "ForAllValues:StringEquals"
+        test     = "StringEquals"
         variable = "cloudformation:ResourceTypes"
-        values   = [ "AWS::WAFv2::WebACL", "AWS::S3::Bucket" ]
+        values   = [ "AWS::WAFv2::WebACL" ]
       }
   }
 
