b0ece02ec0e1e281ba852c62aaf64e4850c90f60
Fani Foteva <fani.foteva@digitaljustice.gov.uk>
2024-07-01 21:33:12 +0100

    LAWS-3933 Add secret pd edw
    Signed-off-by: Fani Foteva <fani.foteva@digitaljustice.gov.uk>

--- terraform/pagerduty/aws.tf
+++ terraform/pagerduty/aws.tf
@@ -74,6 +74,8 @@ resource "aws_secretsmanager_secret_version" "pagerduty_integration_keys" {
     laa_apex_prod_alarms                 = pagerduty_service_integration.apex_prod.integration_key
     delius_mis_nonprod_alarms            = pagerduty_service_integration.delius_mis_non_prod.integration_key
     delius_mis_prod_alarms               = pagerduty_service_integration.delius_mis_prod.integration_key
+    laa_edw_nonprod_alarms               = pagerduty_service_integration.laa_edw_nonprod_cloudwatch.integration_key
+    laa_edw_prod_alarms                  = pagerduty_service_integration.laa_edw_prod_cloudwatch.integration_key
   })
 }
 
