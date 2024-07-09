8319702fb67a2255af7d6e240641c6c154178e90
Aaron Robinson <aaron.robinson@digital.justice.gov.uk>
2024-07-09 17:26:37 +0100

    update core-logging to use hashcommit

--- terraform/environments/core-logging/observability.tf
+++ terraform/environments/core-logging/observability.tf
@@ -2,8 +2,7 @@ module "observability_platform_tenant" {
   #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
   #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
 
-  source  = "ministryofjustice/observability-platform-tenant/aws"
-  version = "1.2.0"
+  source = "github.com/ministryofjustice/terraform-aws-observability-platform-tenant?ref=fbbe5c8282786bcc0a00c969fe598e14f12eea9b" # v1.2.0
 
   observability_platform_account_id = local.environment_management.account_ids["observability-platform-production"]
 
