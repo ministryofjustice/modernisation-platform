7bdbd94b6421a0c5c43d7927a6d6b874437f9d89
Sukesh <sukesh.reddygade@digital.justice.gov.uk>
2024-08-08 17:36:19 +0100

    test

--- terraform/environments/bootstrap/member-bootstrap/iam.tf
+++ terraform/environments/bootstrap/member-bootstrap/iam.tf
@@ -725,5 +725,5 @@ module "shield_response_team_role" {
 # Manages the account alias for the AWS Account.
 resource "aws_iam_account_alias" "alias" {
   count         = (local.account_data.account-type != "member-unrestricted") && !(contains(local.skip_alias, terraform.workspace)) ? 1 : 0
-  account_alias = terraform.workspace
+  account_alias = "laa-mail-delay-development-temp"
 }
