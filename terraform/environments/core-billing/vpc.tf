cc4d8953078d4e6abf0e0616d2daec98ed3dded0
Aaron Robinson <aaron.robinson@digital.justice.gov.uk>
2024-08-23 13:49:47 +0100

    Create core-billing vpc

---
+++ terraform/environments/core-billing/vpc.tf
@@ -0,0 +1,31 @@
+locals {
+  networking = {
+    # live_data     = "10.21.0.0/19"
+    # non_live_data = "10.21.32.0/19"
+  }
+}
+
+module "vpc" {
+  #checkov:skip=CKV_TF_1:Local reference
+  for_each = local.networking
+  source   = "../../modules/vpc-hub"
+
+  # CIDRs
+  vpc_cidr = each.value
+
+  # private gateway type
+  #   nat = Nat Gateway
+  #   transit = Transit Gateway
+  #   none = no gateway for internal traffic
+  gateway = "transit"
+
+  # VPC Flow Logs
+  vpc_flow_log_iam_role = aws_iam_role.vpc_flow_log.arn
+
+  # Transit Gateway ID
+  transit_gateway_id = data.aws_ec2_transit_gateway.transit-gateway.id
+
+  # Tags
+  tags_common = local.tags
+  tags_prefix = each.key
+}
