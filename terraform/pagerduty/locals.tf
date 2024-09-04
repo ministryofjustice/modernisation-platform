f2d2a456b7c21d49f3072441382ec9239cc2d99e
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2024-09-04 11:29:26 +0100

    remove incomplete refs to stephen linden as these break the tf plan step

--- terraform/pagerduty/locals.tf
+++ terraform/pagerduty/locals.tf
@@ -41,7 +41,6 @@ locals {
 
   existing_users = {
     karen_botsh    = data.pagerduty_user.karen_botsh,
-    stephen_linden = data.pagerduty_user.stephen_linden,
     simon_pledger  = data.pagerduty_user.simon_pledger,
     mark_roberts   = data.pagerduty_user.mark_roberts,
     aaron_robinson = data.pagerduty_user.aaron_robinson,
@@ -53,7 +52,6 @@ locals {
   # oncall users local shortcut to make schedules a bit neater
   david_elliott    = pagerduty_user.pager_duty_users["david_elliott"].id
   david_sibley     = pagerduty_user.pager_duty_users["david_sibley"].id
-  stephen_linden   = data.pagerduty_user.stephen_linden.id
   edward_proctor   = pagerduty_user.pager_duty_users["edward_proctor"].id
   ewa_stempel      = pagerduty_user.pager_duty_users["ewa_stempel"].id
   mark_roberts     = data.pagerduty_user.mark_roberts.id
@@ -72,10 +70,6 @@ locals {
 }
 
 # existing users
-data "pagerduty_user" "stephen_linden" {
-  email = "stephen.linden${local.digital_email_suffix}"
-}
-
 data "pagerduty_user" "karen_botsh" {
   email = "karen.botsh${local.digital_email_suffix}"
 }
