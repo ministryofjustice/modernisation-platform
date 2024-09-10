6c05749da3638146d83a14bf557da25ab1995a72
Sukesh <sukesh.reddygade@digital.justice.gov.uk>
2024-09-10 14:02:47 +0100

    Added core-shared-service provider

--- terraform/environments/bootstrap/single-sign-on/providers.tf
+++ terraform/environments/bootstrap/single-sign-on/providers.tf
@@ -23,6 +23,9 @@ provider "aws" {
 
 # AWS provider for the Modernisation Platform, to get things from there if required
 provider "aws" {
-  alias  = "modernisation-platform"
+  alias  = "core-shared-services"
   region = "eu-west-2"
+  assume_role {
+    role_arn = "arn:aws:iam::${local.environment_management.account_ids["core-shared-services-production"]}:role/ModernisationPlatformAccess"
+  }
 }
