b253389e0b6398f6c42e01133163ae939ee08332
SteveLinden <stephen.linden@digital.justice.gov.uk>
2024-09-11 15:31:28 +0100

    Initial setup of Sprinkler

--- terraform/pagerduty/member-services-integrations.tf
+++ terraform/pagerduty/member-services-integrations.tf
@@ -2042,3 +2042,37 @@ resource "pagerduty_event_orchestration_service" "corporate-staff-rostering-prep
     actions {}
   }
 }
+
+resource "pagerduty_service" "sprinkler-development" {
+  name                    = "sptinkler-development"
+  description             = "sprinkler-development"
+  auto_resolve_timeout    = 345600
+  acknowledgement_timeout = "null"
+  escalation_policy       = pagerduty_escalation_policy.member_policy.id
+  alert_creation          = "create_alerts_and_incidents"
+}
+
+resource "pagerduty_service_integration" "sprinkler-integration" {
+  #name    = data.pagerduty_vendor.cloudwatch.name
+  service = pagerduty_service.sprinkler-development.id
+  #vendor  = data.pagerduty_vendor.cloudwatch.id
+}
+
+resource "pagerduty_slack_connection" "sprinkler_connection" {
+  source_id         = pagerduty_service.sprinkler-development.id
+  source_type       = "service_reference"
+  workspace_id      = local.slack_workspace_id
+  channel_id        = "C04QGQML68P"
+  notification_type = "responder"
+  lifecycle {
+    ignore_changes = [
+      config,
+    ]
+  }
+  config {
+    events = [
+      "incident.resolved"
+    ]
+    priorities = ["*"]
+  }
+}
