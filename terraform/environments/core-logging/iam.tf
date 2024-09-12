c108fa69c99aa8887c8374fdbbbff7f32be51c33
Aaron Robinson <aaron.robinson@digital.justice.gov.uk>
2024-09-12 16:15:56 +0100

    fixup typo

--- terraform/environments/core-logging/iam.tf
+++ terraform/environments/core-logging/iam.tf
@@ -57,7 +57,7 @@ resource "aws_iam_role_policy_attachment" "vpc_flow_log_publish_policy" {
 # Grafana-Athena Role
 resource "aws_iam_role" "grafana_athena" {
   name               = "grafana-athena"
-  assume_role_policy = data.aws_iam_policy_document.grafana-athena.json
+  assume_role_policy = data.aws_iam_policy_document.grafana_athena.json
 }
 
 # Grafana-Athena Policy
@@ -79,7 +79,7 @@ data "aws_iam_policy_document" "grafana_athena_policy" {
 
     principals {
       type        = "AWS"
-      identifiers = [aws_iam_role.ssm_role.arn]
+      identifiers = [aws_iam_role.grafana_athena.arn]
     }
   }
 }
