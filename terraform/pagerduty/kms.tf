a67af476278b1e0138c466ad5b8c75c2b8fce7ce
Kudzai Mtoko <kudzai.mtoko@digital.justice.gov.uk>
2024-09-13 15:02:17 +0100

    first commit

--- terraform/pagerduty/kms.tf
+++ terraform/pagerduty/kms.tf
@@ -20,3 +20,15 @@ resource "aws_kms_alias" "pagerduty_multi_region" {
   name          = "alias/pagerduty-secret-multi-region"
   target_key_id = aws_kms_key.pagerduty_multi_region.id
 }
+
+resource "aws_kms_replica_key" "pagerduty_multi_region_replica" {
+  description             = "AWS Secretsmanager CMK replica key"
+  deletion_window_in_days = 30
+  primary_key_arn         = aws_kms_key.pagerduty_multi_region.arn
+  provider                = aws.modernisation-platform-eu-west-1
+}
+
+resource "aws_kms_alias" "pagerduty_multi_region_replica" {
+  name          = "alias/pagerduty-secret-multi-region-replica"
+  target_key_id = aws_kms_replica_key.pagerduty_multi_region_replica.id
+}
