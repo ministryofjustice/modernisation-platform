e4169d67e40cd93225fee75d58eecb1172d41849
mark roberts <mark.roberts@digital.justice.gov.uk>
2024-09-19 10:02:38 +0100

    New NAT Alarms core network services

--- terraform/environments/core-network-services/data.tf
+++ terraform/environments/core-network-services/data.tf
@@ -21,3 +21,43 @@ data "aws_route" "live_data" {
 data "aws_kms_key" "general_shared" {
   key_id = "arn:aws:kms:eu-west-2:${local.environment_management.account_ids["core-shared-services-production"]}:alias/general-platforms"
 }
+
+# Data source to fetch information about the live VPC
+data "aws_vpc" "live" {
+  filter {
+    name   = "tag:Name"
+    values = ["live_data"]
+  }
+}
+
+# Data source to fetch information about the non-live VPC
+data "aws_vpc" "non_live" {
+  filter {
+    name   = "tag:Name"
+    values = ["non_live_data"]
+  }
+}
+
+# Data source to fetch existing NAT gateways in the live VPC
+data "aws_nat_gateways" "live" {
+  vpc_id = data.aws_vpc.live.id
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+# Data source to fetch existing NAT gateways in the non-live VPC
+data "aws_nat_gateways" "non_live" {
+  vpc_id = data.aws_vpc.non_live.id
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+data "aws_sns_topic" "security_hub_arn" {
+  name = "securityhub-alarms"
+}
