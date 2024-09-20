0ab33d42b6ba9660ff0e52f259cacb125792d813
David Sibley <david.sibley@digital.justice.gov.uk>
2024-09-20 16:01:54 +0100

    first pass at translating the configuration to send VPC flow logs to S3 across to this repository

--- terraform/modules/vpc-hub/main.tf
+++ terraform/modules/vpc-hub/main.tf
@@ -58,6 +58,9 @@ locals {
     }
   }
 
+  # Custom VPC flow log statement
+  custom_flow_log_format = "$${version} $${account-id} $${interface-id} $${srcaddr} $${dstaddr} $${srcport} $${dstport} $${protocol} $${packets} $${bytes} $${start} $${end} $${action} $${log-status} $${vpc-id} $${subnet-id} $${instance-id} $${tcp-flags} $${type} $${pkt-srcaddr} $${pkt-dstaddr} $${region} $${az-id} $${sublocation-type} $${sublocation-id} $${pkt-src-aws-service} $${pkt-dst-aws-service} $${flow-direction} $${traffic-path}"
+
 }
 
 #######
@@ -146,6 +149,23 @@ resource "aws_flow_log" "cloudwatch" {
     var.tags_common,
     {
       Name = "${var.tags_prefix}-vpc-flow-logs"
+    }
+  )
+}
+
+resource "aws_flow_log" "s3" {
+  for_each                 = var.flow_log_s3_destination_arn != "" ? toset([var.flow_log_s3_destination_arn]) : toset([])
+  log_destination          = each.key
+  log_destination_type     = "s3"
+  log_format               = local.custom_flow_log_format
+  max_aggregation_interval = "60"
+  traffic_type             = "ALL"
+  vpc_id                   = aws_vpc.default.id
+
+  tags = merge(
+    var.tags_common,
+    {
+      Name = "${var.tags_prefix}-vpc-flow-logs-s3"
     }
   )
 }
--- terraform/modules/vpc-inspection/main.tf
+++ terraform/modules/vpc-inspection/main.tf
@@ -23,6 +23,9 @@ locals {
     sync_state.availability_zone => sync_state.attachment[0].endpoint_id
   }
 
+  # Custom VPC flow log statement
+  custom_flow_log_format = "$${version} $${account-id} $${interface-id} $${srcaddr} $${dstaddr} $${srcport} $${dstport} $${protocol} $${packets} $${bytes} $${start} $${end} $${action} $${log-status} $${vpc-id} $${subnet-id} $${instance-id} $${tcp-flags} $${type} $${pkt-srcaddr} $${pkt-dstaddr} $${region} $${az-id} $${sublocation-type} $${sublocation-id} $${pkt-src-aws-service} $${pkt-dst-aws-service} $${flow-direction} $${traffic-path}"
+
 }
 
 ### VPC ###
@@ -70,6 +73,23 @@ resource "aws_flow_log" "cloudwatch" {
   tags = merge(
     var.tags_common,
     { Name = format("%s-vpc-flow-logs", var.tags_prefix) }
+  )
+}
+
+resource "aws_flow_log" "s3" {
+  for_each                 = var.flow_log_s3_destination_arn != "" ? toset([var.flow_log_s3_destination_arn]) : toset([])
+  log_destination          = each.key
+  log_destination_type     = "s3"
+  log_format               = local.custom_flow_log_format
+  max_aggregation_interval = "60"
+  traffic_type             = "ALL"
+  vpc_id                   = aws_vpc.main.id
+
+  tags = merge(
+    var.tags_common,
+    {
+      Name = "${var.tags_prefix}-vpc-flow-logs-s3-${random_string.main.result}"
+    }
   )
 }
 
