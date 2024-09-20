b3dba16b06893fef636f69ba6cd7e391ce67822f
David Sibley <david.sibley@digital.justice.gov.uk>
2024-09-20 13:11:59 +0100

    removed firehose logging of vpc flow logs

--- terraform/environments/core-vpc/logging.tf
+++ terraform/environments/core-vpc/logging.tf
@@ -5,11 +5,3 @@ module "logging-r53-resolver" {
   destination_bucket_arn     = local.cloudwatch_log_buckets["r53-resolver-logs"]
   tags                       = local.tags
 }
-
-module "logging-vpc-flow-logs" {
-  source                     = "github.com/ministryofjustice/modernisation-platform-terraform-aws-data-firehose?ref=2e58c8fd0b43ca8461dfd0c8cc5f43a1a9c49987" #v1.1.0
-  for_each                   = local.is-production ? { "build" = true } : {}
-  cloudwatch_log_group_names = local.cloudwatch_vpc_flow_log_groups
-  destination_bucket_arn     = local.cloudwatch_log_buckets["vpc-flow-logs"]
-  tags                       = local.tags
-}
