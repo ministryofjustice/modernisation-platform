8ba744fd02df47815416f38d193629a393d0a589
David Sibley <david.sibley@digital.justice.gov.uk>
2024-09-23 16:39:22 +0100

    incremented version of VPC module, removed locals referring explicitly to firehose creation

--- terraform/environments/core-vpc/locals.tf
+++ terraform/environments/core-vpc/locals.tf
@@ -11,13 +11,9 @@ locals {
 
   # This takes the name of the Terraform workspace (e.g. core-vpc-production), strips out the application name (e.g. core-vpc), and checks if
   # the string leftover is `-production`, if it isn't (e.g. core-vpc-non-production => -non-production) then it sets the var to false.
-  is-production = substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production"
-  is-live_data  = (substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production") || (substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-preproduction")
-
-  # This applies the same logic as above but to determine whether the environment is the development environment. Required for firehose resources.
+  is-production  = substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production"
   is-development = substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-development"
-  # Determines if Firehose should be built in an environment.
-  build_firehose = anytrue([local.is-development, local.is-production]) ? true : false
+  is-live_data   = (substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-production") || (substr(terraform.workspace, length(local.application_name), length(terraform.workspace)) == "-preproduction")
 
   # Secrets used by Firehose resources which we only require for development & production VPCs.
   xsiam                              = jsondecode(data.aws_secretsmanager_secret_version.xsiam_secret_arn_version.secret_string)
