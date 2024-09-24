71250acf224d1f144ca253ac1d87954ac31191d7
David Sibley <david.sibley@digital.justice.gov.uk>
2024-09-24 09:53:30 +0100

    removed random provider as not required by any objects in state

--- terraform/environments/core-shared-services/versions.tf
+++ terraform/environments/core-shared-services/versions.tf
@@ -4,10 +4,6 @@ terraform {
       version = "~> 5.0"
       source  = "hashicorp/aws"
     }
-    random = {
-      version = "~> 3.0"
-      source  = "hashicorp/random"
-    }
   }
   required_version = "~> 1.0"
 }
