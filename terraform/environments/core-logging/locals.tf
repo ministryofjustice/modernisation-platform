d4ea41d90facd77f35ce4cc2d9b42a64f7baca86
David Sibley <david.sibley@digital.justice.gov.uk>
2024-09-25 16:41:41 +0100

    added tags to resource share, added principal to allow accounts in MP OU to accept share

--- terraform/environments/core-logging/locals.tf
+++ terraform/environments/core-logging/locals.tf
@@ -4,6 +4,7 @@ data "aws_caller_identity" "modernisation-platform" {
 
 data "aws_organizations_organization" "root_account" {}
 
+
 locals {
   application_name           = "core-logging"
   environment_management     = jsondecode(data.aws_secretsmanager_secret_version.environment_management.secret_string)
