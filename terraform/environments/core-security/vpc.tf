6b6a105d36a5c3c94054e7fdf03636ef1e56e6e6
David Sibley <david.sibley@digital.justice.gov.uk>
2024-09-27 16:18:20 +0100

    added resolver logging config and applied some linting fixes

--- terraform/environments/core-security/vpc.tf
+++ terraform/environments/core-security/vpc.tf
@@ -20,7 +20,7 @@ module "vpc" {
   gateway = "transit"
 
   # VPC Flow Logs
-  vpc_flow_log_iam_role = aws_iam_role.vpc_flow_log.arn
+  vpc_flow_log_iam_role       = aws_iam_role.vpc_flow_log.arn
   flow_log_s3_destination_arn = each.key == "live_data" ? local.cloudwatch_log_buckets["vpc-flow-logs"] : ""
 
   # Transit Gateway ID
