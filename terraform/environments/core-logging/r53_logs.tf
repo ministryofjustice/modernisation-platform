8e4ba0a5b5547d0c9e1f614096431f7974ffdf63
David Sibley <david.sibley@digital.justice.gov.uk>
2024-09-27 11:49:23 +0100

    Addressed SCA failures

--- terraform/environments/core-logging/r53_logs.tf
+++ terraform/environments/core-logging/r53_logs.tf
@@ -50,6 +50,9 @@ resource "aws_kms_key" "r53_resolver_logs" {
 }
 
 data "aws_iam_policy_document" "r53_resolver_logs_kms" {
+  #checkov:skip=CKV_AWS_109:"Policy is directly related to the resource"
+  #checkov:skip=CKV_AWS_111:"Policy is directly related to the resource"
+  #checkov:skip=CKV_AWS_356:"Policy is directly related to the resource"
   statement {
     sid    = "Allow management access of the key to the logging account"
     effect = "Allow"
