c90a2411537c3a2bea9b3a8da02113ea877d6012
David Sibley <david.sibley@digital.justice.gov.uk>
2024-09-27 15:52:06 +0100

    log live_data resolver log queries to core-logging

--- terraform/environments/core-network-services/logging.tf
+++ terraform/environments/core-network-services/logging.tf
@@ -5,3 +5,32 @@ module "logging-generic-logs" {
   destination_bucket_arn     = local.cloudwatch_log_buckets["generic-logs"]
   tags                       = local.tags
 }
+
+locals {
+  resolver_query_log_config_names = toset(["core-logging-rlq-cloudwatch", "core-logging-rlq-s3"])
+  vpc_ids                         = { for key, value in module.vpc_inspection : key => value["vpc_id"] if key == "live_data" }
+  rlq_ids                         = { for name, config in data.aws_route53_resolver_query_log_config.core_logging : name => config.id }
+  vpc_rlq_associations = merge([
+    for vpc_key, vpc_id in local.vpc_ids : {
+      for rlq_name, rlq_id in local.rlq_ids :
+      "${vpc_key}_${rlq_name}" => {
+        vpc_id = vpc_id
+        rlq_id = rlq_id
+      }
+    }
+  ]...)
+}
+
+data "aws_route53_resolver_query_log_config" "core_logging" {
+  for_each = local.resolver_query_log_config_names
+  filter {
+    name   = "Name"
+    values = [each.value]
+  }
+}
+
+resource "aws_route53_resolver_query_log_config_association" "core_logging" {
+  for_each                     = local.is-production ? local.vpc_rlq_associations : {}
+  resolver_query_log_config_id = each.value.rlq_id
+  resource_id                  = each.value.vpc_id
+}
