9c873a783cb9b810dcfbb6246b951d67b7d19293
David Sibley <david.sibley@digital.justice.gov.uk>
2024-09-30 21:57:16 +0100

    removed deprecated secret used to store bucket ARNs

--- terraform/environments/core-network-services/secrets.tf
+++ terraform/environments/core-network-services/secrets.tf
@@ -21,17 +21,6 @@ data "aws_secretsmanager_secret_version" "pagerduty_integration_keys" {
   secret_id = data.aws_secretsmanager_secret.pagerduty_integration_keys.id
 }
 
-# Get the ARNs of the logging buckets in `core-logging`
-data "aws_secretsmanager_secret" "core_logging_bucket_arns" {
-  provider = aws.modernisation-platform
-  name     = "core_logging_bucket_arns"
-}
-
-data "aws_secretsmanager_secret_version" "core_logging_bucket_arns" {
-  provider  = aws.modernisation-platform
-  secret_id = data.aws_secretsmanager_secret.core_logging_bucket_arns.id
-}
-
 # Environment logging secret KMS key
 resource "aws_kms_key" "environment_logging" {
   description             = "environment-logging"
