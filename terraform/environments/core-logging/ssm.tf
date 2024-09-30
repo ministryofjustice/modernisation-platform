cb229a89a5a06e165071297a4bcafd03195d639d
David Sibley <david.sibley@digital.justice.gov.uk>
2024-09-30 14:59:06 +0100

    properly set insecure values as insecure

--- terraform/environments/core-logging/ssm.tf
+++ terraform/environments/core-logging/ssm.tf
@@ -3,10 +3,23 @@ resource "aws_ssm_parameter" "cortex_account_id" {
   lifecycle {
     ignore_changes = [value]
   }
+  provider       = aws.modernisation-platform
+  description    = "Account ID for Palo Alto Cortex XSIAM cross-account role."
+  name           = "cortex_account_id"
+  type           = "String"
+  insecure_value = "Placeholder"
+  tags           = local.tags
+}
+
+resource "aws_ssm_parameter" "core_logging_bucket_arns" {
+  #checkov:skip=CKV2_AWS_34: "Parameter is not sensitive; bucket ARNs are stored here for programmatic retrieval."
   provider    = aws.modernisation-platform
-  description = "Account ID for Palo Alto XSIAM Cortex cross-account role."
-  name        = "cortex_account_id"
+  description = "Bucket ARNs in core-logging for Palo Alto Cortex XSIAM."
+  name        = "core_logging_bucket_arns"
   type        = "String"
-  value       = "Placeholder"
-  tags        = local.tags
+  insecure_value = jsonencode({
+    for key in local.cortex_logging_buckets :
+    key => aws_s3_bucket.logging[key].arn
+  })
+  tags = local.tags
 }
