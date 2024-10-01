6581fd772634482c4795a812338477d91964052a
David Elliott <david.elliott@digital.justice.gov.uk>
2024-10-01 15:20:52 +0100

    Bump baselines for all bootstrap
    Bump remaining baselines after testing on mp account.

--- terraform/environments/bootstrap/secure-baselines/main.tf
+++ terraform/environments/bootstrap/secure-baselines/main.tf
@@ -6,7 +6,7 @@ data "aws_kms_key" "cloudtrail_key" {
 
 #trivy:ignore:AVD-AWS-0136
 module "baselines" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-baselines?ref=7a44478e0f15d0158cb06a2e8ab8f95448a796cd" # v7.7.0
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-baselines?ref=9d62fce5d0493e7da26f08c6331c627c4ae2ddfb" # v7.7.1
 
   providers = {
     # Default and replication regions
