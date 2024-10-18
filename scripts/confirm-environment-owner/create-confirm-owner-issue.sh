2657983a059abf2c01b8d7fc7df5c3c4d3bb1848
mikereiddigital <mike.reid@justice.gov.uk>
2024-10-18 13:24:25 +0100

    bug

--- scripts/confirm-environment-owner/create-confirm-owner-issue.sh
+++ scripts/confirm-environment-owner/create-confirm-owner-issue.sh
@@ -34,8 +34,8 @@ jq -c '.[]' "output.json" | while IFS= read -r row; do
                 --title "Confirmation of Owner Details Required - Environment: $file" \
                 --label security \
                 --project "Modernisation Platform" \
+                --body "Test Issue Please Ignore" \
                 # --body "Can you please review the contact details provided in the Owner field in environments/$file.json and create a PR if an update is necessary. Consult [this documentation](https://user-guide.modernisation-platform.service.justice.gov.uk/runbooks/reviewing-owners.html#review-owner)" \
-                -- body "Test Issue Please Ignore" \
             )
 
             echo "$issue_url"
