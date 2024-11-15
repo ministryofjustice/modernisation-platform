1db2bf725db4f7de1180618fb60f671e5f054f29
mikereiddigital <mike.reid@justice.gov.uk>
2024-11-15 11:26:52 +0000

    Fixes the interpolation warning.

--- terraform/environments/core-logging/cortex_alarms.tf
+++ terraform/environments/core-logging/cortex_alarms.tf
@@ -133,7 +133,7 @@ resource "aws_sns_topic" "cortex_sqs_sns_topic" {
 module "mp-sqs-sns-chatbot" {
   for_each = { for topic in local.cortex_topic_names : topic.name => topic }
   source = "github.com/ministryofjustice/modernisation-platform-terraform-aws-chatbot?ref=73280f80ce8a4557cec3a76ee56eb913452ca9aa" // v2.0.0
-  slack_channel_id = "${each.value.channel_id}"
+  slack_channel_id = each.value.channel_id
   sns_topic_arns   = ["arn:aws:sns:eu-west-2:${local.environment_management.account_ids[terraform.workspace]}:${aws_sns_topic.cortex_sqs_sns_topic[each.key].name}"]
   tags             = local.tags
   application_name = "${each.value.name}-sqs-alarm-chatbot"
