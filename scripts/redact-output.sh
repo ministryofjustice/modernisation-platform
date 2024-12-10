d6d757933a4f11bb71b77882eb6cc75ead46adca
Mark Roberts <mark.roberts@digital.justice.gov.uk>
2024-12-10 13:16:02 +0000

    changes

--- scripts/redact-output.sh
+++ scripts/redact-output.sh
@@ -2,10 +2,10 @@
 
 # Based on: https://github.com/ministryofjustice/opg-org-infra/blob/main/scripts/redact_output.sh
 
-sed -e 's/AWS_SECRET_ACCESS_KEY".*/<REDACTED>/g' \
-    -e 's/AWS_ACCESS_KEY_ID".*/<REDACTED>/g' \
-    -e 's/$AWS_SECRET_ACCESS_KEY".*/<REDACTED>/g' \
-    -e 's/$AWS_ACCESS_KEY_ID".*/<REDACTED>/g' \
-    -e 's/\[id=.*\]/\[id=<REDACTED>\]/g' \
-    -e 's/::[0-9]\{12\}:/::REDACTED:/g' \
-    -e 's/:[0-9]\{12\}:/:REDACTED:/g'
+sed -u -e 's/AWS_SECRET_ACCESS_KEY".*/<REDACTED>/g' \
+    -u -e 's/AWS_ACCESS_KEY_ID".*/<REDACTED>/g' \
+    -u-e 's/$AWS_SECRET_ACCESS_KEY".*/<REDACTED>/g' \
+    -u -e 's/$AWS_ACCESS_KEY_ID".*/<REDACTED>/g' \
+    -u -e 's/\[id=.*\]/\[id=<REDACTED>\]/g' \
+    -u -e 's/::[0-9]\{12\}:/::REDACTED:/g' \
+    -u -e 's/:[0-9]\{12\}:/:REDACTED:/g'
