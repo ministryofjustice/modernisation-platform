6adf04f0c65161b04a1ce6e953bd81768348e381
Mark Roberts <mark.roberts@digital.justice.gov.uk>
2024-12-10 15:46:10 +0000

    changes to redact

--- scripts/redact-output.sh
+++ scripts/redact-output.sh
@@ -2,10 +2,10 @@
 
 # Based on: https://github.com/ministryofjustice/opg-org-infra/blob/main/scripts/redact_output.sh
 
-sed -u -e 's/AWS_SECRET_ACCESS_KEY".*/<REDACTED>/g' \
-    -u -e 's/AWS_ACCESS_KEY_ID".*/<REDACTED>/g' \
-    -u -e 's/$AWS_SECRET_ACCESS_KEY".*/<REDACTED>/g' \
-    -u -e 's/$AWS_ACCESS_KEY_ID".*/<REDACTED>/g' \
-    -u -e 's/\[id=.*\]/\[id=<REDACTED>\]/g' \
-    -u -e 's/::[0-9]\{12\}:/::REDACTED:/g' \
-    -u -e 's/:[0-9]\{12\}:/:REDACTED:/g'
+sed -u -E "s/AWS_SECRET_ACCESS_KEY=.*/AWS_SECRET_ACCESS_KEY=<REDACTED>/g" \
+    -u -E "s/AWS_ACCESS_KEY_ID=.*/AWS_ACCESS_KEY_ID=<REDACTED>/g" \
+    -u -E "s/\$(AWS_SECRET_ACCESS_KEY)=.*/\$(AWS_SECRET_ACCESS_KEY)=<REDACTED>/g" \
+    -u -E "s/\$(AWS_ACCESS_KEY_ID)=.*/\$(AWS_ACCESS_KEY_ID)=<REDACTED>/g" \
+    -u -E "s/\[id=[^]]*\]/\[id=<REDACTED>]/g" \
+    -u -E "s/::[0-9]{12}:/::REDACTED:/g" \
+    -u -E "s/:[0-9]{12}:/:REDACTED:/g"
