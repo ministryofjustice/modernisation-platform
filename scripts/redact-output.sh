79036f3d22c3b2e2f9781a4f9e63b45c4df3343e
Mark Roberts <mark.roberts@digital.justice.gov.uk>
2024-12-10 13:17:39 +0000

    missed a space

--- scripts/redact-output.sh
+++ scripts/redact-output.sh
@@ -4,7 +4,7 @@
 
 sed -u -e 's/AWS_SECRET_ACCESS_KEY".*/<REDACTED>/g' \
     -u -e 's/AWS_ACCESS_KEY_ID".*/<REDACTED>/g' \
-    -u-e 's/$AWS_SECRET_ACCESS_KEY".*/<REDACTED>/g' \
+    -u -e 's/$AWS_SECRET_ACCESS_KEY".*/<REDACTED>/g' \
     -u -e 's/$AWS_ACCESS_KEY_ID".*/<REDACTED>/g' \
     -u -e 's/\[id=.*\]/\[id=<REDACTED>\]/g' \
     -u -e 's/::[0-9]\{12\}:/::REDACTED:/g' \
