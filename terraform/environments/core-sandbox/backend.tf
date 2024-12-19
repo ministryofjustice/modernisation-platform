b33611549275f543c7d62d3949322d4d18f7271e
Rich Green <richard.green@digital.justice.gov.uk>
2024-12-19 11:04:47 +0000

    Temporarily suppress CKV_TF_3

--- terraform/environments/core-sandbox/backend.tf
+++ terraform/environments/core-sandbox/backend.tf
@@ -2,6 +2,7 @@
 terraform {
   # `backend` blocks do not support variables, so the following are hard-coded here:
   # - S3 bucket name, which is created in modernisation-platform-account/s3.tf
+  #checkov:skip=CKV_TF_3:Ensure state files are locked - temporarily suppressed pending issue #8789
   backend "s3" {
     acl                  = "bucket-owner-full-control"
     bucket               = "modernisation-platform-terraform-state"
