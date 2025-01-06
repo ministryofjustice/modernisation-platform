d1559bd6c2f48127ed336dcfa2e008c9b74dc178
Sukesh <sukesh.reddygade@digital.justice.gov.uk>
2025-01-06 14:46:35 +0000

    Add Athena setup for querying member information

---
+++ terraform/modernisation-platform-account/athena.tf
@@ -0,0 +1,68 @@
+resource "aws_athena_workgroup" "member_information" {
+  name        = "member_information_workgroup"
+  state       = "ENABLED"
+  description = "Athena Workgroup for CSV queries"
+
+  configuration {
+    result_configuration {
+      output_location = "s3://${module.member_information_bucket.bucket.bucket}/query-results/"
+    }
+  }
+}
+
+resource "aws_glue_catalog_database" "member_information" {
+  name = "member_information_db"
+}
+
+resource "aws_glue_catalog_table" "member_information" {
+  name          = "member_information"
+  database_name = aws_glue_catalog_database.member_information.name
+  table_type    = "EXTERNAL_TABLE"
+
+  storage_descriptor {
+    columns {
+      name = "accountname"
+      type = "string"
+    }
+    columns {
+      name = "awsaccountid"
+      type = "string"
+    }
+    columns {
+      name = "slackchannel"
+      type = "string"
+    }
+    columns {
+      name = "infrastructuresupportemail"
+      type = "string"
+    }
+    columns {
+      name = "incidenthours"
+      type = "string"
+    }
+    columns {
+      name = "incidentcontactdetails"
+      type = "string"
+    }
+    columns {
+      name = "serviceurls"
+      type = "string"
+    }
+
+    ser_de_info {
+      name                  = "CSV Serde"
+      serialization_library = "org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe"
+      parameters = {
+        "separatorChar"           = ","
+        "quoteChar"               = "\""
+        "field.delim"             = ","
+        "skip.header.line.count"  = "1"
+        "escapeChar"              = "\\"
+      }
+    }
+
+    location = "s3://${module.member_information_bucket.bucket.bucket}/data/"
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+  }
+}
