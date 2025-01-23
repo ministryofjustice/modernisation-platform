6c8d768017ce1d334ed9574ee234db929383cc45
Rich Green <richard.green@digital.justice.gov.uk>
2025-01-23 16:36:55 +0000

    associate non-production VPCs with cloudwatch rqlc only

--- terraform/environments/core-vpc/logging.tf
+++ terraform/environments/core-vpc/logging.tf
@@ -11,6 +11,15 @@ locals {
       }
     }
   ]...)
+  vpc_rlq_associations_cloudwatch_only = merge([
+    for vpc_key, vpc_id in local.vpc_ids : {
+      for rlq_name, rlq_id in local.rlq_ids :
+      "${vpc_key}_${rlq_name}" => {
+        vpc_id = vpc_id
+        rlq_id = rlq_id
+      } if rlq_name == "core-logging-rlq-cloudwatch"
+    }
+  ]...)
 }
 
 data "aws_route53_resolver_query_log_config" "core_logging" {
@@ -22,7 +31,7 @@ data "aws_route53_resolver_query_log_config" "core_logging" {
 }
 
 resource "aws_route53_resolver_query_log_config_association" "core_logging" {
-  for_each                     = local.is-production ? local.vpc_rlq_associations : {}
+  for_each                     = local.is-production ? local.vpc_rlq_associations : local.vpc_rlq_associations_cloudwatch_only
   resolver_query_log_config_id = each.value.rlq_id
   resource_id                  = each.value.vpc_id
 }
