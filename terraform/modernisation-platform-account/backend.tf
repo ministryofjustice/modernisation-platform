8da31c8bf0b8886ced65512250a4d940935020b3
David Sibley <david.sibley@digital.justice.gov.uk>
2025-01-23 17:18:18 +0000

    set modernisation platform account to use native locking

--- terraform/modernisation-platform-account/backend.tf
+++ terraform/modernisation-platform-account/backend.tf
@@ -3,11 +3,11 @@ terraform {
   # - S3 bucket name, which is created in s3.tf
   # - DynamoDB table name, which is created in dynamodb.tf
   backend "s3" {
-    acl            = "bucket-owner-full-control"
-    bucket         = "modernisation-platform-terraform-state"
-    dynamodb_table = "modernisation-platform-terraform-state-lock"
-    encrypt        = true
-    key            = "modernisation-platform-account/terraform.tfstate"
-    region         = "eu-west-2"
+    acl          = "bucket-owner-full-control"
+    bucket       = "modernisation-platform-terraform-state"
+    encrypt      = true
+    key          = "modernisation-platform-account/terraform.tfstate"
+    region       = "eu-west-2"
+    use_lockfile = true
   }
 }
