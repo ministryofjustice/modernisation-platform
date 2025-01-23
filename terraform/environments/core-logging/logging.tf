f049e0a0c9c38c5e09c219734f51ef6944494838
Rich Green <richard.green@digital.justice.gov.uk>
2025-01-23 11:08:45 +0000

    add query logging for `non_live_data` vpc and monitor cw logs for r53 dns firewall matches to be alerted to sns/pagerduty

--- terraform/environments/core-logging/logging.tf
+++ terraform/environments/core-logging/logging.tf
@@ -1,6 +1,6 @@
 locals {
   resolver_query_log_config_names = toset(["core-logging-rlq-cloudwatch", "core-logging-rlq-s3"])
-  vpc_ids                         = { for key, value in module.vpc : key => value["vpc_id"] if key == "live_data" }
+  vpc_ids                         = { for key, value in module.vpc : key => value["vpc_id"] if key == "live_data" || key == "non_live_data" }
   rlq_ids                         = { for name, config in data.aws_route53_resolver_query_log_config.core_logging : name => config.id }
   vpc_rlq_associations = merge([
     for vpc_key, vpc_id in local.vpc_ids : {
