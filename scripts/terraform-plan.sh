6be0570745cbe209592120bc7959966f77e6067b
mikereiddigital <mike.reid@justice.gov.uk>
2025-01-23 20:20:14 +0000

    Final Format

--- scripts/terraform-plan.sh
+++ scripts/terraform-plan.sh
@@ -38,8 +38,6 @@ else
     echo "$plan_output"                   # Output full redacted plan to stdout (GitHub Actions logs)
 fi
 
-echo "plan_destroy_check=$plan_summary" >> "$GITHUB_OUTPUT"
-
 echo "summary<<EOF" >> $GITHUB_OUTPUT
 echo "$plan_summary" >> $GITHUB_OUTPUT
 echo "EOF" >> $GITHUB_OUTPUT
