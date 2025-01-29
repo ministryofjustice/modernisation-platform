38ff389a17befc31813c8ee2045b258dad60aeb3
Rich Green <richard.green@digital.justice.gov.uk>
2025-01-29 11:01:21 +0000

    try feeding ram resource share as map

--- terraform/modules/ram-resource-share/main.tf
+++ terraform/modules/ram-resource-share/main.tf
@@ -1,3 +1,6 @@
+locals {
+  resource_arns_map = { for idx, arn in var.resource_arns : idx => arn }
+}
 resource "aws_ram_resource_share" "default" {
   name                      = "${var.tags_prefix}-resource-share"
   allow_external_principals = false
@@ -10,7 +13,7 @@ resource "aws_ram_resource_share" "default" {
 }
 
 resource "aws_ram_resource_association" "default" {
-  for_each = toset(var.resource_arns)
+  for_each = local.resource_arns_map
 
   resource_arn       = each.value
   resource_share_arn = aws_ram_resource_share.default.arn
