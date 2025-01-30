bb496acd2b4c3ddff6841bccef6a03192285b30a
Sukesh <sukesh.reddygade@digital.justice.gov.uk>
2025-01-30 15:51:38 +0000

    Add template files for modernisation-platform-environments-components

---
+++ terraform/templates/modernisation-platform-environments-components/versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 5.0"
+      source  = "hashicorp/aws"
+    }
+    dns = {
+      version = "~> 3.0"
+      source  = "hashicorp/dns"
+    }
+    external = {
+      version = "~> 2.0"
+      source  = "hashicorp/external"
+    }
+    http = {
+      version = "~> 3.0"
+      source  = "hashicorp/http"
+    }
+  }
+  required_version = "~> 1.10"
+}
