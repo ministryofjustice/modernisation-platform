67527e42dd9bc49f3cfd2544ea195059920ef7b5
khatraf <khatra.farah@digital.justice.gov.uk>
2025-03-03 13:38:52 +0000

    first pass

--- terraform/environments/core-shared-services/iam.tf
+++ terraform/environments/core-shared-services/iam.tf
@@ -360,3 +360,109 @@ resource "aws_iam_role_policy_attachment" "lambda_invoke_policy_attachment" {
 }
 
 ## END: IAM for EventBridge Scheduler to trigger Lambda Function
+
+
+#  IAM role dedicated to AWS GuardDuty S3 Malware Scanning
+
+data "aws_iam_policy_document" "guardduty_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["malware-protection-plan.guardduty.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "guardduty_s3_malware_scan" {
+  name               = "GuardDutyS3MalwareProtection"
+  assume_role_policy = data.aws_iam_policy_document.guardduty_assume_role.json
+}
+
+module "guardduty_s3_malware_scan" {
+  count      = (local.account_data.account-type == "member" && terraform.workspace != "testing-test" && !can(regex(".*-production$", terraform.workspace))) ? 1 : 0
+  source     = "github.com/ministryofjustice/modernisation-platform-terraform-cross-account-access?ref=6819b090bce6d3068d55c7c7b9b3fd18c9dca648"
+  account_id = local.environment_management.account_ids["core-shared-services-production"]
+  policy_arn = aws_iam_policy.guardduty_s3_malware_scan[0].id
+  role_name  = aws_iam_role.guardduty_s3_malware_scan.name
+}
+
+# IAM Policy Document for GuardDuty S3 Malware Scan Role
+data "aws_iam_policy_document" "guardduty_s3_malware_scan" {
+  statement {
+    sid    = "EventBridgeActionsForGuardDuty"
+    effect = "Allow"
+    actions = [
+      "events:PutRule",
+      "events:PutTargets",
+      "events:DescribeRule"
+    ]
+    resources = [
+      "arn:aws:events:eu-west-2:${data.aws_caller_identity.current.account_id}:rule/DO-NOT-DELETE-AmazonGuardDutyMalwareProtectionS3*:rule/DO-NOT-DELETE-AmazonGuardDutyMalwareProtectionS3*"
+    ]
+    condition {
+      test     = "StringLike"
+      variable = "events:ManagedBy"
+      values   = ["malware-protection-plan.guardduty.amazonaws.com"]
+    }
+  }
+  statement {
+    sid    = "S3ActionsForGuardDuty"
+    effect = "Allow"
+    actions = [
+      "s3:PutObject*",
+      "s3:GetObject*",
+      "s3:GetBucket*",
+      "s3:ListBucket",
+      "s3:PutBucketNotification",
+      "s3:GetBucketNotification"
+    ]
+    resources = [
+      "arn:aws:s3:::*/malware-protection-resource-validation-object",
+      "arn:aws:s3:::*"
+    ]
+    condition {
+      test     = "StringEqualsIfExists"
+      variable = "aws:CalledVia"
+      values   = ["malware-protection-plan.guardduty.amazonaws.com"]
+    }
+  }
+  statement {
+    sid    = "AllowDecryptForMalwareScan"
+    effect = "Allow"
+    actions = [
+      "kms:GenerateDataKey",
+      "kms:Decrypt"
+    ]
+    resources = ["*"]
+    condition {
+      test     = "StringLike"
+      variable = "kms:ViaService"
+      values   = ["s3.eu-west-2.amazonaws.com"]
+    }
+  }
+  statement {
+    sid    = "Guardduty actions"
+    effect = "Allow"
+    actions = [
+      "guardduty:Describe*",
+      "guardduty:Get*",
+      "guardduty:List*"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid       = "AllowPassGuardDutyRole"
+    effect    = "Allow"
+    actions   = ["iam:PassRole"]
+    resources = ["arn:aws:iam::*:role/GuardDutyS3MalwareProtection"]
+    condition {
+      test     = "StringEqualsIfExists"
+      variable = "iam:PassedToService"
+      values   = ["malware-protection-plan.guardduty.amazonaws.com"]
+    }
+  }
+}
