b1da24b6f8d5915ef26bbbbf9cac770a07362642
David Sibley <david.sibley@digital.justice.gov.uk>
2025-03-13 12:57:23 +0000

    updated reference to tagged version now that a new tag has been deployed in modernisation-platform-terraform-environments

--- terraform/environments/environments.tf
+++ terraform/environments/environments.tf
@@ -1,5 +1,5 @@
 module "environments" {
-  source                             = "github.com/ministryofjustice/modernisation-platform-terraform-environments?ref=32119ae1515dce784578a86e57c54471c0da86bb" # v7.0.0
+  source                             = "github.com/ministryofjustice/modernisation-platform-terraform-environments?ref=0f15745396a2da4627f10ad48190e07032bb9888" # v7.0.0
   environment_directory              = "../../environments"
   environment_parent_organisation_id = local.modernisation_platform_ou_id
   environment_prefix                 = "modernisation-platform"
