542ff8dfbdccb1e8f07d5436d7a9cea38c9ed1a1
David Sibley <david.sibley@digital.justice.gov.uk>
2025-03-13 10:51:22 +0000

    caught some small linting issues, pointed the environments at a theoretical new tag which does not presently exist

--- terraform/environments/environments.tf
+++ terraform/environments/environments.tf
@@ -1,5 +1,5 @@
 module "environments" {
-  source                             = "github.com/ministryofjustice/modernisation-platform-terraform-environments?ref=b17ca8d6c23a2c22d2efc6c5116a1328492c32bd" # v6.0.0
+  source                             = "github.com/ministryofjustice/modernisation-platform-terraform-environments?ref=32119ae1515dce784578a86e57c54471c0da86bb" # v7.0.0
   environment_directory              = "../../environments"
   environment_parent_organisation_id = local.modernisation_platform_ou_id
   environment_prefix                 = "modernisation-platform"
