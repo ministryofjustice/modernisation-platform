5a62a38ac389f6465ee615d469f021fd83c863bb
Mark Roberts <mark.roberts@digital.justice.gov.uk>
2025-03-25 15:45:36 +0000

    changes reverted for locals on single sign on

--- terraform/environments/bootstrap/single-sign-on/locals.tf
+++ terraform/environments/bootstrap/single-sign-on/locals.tf
@@ -1,13 +1,8 @@
 # This data sources allows us to get the Modernisation Platform account information for use elsewhere
 data "aws_caller_identity" "modernisation-platform" {
 }
-# data "aws_secretsmanager_secret_version" "organisation_security_id" {
-#   provider  = aws.modernisation-platform
-#   secret_id = "organisation_security_id" # Name of the secret in AWS Secrets Manager
-# }
-locals {
 
-  #organisation_security_account_id = jsondecode(data.aws_secretsmanager_secret_version.organisation_security_id.secret_string)["organisation_security_id"]
+locals {
 
   app_name = try(regex("^bichard*.|^remote-supervisio*.", terraform.workspace), replace(terraform.workspace, "/-([[:alnum:]]+)$/", ""))
 
