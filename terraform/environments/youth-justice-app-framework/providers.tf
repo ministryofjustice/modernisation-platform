607a863863aad4434fd35b5bb87d41491bea63ed
ep-93 <109581241+ep-93@users.noreply.github.com>
2025-03-27 14:59:18 +0000

    Update providers.tf

--- terraform/environments/youth-justice-app-framework/providers.tf
+++ terraform/environments/youth-justice-app-framework/providers.tf
@@ -42,7 +42,7 @@ provider "aws" {
 
 # Get secret by arn for environment management
 data "aws_secretsmanager_secret" "mod_plat_circleci" {
-  provider = aws.modernisation-platform
+  provider = aws.modernisation-secrets-read
   name     = "mod-platform-circleci"
 }
 
