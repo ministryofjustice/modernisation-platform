8844f70d958742d45093567bd61ace57f0905b5e
Kolade Adewuyi <kolade.adewuyi@digital.justice.gov.uk>
2025-05-06 09:32:56 +0100

    DPR2-1774: Update actions for reporting roles

--- terraform/environments/bootstrap/single-sign-on/policies.tf
+++ terraform/environments/bootstrap/single-sign-on/policies.tf
@@ -1213,6 +1213,7 @@ data "aws_iam_policy_document" "reporting-operations" {
       "dms:StopReplicationTask",
       "dms:TestConnection",
       "dms:ReloadTables",
+      "dms:RebootReplicationInstance",
       "sqlworkbench:CreateFolder",
       "sqlworkbench:PutTab",
       "sqlworkbench:BatchDeleteFolder",
