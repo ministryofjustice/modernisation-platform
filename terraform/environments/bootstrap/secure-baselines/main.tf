a1646be0af6c51fca1887e041f7a32c018fa2ece
mikereiddigital <mike.reid@justice.gov.uk>
2025-05-08 10:40:43 +0100

    Updates baselines to 7.13.6 which routes root account usage to high priority alarms channel.

--- terraform/environments/bootstrap/secure-baselines/main.tf
+++ terraform/environments/bootstrap/secure-baselines/main.tf
@@ -6,7 +6,7 @@ data "aws_kms_key" "cloudtrail_key" {
 
 #trivy:ignore:AVD-AWS-0136
 module "baselines" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-baselines?ref=73761b029ce904ed09131fa9e8ebbd6d2a6b19b9" # v7.13.5
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-baselines?ref=520a8640a5116a098d80d1bd7221c9864c07b31a" # v7.13.6
   providers = {
     # Default and replication regions
     aws                    = aws.workspace-eu-west-2
@@ -79,6 +79,9 @@ module "baselines" {
 
   # Pass in pagerduty integration key for security hub alerts
   pagerduty_integration_key = local.is_core_account ? local.pagerduty_integration_keys["security_hub"] : local.pagerduty_integration_keys["security_hub_members"]
+
+  # PagerDuty Key for High Priority Alarms
+  high_priority_pagerduty_integration_key = local.pagerduty_integration_keys["core_alerts_high_priority_cloudwatch"]
 }
 
 # Keys for pagerduty
