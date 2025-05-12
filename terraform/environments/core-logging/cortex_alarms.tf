114fb5012f019f62c57c9aae7e67b5f75c891564
Rich Green <richard.green@digital.justice.gov.uk>
2025-05-12 09:07:42 +0100

    Adds the IP usage alerts SNS topic only to modplatform chatbot config so we can receive alerts in the low priority alerts channel

--- terraform/environments/core-logging/cortex_alarms.tf
+++ terraform/environments/core-logging/cortex_alarms.tf
@@ -135,7 +135,10 @@ module "mp-sqs-sns-chatbot" {
   for_each         = { for topic in local.cortex_topic_names : topic.name => topic }
   source           = "github.com/ministryofjustice/modernisation-platform-terraform-aws-chatbot?ref=73280f80ce8a4557cec3a76ee56eb913452ca9aa" // v2.0.0
   slack_channel_id = each.value.channel_id
-  sns_topic_arns   = [aws_sns_topic.cortex_sqs_sns_topic[each.key].arn]
+  sns_topic_arns = each.key == "modplatform" ? [
+    aws_sns_topic.cortex_sqs_sns_topic[each.key].arn,
+    aws_sns_topic.ip_usage_alerts.arn
+  ] : [aws_sns_topic.cortex_sqs_sns_topic[each.key].arn]
   tags             = merge(local.tags, try(each.value.tags, {}))
   application_name = "${each.value.name}-sqs-alarm-chatbot"
 }
