e68c7b64f3a50893fe1e13672f5e51182c0054f4
Aaron Robinson <41325732+ASTRobinson@users.noreply.github.com>
2025-05-16 17:19:27 +0100

    Update route53-laa.tf
    add missing ttl value

--- terraform/environments/core-vpc/route53-laa.tf
+++ terraform/environments/core-vpc/route53-laa.tf
@@ -13,5 +13,6 @@ resource "aws_route53_record" "portal_oid" {
   name    = "portal-oid.aws.uat.legalservices.gov.uk"
   records = ["10.206.4.156", "10.206.6.93"]
   type    = "A"
+  ttl     = 300
   zone_id = aws_route53_zone.aws_uat_legalservices_gov_uk[0].zone_id
 }
