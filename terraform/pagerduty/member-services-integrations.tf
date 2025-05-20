bb1e170983bb1ac7f3f84b923098ed7a1f7351fa
mikereiddigital <mike.reid@justice.gov.uk>
2025-05-20 16:50:48 +0100

    PagerDuty resources for the LAA MAAT application

--- terraform/pagerduty/member-services-integrations.tf
+++ terraform/pagerduty/member-services-integrations.tf
@@ -2425,3 +2425,107 @@ resource "pagerduty_slack_connection" "performance_hub_prod" {
   }
 }
 # End Performance Hub ----------
+
+# LAA MAAT
+
+# LAA MAAT NonProd
+resource "pagerduty_service" "laa_maat_non_prod" {
+  name                    = "Legal Aid Agency MAAT Application NonProduction"
+  description             = "Legal Aid Agency MAAT Application NonProduction Alarms"
+  auto_resolve_timeout    = 345600
+  acknowledgement_timeout = "null"
+  escalation_policy       = pagerduty_escalation_policy.member_policy.id
+  alert_creation          = "create_alerts_and_incidents"
+}
+
+resource "pagerduty_service_integration" "laa_maat_non_prod_cloudwatch" {
+  name    = data.pagerduty_vendor.cloudwatch.name
+  service = pagerduty_service.laa_maat_non_prod.id
+  vendor  = data.pagerduty_vendor.cloudwatch.id
+}
+
+resource "pagerduty_slack_connection" "laa_maat_non_prod_connection" {
+  source_id         = pagerduty_service.laa_maat_non_prod.id
+  source_type       = "service_reference"
+  workspace_id      = local.slack_workspace_id
+  channel_id        = "C021QHTNNN9"
+  notification_type = "responder"
+  lifecycle {
+    ignore_changes = [
+      config,
+    ]
+  }
+  config {
+    events = [
+      "incident.triggered",
+      "incident.acknowledged",
+      "incident.escalated",
+      "incident.resolved",
+      "incident.reassigned",
+      "incident.annotated",
+      "incident.unacknowledged",
+      "incident.delegated",
+      "incident.priority_updated",
+      "incident.action_invocation.created",
+      "incident.action_invocation.terminated",
+      "incident.action_invocation.updated",
+      "incident.responder.added",
+      "incident.responder.replied",
+      "incident.status_update_published",
+      "incident.reopened"
+    ]
+    priorities = ["*"]
+  }
+}
+
+# LAA MAAT Prod
+resource "pagerduty_service" "laa_maat_prod" {
+  name                    = "Legal Aid Agency MAAT Application Production"
+  description             = "Legal Aid Agency MAAT Application Production Alarms"
+  auto_resolve_timeout    = 345600
+  acknowledgement_timeout = "null"
+  escalation_policy       = pagerduty_escalation_policy.member_policy.id
+  alert_creation          = "create_alerts_and_incidents"
+}
+
+resource "pagerduty_service_integration" "laa_maat_prod_cloudwatch" {
+  name    = data.pagerduty_vendor.cloudwatch.name
+  service = pagerduty_service.laa_maat_prod.id
+  vendor  = data.pagerduty_vendor.cloudwatch.id
+}
+
+resource "pagerduty_slack_connection" "laa_maat_prod_connection" {
+  source_id         = pagerduty_service.laa_maat_prod.id
+  source_type       = "service_reference"
+  workspace_id      = local.slack_workspace_id
+  channel_id        = "C021TL475NE"
+  notification_type = "responder"
+  lifecycle {
+    ignore_changes = [
+      config,
+    ]
+  }
+  config {
+    events = [
+      "incident.triggered",
+      "incident.acknowledged",
+      "incident.escalated",
+      "incident.resolved",
+      "incident.reassigned",
+      "incident.annotated",
+      "incident.unacknowledged",
+      "incident.delegated",
+      "incident.priority_updated",
+      "incident.action_invocation.created",
+      "incident.action_invocation.terminated",
+      "incident.action_invocation.updated",
+      "incident.responder.added",
+      "incident.responder.replied",
+      "incident.status_update_published",
+      "incident.reopened"
+    ]
+    priorities = ["*"]
+  }
+}
+
+# End of MAAT ------------------
