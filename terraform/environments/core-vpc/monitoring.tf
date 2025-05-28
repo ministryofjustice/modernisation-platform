deda16c5dab38b9f1be3b9015841089a4d649781
Kudzai-moj <kudzai.mtoko@digital.justice.gov.uk>
2025-05-28 12:08:51 +0100

    made changes

--- terraform/environments/core-vpc/monitoring.tf
+++ terraform/environments/core-vpc/monitoring.tf
@@ -131,7 +131,7 @@ resource "aws_cloudwatch_metric_alarm" "rejected_connections_alarm" {
   metric_query {
     id          = "ad1"
     expression  = "ANOMALY_DETECTION_BAND(m1, 2)"
-    label       = "Sum AcceptedTrafficCount ${each.key} GreaterThanUpperThreshold 1.0"
+    label       = "Sum RejectedConnections ${each.key} GreaterThanUpperThreshold 1.0"
     return_data = true
   }
 }
@@ -160,7 +160,7 @@ resource "aws_cloudwatch_metric_alarm" "ssh_connection_attempts_alarm" {
   metric_query {
     id          = "ad1"
     expression  = "ANOMALY_DETECTION_BAND(m1, 2)"
-    label       = "Sum AcceptedTrafficCount ${each.key} GreaterThanUpperThreshold 1.0"
+    label       = "Sum SSHConnectionAttempts ${each.key} GreaterThanUpperThreshold 1.0"
     return_data = true
   }
 }
