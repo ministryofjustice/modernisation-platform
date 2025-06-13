7965f6c2540ab69a3f35042193968468a2873cef
Sukesh <sukesh.reddygade@digital.justice.gov.uk>
2025-06-13 12:38:27 +0100

    Removed space between resource and checkov

--- terraform/environments/core-security/s3.tf
+++ terraform/environments/core-security/s3.tf
@@ -5,7 +5,6 @@
 # checkov:skip=CKV_AWS_144  Cross-region replication not required
 # checkov:skip=CKV_AWS_21   Versioning not needed for overwrite-safe JSON
 # checkov:skip=CKV2_AWS_61  No lifecycle needed for now
-
 resource "aws_s3_bucket" "route53_data" {
   bucket = "modernisation-platform-route53-data"
 }
