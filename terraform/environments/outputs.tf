0ac62b00058b1f6c9d0bea4f680aca2ec20215ba
Rich Green <richard.green@digital.justice.gov.uk>
2025-06-17 18:42:09 +0100

    Declare outputs of environments module nuke lists

---
+++ terraform/environments/outputs.tf
@@ -0,0 +1,11 @@
+output "environment_nuke_accounts" {
+  value = module.environments.environment_nuke_accounts
+}
+
+output "environment_nuke_blocklist_accounts" {
+  value = module.environments.environment_nuke_blocklist_accounts
+}
+
+output "environment_rebuild_after_nuke_accounts" {
+  value = module.environments.environment_rebuild_after_nuke_accounts
+}
