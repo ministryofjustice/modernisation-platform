a7bca7614d73cb63af49685d232877cf0ea1392d
khatraf <khatra.farah@digital.justice.gov.uk>
2025-06-20 12:44:25 +0100

    more testing

--- terraform/modules/vpc-nacls/locals.tf
+++ terraform/modules/vpc-nacls/locals.tf
@@ -1,39 +1,45 @@
 locals {
-  laa_workspaces = [
-    "laa-development",
-    "laa-test",
+  laa_vpc_keys = [
+    "laa-production",
     "laa-preproduction",
-    "laa-production"
+    "laa-test",
+    "laa-development"
   ]
 
-  subnet_suffixes = ["a", "b", "c"]
-
-  cidr_blocks = {
-    "laa-development"   = "13.17.132.44/32"
-    "laa-test"          = "13.17.132.45/32"
-    "laa-preproduction" = "13.17.132.46/32"
-    "laa-production"    = "13.17.132.47/32"
-  }
-
-  base_rule_numbers = {
-    "laa-development"   = 6010
-    "laa-test"          = 6020
-    "laa-preproduction" = 6030
-    "laa-production"    = 6040
-  }
-
-  laa_ssh_acl_rules_by_workspace = {
-    for ws in local.laa_workspaces : ws => {
-      for idx, suffix in local.subnet_suffixes :
-      "subnet_${suffix}_ssh" => {
-        cidr_block  = local.cidr_blocks[ws]
-        egress      = true
-        from_port   = 22
-        protocol    = "tcp"
-        rule_action = "allow"
-        rule_number = local.base_rule_numbers[ws] + idx
-        to_port     = 22
-      }
+  # SSH NACL rules specific to LAA VPCs
+  laa_ssh_acl_rules = {
+    laa_development_subnet_a_ssh = {
+      cidr_block  = "13.17.132.44/32"
+      egress      = true
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      rule_action = "allow"
+      rule_number = 6010
+    }
+    laa_development_subnet_b_ssh = {
+      cidr_block  = "13.17.132.44/32"
+      egress      = true
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      rule_action = "allow"
+      rule_number = 6011
+    }
+    laa_development_subnet_c_ssh = {
+      cidr_block  = "13.17.132.44/32"
+      egress      = true
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      rule_action = "allow"
+      rule_number = 6012
     }
   }
+
+  # Determine if the current VPC is in the LAA set
+  apply_laa_ssh_rules = contains(local.laa_vpc_keys, var.vpc_name)
+
+  # Only expose SSH rules for LAA environments
+  laa_ssh_rules_to_apply = local.apply_laa_ssh_rules ? local.laa_ssh_acl_rules : {}
 }
