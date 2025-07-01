2006b1e6dc20de9067f07ed47c90783cd235ad74
Kudzai-moj <kudzai.mtoko@digital.justice.gov.uk>
2025-07-01 10:22:42 +0100

    Revert "added firehose logging"
    This reverts commit c8bd79fede68a34da8ccc65e3a361a1f992e3a14.

--- terraform/environments/core-logging/s3_logging.tf
+++ terraform/environments/core-logging/s3_logging.tf
@@ -639,56 +639,46 @@ resource "aws_iam_role" "firehose_to_s3" {
 
 resource "aws_iam_role_policy" "firehose_to_s3_policy" {
   role = aws_iam_role.firehose_to_s3.id
-
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow",
+        Effect = "Allow"
         Action = [
           "s3:PutObject",
           "s3:PutObjectAcl"
-        ],
+        ]
         Resource = "${module.s3-bucket-modernisation-platform-waf-logs.bucket.arn}/*"
       },
       {
-        Effect = "Allow",
+        Effect = "Allow"
         Action = [
           "kms:Encrypt",
           "kms:Decrypt",
           "kms:GenerateDataKey*",
           "kms:DescribeKey"
-        ],
+        ]
         Resource = aws_kms_key.s3_modernisation_platform_waf_logs.arn
       },
       {
-        Effect = "Allow",
+        Effect = "Allow"
         Action = [
           "firehose:PutRecord",
           "firehose:PutRecordBatch"
-        ],
+        ]
         Resource = "arn:aws:firehose:eu-west-2:${data.aws_caller_identity.current.account_id}:deliverystream/waf-logs-to-s3"
       },
       {
-        Effect = "Allow",
+        Effect = "Allow"
         Action = [
-          "logs:PutLogEvents",
-          "logs:CreateLogStream",
-          "logs:CreateLogGroup"
-        ],
-        Resource = "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/kinesisfirehose/waf-logs-to-s3:*"
-      },
-      {
-        Effect = "Allow",
-        Action = [
-          "lambda:InvokeFunction"
-        ],
-        Resource = aws_lambda_function.prefix_transform.arn
+          "logs:PutLogEvents"
+        ]
+        # Resource = "*"
+          Resource = "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/kinesisfirehose/waf-logs-to-s3:*"
       }
     ]
   })
 }
-
 
 # This stream receives WAF logs from member accounts and delivers them to the centralized S3 bucket
 resource "aws_kinesis_firehose_delivery_stream" "waf_logs_to_s3" {
@@ -704,26 +694,6 @@ resource "aws_kinesis_firehose_delivery_stream" "waf_logs_to_s3" {
     buffering_interval = 300
     compression_format = "GZIP"
     kms_key_arn        = aws_kms_key.s3_modernisation_platform_waf_logs.arn
-
-    cloudwatch_logging_options {
-      enabled         = true
-      log_group_name  = "/aws/kinesisfirehose/waf-logs-to-s3"
-      log_stream_name = "S3Delivery"
-    }
-
-    # ✅ Inline Lambda processor block
-    processing_configuration {
-      enabled = true
-
-      processors {
-        type = "Lambda"
-
-        parameters {
-          parameter_name  = "LambdaArn"
-          parameter_value = "${aws_lambda_function.prefix_transform.qualified_arn}"
-        }
-      }
-    }
   }
 }
 
@@ -810,50 +780,4 @@ resource "aws_iam_role_policy" "cwl_to_firehose_policy" {
       Resource = [aws_kinesis_firehose_delivery_stream.waf_logs_to_s3.arn]
     }]
   })
-}
-
-# lambda for prefix
-resource "aws_iam_role" "firehose_lambda_role" {
-  name = "firehose_lambda_prefix_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      },
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy" "firehose_lambda_policy" {
-  role = aws_iam_role.firehose_lambda_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_lambda_function" "prefix_transform" {
-  filename         = "${path.module}/lambda/lambda_firehose_prefix_transform.zip"
-  function_name    = "firehose-prefix-transform"
-  role             = aws_iam_role.firehose_lambda_role.arn
-  handler          = "lambda_firehose_prefix_transform.lambda_handler"
-  runtime          = "python3.11"
-  timeout          = 60
-  source_code_hash = filebase64sha256("${path.module}/lambda/lambda_firehose_prefix_transform.zip")
-  publish          = true
 }
