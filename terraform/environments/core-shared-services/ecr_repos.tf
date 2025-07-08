c97635ba566e2e9f19f3a71d8d019fde5914b442
Andy Welsh <andy.welsh@justice.gov.uk>
2025-07-08 10:10:50 +0100

    chore: add same iam grant for soa managed

--- terraform/environments/core-shared-services/ecr_repos.tf
+++ terraform/environments/core-shared-services/ecr_repos.tf
@@ -1360,7 +1360,8 @@ module "soa_managed_ecr_repo" {
   app_name = "soa-managed"
 
   push_principals = [
-    "arn:aws:iam::${local.environment_management.account_ids["laa-ccms-soa-development"]}:root"
+    "arn:aws:iam::${local.environment_management.account_ids["laa-ccms-soa-development"]}:root",
+    "arn:aws:iam::${local.environment_management.account_ids["laa-ccms-soa-development"]}:role/modernisation-platform-oidc-cicd"
   ]
 
   pull_principals = [
