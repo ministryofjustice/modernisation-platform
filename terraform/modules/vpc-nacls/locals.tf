e08ad4c2b5edb76f0de77e70460dfe310025d5ec
mikereiddigital <mike.reid@justice.gov.uk>
2025-07-09 16:31:50 +0100

    Adds NACL rules for port 8022 outbound for laa-general-private subnets only

--- terraform/modules/vpc-nacls/locals.tf
+++ terraform/modules/vpc-nacls/locals.tf
@@ -42,4 +42,69 @@ locals {
 
   # Only expose SSH rules for LAA environments
   laa_ssh_rules_to_apply = local.apply_laa_ssh_rules ? local.laa_ssh_acl_rules : {}
+
+
+# LAA Custom Egress Ports
+
+  laa_custom_egress_tcp_acl_rules = {
+        laa_development_subnet_a_ftp_custom = {
+        cidr_block  = "0.0.0.0/0"
+        egress      = true
+        from_port   = 8022
+        to_port     = 8022
+        protocol    = "tcp"
+        rule_action = "allow"
+        rule_number = 6050
+      }
+      laa_development_subnet_b_ftp_custom = {
+        cidr_block  = "0.0.0.0/0"
+        egress      = true
+        from_port   = 8022
+        to_port     = 8022
+        protocol    = "tcp"
+        rule_action = "allow"
+        rule_number = 6051
+      }
+      laa_development_subnet_c_ftp_custom = {
+        cidr_block  = "0.0.0.0/0"
+        egress      = true
+        from_port   = 8022
+        to_port     = 8022
+        protocol    = "tcp"
+        rule_action = "allow"
+        rule_number = 6052
+      }
+      laa_production_subnet_a_ftp_custom = {
+        cidr_block  = "0.0.0.0/0"
+        egress      = true
+        from_port   = 8022
+        to_port     = 8022
+        protocol    = "tcp"
+        rule_action = "allow"
+        rule_number = 6050
+      }
+      laa_production_subnet_b_ftp_custom = {
+        cidr_block  = "0.0.0.0/0"
+        egress      = true
+        from_port   = 8022
+        to_port     = 8022
+        protocol    = "tcp"
+        rule_action = "allow"
+        rule_number = 6051
+      }
+      laa_production_subnet_c_ftp_custom = {
+        cidr_block  = "0.0.0.0/0"
+        egress      = true
+        from_port   = 8022
+        to_port     = 8022
+        protocol    = "tcp"
+        rule_action = "allow"
+        rule_number = 6052
+      }
+  }
+
+  apply_laa_custom_tcp_rules = contains(local.laa_custom_egress_tcp_acl_rules, var.vpc_name)
+
+  laa_custom_tcp_rules_to_apply = local.apply_laa_custom_tcp_rules ? local.laa_custom_egress_tcp_acl_rules : {}
+
 }
