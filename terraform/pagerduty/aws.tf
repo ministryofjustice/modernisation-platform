51b3a9bd7ea67415fd6a7c4b3c30ef8db17ed74e
Kudzai-moj <kudzai.mtoko@digital.justice.gov.uk>
2025-07-16 12:36:57 +0100

    removing laa-cwa references from codebase

--- terraform/pagerduty/aws.tf
+++ terraform/pagerduty/aws.tf
@@ -57,8 +57,6 @@ resource "aws_secretsmanager_secret_version" "pagerduty_integration_keys" {
     delius_core_nonprod_alarms           = pagerduty_service_integration.delius_core_nonprod_cloudwatch.integration_key
     delius_core_prod_alarms              = pagerduty_service_integration.delius_core_prod_cloudwatch.integration_key
     delius_oracle_nonprod_alarms         = pagerduty_service_integration.delius_oracle_nonprod_cloudwatch.integration_key
-    laa_cwa_nonprod_alarms               = pagerduty_service_integration.cwa_non_prod.integration_key
-    laa_cwa_prod_alarms                  = pagerduty_service_integration.cwa_prod.integration_key
     laa_apex_nonprod_alarms              = pagerduty_service_integration.apex_non_prod.integration_key
     laa_apex_prod_alarms                 = pagerduty_service_integration.apex_prod.integration_key
     electronic_monitoring_data_alarms    = pagerduty_service_integration.electronic_monitoring_data_cloudwatch.integration_key
