b7cf59e467755c9746d0c2fed4d8cb38db9a10cc
Rich Green <richard.green@digital.justice.gov.uk>
2025-07-16 09:08:39 +0100

    Update member environment configs to v6 of aws tf provider

--- terraform/environments/cooker/s3.tf
+++ terraform/environments/cooker/s3.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   # checkov:skip=CKV_TF_1:
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=v8.2.1"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=9facf9fc8f8b8e3f93ffbda822028534b9a75399" # v9.0.0
 
   bucket_prefix      = "s3-security-testing-bucket"
   versioning_enabled = true
