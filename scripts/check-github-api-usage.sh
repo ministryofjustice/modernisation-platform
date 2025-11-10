ec822f4395b70d1d26a08cf2c166437945791735
mikereiddigital <mike.reid@justice.gov.uk>
2025-11-10 15:38:39 +0000

    Issue whereby the two values of the api rate check were not being treated as integers.

--- scripts/check-github-api-usage.sh
+++ scripts/check-github-api-usage.sh
@@ -3,10 +3,12 @@
 set -o pipefail
 
 # This script checks the remaining API usage remaining for the authenticated GITHUB APP.
-
 # If the remaining amount is less than the threshold (defined in RATE_LIMIT_THRESHOLD) then the script forces an exit.
-
 # Requires $GH_TOKEN taken as output from the GH App session creation.
+
+# Declare the two main vars of the check as integers:
+declare -i REMAINING_INT
+declare -i RATE_THRESHOLD_INT
 
 echo "Fetching API rate limit for App installation..."
 RESPONSE=$(curl -s \
@@ -25,9 +27,13 @@ REMAINING=$(echo "$RESPONSE" | jq '.resources.core.remaining')
 echo "Rate used: $USED"
 echo "Rate remaining: $REMAINING"
 
+# Set to ensure these vars are integers.
+REMAINING_INT=${REMAINING:-0}
+RATE_THRESHOLD_INT=${RATE_THRESHOLD:-0}
+
 if [ "$REMAINING" -lt "$RATE_THRESHOLD" ]; then
-echo "❌ Rate usage below threshold ($RATE_THRESHOLD > $REMAINING). Exiting."
+echo "❌ Rate usage at threshold ($RATE_THRESHOLD_INT => $REMAINING_INT). Exiting."
 exit 1
 else
-echo "✅ Rate usage within threshold ($RATE_THRESHOLD <= $REMAINING)."
+echo "✅ Rate usage within threshold ($RATE_THRESHOLD_INT < $REMAINING_INT)."
 fi
