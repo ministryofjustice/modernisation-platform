90df2dec561950a51ee4bcd86dbb130b58584eb4
mikereiddigital <mike.reid@justice.gov.uk>
2025-11-27 15:07:28 +0000

    Custom rules for CICA general_private rules to allow access from AP.

--- terraform/modules/vpc-nacls/locals.tf
+++ terraform/modules/vpc-nacls/locals.tf
@@ -80,4 +80,30 @@ locals {
 
   laa_custom_tcp_rules_to_apply = local.apply_laa_custom_tcp_rules ? local.laa_custom_egress_tcp_acl_rules : {}
 
+
+
+# Custom Rules for access to selected CICA subnets from AP
+
+  cica_vpc_keys = [
+    "cica_development",
+    "cica_production"
+  ]
+
+cica_general_private_ap_access_rules = {
+  cica_development_subnet_db = {
+    cidr_block  = "10.27.128.28/32"
+    egress      = false
+    from_port   = 1521
+    to_port     = 1521
+    protocol    = "tcp"
+    rule_action = "allow"
+    rule_number = 2015
+  }
+}
+
+apply_cica_ap_rules = contains(local.cica_vpc_keys, var.vpc_name)
+
+cica_db_rules_to_apply = local.apply_cica_ap_rules ? local.cica_general_private_ap_access_rules : {}
+
+
 }
