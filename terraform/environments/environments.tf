07f3030e0d7adf3a0e716ecad976cc386c37af75
Rich Green <richard.green@digital.justice.gov.uk>
2026-01-16 16:02:35 +0000

    Update Terraform module source reference to v8.2.0

--- terraform/environments/environments.tf
+++ terraform/environments/environments.tf
@@ -1,5 +1,5 @@
 module "environments" {
-  source                             = "github.com/ministryofjustice/modernisation-platform-terraform-environments?ref=b78577cc957def05bbfc4edd215612fa219491cb" # v8.1.0
+  source                             = "github.com/ministryofjustice/modernisation-platform-terraform-environments?ref=810b0385e0e0537e95ede1db2d968d323959ad5f" # v8.2.0
   environment_directory              = "../../environments"
   environment_parent_organisation_id = local.modernisation_platform_ou_id
   environment_prefix                 = "modernisation-platform"
