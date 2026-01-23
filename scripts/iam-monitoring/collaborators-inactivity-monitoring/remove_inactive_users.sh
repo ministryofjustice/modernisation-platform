05d39b6d2020b44ef5c6af555d5ed74bcc7d080b
Sukesh <sukesh.reddygade@digital.justice.gov.uk>
2026-01-23 13:58:25 +0000

    Test

--- scripts/iam-monitoring/collaborators-inactivity-monitoring/remove_inactive_users.sh
+++ scripts/iam-monitoring/collaborators-inactivity-monitoring/remove_inactive_users.sh
@@ -2,6 +2,13 @@
 COLLAB_FILE="./collaborators.json"
 PR_BODY="The following collaborators were inactive for more than $threshold days and have been removed automatically:\n\n"
 
+# Exit if no inactive users
+if [ ! -f final_users.list ] || [ ! -s final_users.list ]; then
+    echo "No inactive collaborators found. Exiting."
+    exit 0
+fi
+
+# Read inactive users into array
 inactive_users=()
 while IFS= read -r user; do
     inactive_users+=("$user")
