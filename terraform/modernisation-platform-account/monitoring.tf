68b868838bc7dd3cc9b0a8bb976885bd40115eb7
ep-93 <109581241+ep-93@users.noreply.github.com>
2026-02-17 10:39:12 +0000

    Update monitoring.tf

--- terraform/modernisation-platform-account/monitoring.tf
+++ terraform/modernisation-platform-account/monitoring.tf
@@ -18,3 +18,36 @@ moved {
   from = module.core_monitoring.module.pagerduty_core_alerts.aws_sns_topic_subscription.pagerduty_subscription["securityhub-alarms"]
   to   = module.pagerduty_core_alerts.aws_sns_topic_subscription.pagerduty_subscription["securityhub-alarms"]
 }
+
+resource "aws_securityhub_automation_rule" "suppress_tf_state_bucket_cross_account" {
+  rule_name     = "suppress-tf-state-bucket-cross-account-policy"
+  rule_order    = 1
+  description   = "Suppress cross-account S3 policy finding for terraform state bucket"
+
+  criteria {
+    resource_id {
+      comparison = "EQUALS"
+      value      = "arn:aws:s3:::modernisation-platform-terraform-state"
+    }
+
+    title {
+      comparison = "CONTAINS"
+      value      = "S3 general purpose bucket policies should restrict access to other AWS accounts"
+    }
+  }
+
+  actions {
+    type = "FINDING_FIELDS_UPDATE"
+
+    finding_fields_update {
+      workflow {
+        status = "SUPPRESSED"
+      }
+
+      note {
+        text       = "Approved exception - Terraform backend requires controlled cross-account access."
+        updated_by = "terraform"
+      }
+    }
+  }
+}
