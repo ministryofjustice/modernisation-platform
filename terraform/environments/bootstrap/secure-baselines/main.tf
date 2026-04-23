065ba7498c17fbc4e9c1c90a01bac2d50ae35d1f
mikereiddigital <mike.reid@justice.gov.uk>
2026-04-23 09:06:39 +0100

    sprinkler testing

--- terraform/environments/bootstrap/secure-baselines/main.tf
+++ terraform/environments/bootstrap/secure-baselines/main.tf
@@ -6,7 +6,7 @@ data "aws_kms_key" "cloudtrail_key" {
 
 #trivy:ignore:AVD-AWS-0136
 module "baselines" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-baselines?ref=607459430376ad2eca8b8647bd70ba9a4a1deda8" # Testing
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-baselines?ref=d9186d6581d127feacd426091eb55486e29f5e08" # Testing
   providers = {
     # Default and replication regions
     aws                    = aws.workspace-eu-west-2
