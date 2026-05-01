9a20053cbdfd206762615e509365a1d886c09552
Kudzai-moj <kudzai.mtoko@digital.justice.gov.uk>
2026-05-01 21:16:57 +0100

    fix

--- terraform/environments/core-shared-services/bucket.tf
+++ terraform/environments/core-shared-services/bucket.tf
@@ -9,7 +9,8 @@ module "imagebuilder_log_bucket" {
   bucket_prefix       = "ec2-image-builder-logs-"
   versioning_enabled  = false
   replication_enabled = false
-  sse_algorithm       = "AES256"
+  custom_kms_key      = aws_kms_key.imagebuilder_log_bucket.arn
+  sse_algorithm       = "aws:kms"
 
   lifecycle_rule = [
     {
@@ -46,6 +47,58 @@ module "imagebuilder_log_bucket" {
 
 
   tags = local.tags
+}
+
+resource "aws_kms_key" "imagebuilder_log_bucket" {
+  description             = "KMS key for EC2 Image Builder logs S3 bucket encryption"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+  policy                  = data.aws_iam_policy_document.imagebuilder_log_bucket_kms_key_policy.json
+}
+
+data "aws_iam_policy_document" "imagebuilder_log_bucket_kms_key_policy" {
+  statement {
+    sid    = "EnableIAMPermissions"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      ]
+    }
+
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "AllowImageBuilderUseOfKey"
+    effect = "Allow"
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ImageBuilder"
+      ]
+    }
+
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey"
+    ]
+
+    resources = ["*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "kms:ViaService"
+      values   = ["s3.${data.aws_region.current.name}.amazonaws.com"]
+    }
+  }
 }
 
 output "imagebuilder_log_bucket_id" {
