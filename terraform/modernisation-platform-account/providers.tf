9dc01f366f10011662087e3ce72f3cb701991009
Rich Green <richard.green@digital.justice.gov.uk>
2026-05-06 17:14:04 +0100

    Add default tags to all AWS provider configurations in `terraform/modernisation-platform-account`

--- terraform/modernisation-platform-account/providers.tf
+++ terraform/modernisation-platform-account/providers.tf
@@ -3,86 +3,109 @@
 # Default provider
 provider "aws" {
   region = "eu-west-2"
+  default_tags { tags = local.tags }
 }
 
 provider "aws" {
   region = "ap-northeast-1"
   alias  = "modernisation-platform-ap-northeast-1"
+  default_tags { tags = local.tags }
 }
 
 provider "aws" {
   region = "ap-northeast-2"
   alias  = "modernisation-platform-ap-northeast-2"
+  default_tags { tags = local.tags }
 }
 
 provider "aws" {
   region = "ap-south-1"
   alias  = "modernisation-platform-ap-south-1"
+  default_tags { tags = local.tags }
 }
 
 provider "aws" {
   region = "ap-southeast-1"
   alias  = "modernisation-platform-ap-southeast-1"
+  default_tags { tags = local.tags }
 }
 
 provider "aws" {
   region = "ap-southeast-2"
   alias  = "modernisation-platform-ap-southeast-2"
+  default_tags { tags = local.tags }
 }
 
 provider "aws" {
   region = "ca-central-1"
   alias  = "modernisation-platform-ca-central-1"
+  default_tags { tags = local.tags }
 }
 
 provider "aws" {
   region = "eu-central-1"
   alias  = "modernisation-platform-eu-central-1"
+  default_tags { tags = local.tags }
 }
 
 provider "aws" {
   region = "eu-north-1"
   alias  = "modernisation-platform-eu-north-1"
+  default_tags { tags = local.tags }
 }
 
 provider "aws" {
   region = "eu-west-1"
   alias  = "modernisation-platform-eu-west-1"
+  default_tags { tags = local.tags }
 }
 
 provider "aws" {
   region = "eu-west-2"
   alias  = "modernisation-platform-eu-west-2"
+  default_tags { tags = local.tags }
 }
 
 provider "aws" {
   region = "eu-west-3"
   alias  = "modernisation-platform-eu-west-3"
+  default_tags { tags = local.tags }
 }
 
 provider "aws" {
   region = "sa-east-1"
   alias  = "modernisation-platform-sa-east-1"
+  default_tags { tags = local.tags }
 }
 
 provider "aws" {
   region = "us-east-1"
   alias  = "modernisation-platform-us-east-1"
+  default_tags { tags = local.tags }
 }
 
 provider "aws" {
   region = "us-east-2"
   alias  = "modernisation-platform-us-east-2"
+  default_tags { tags = local.tags }
+}
+
+provider "aws" {
+  region = "us-east-2"
+  alias  = "modernisation-platform-us-east-2"
+  default_tags { tags = local.tags }
 }
 
 provider "aws" {
   region = "us-west-1"
   alias  = "modernisation-platform-us-west-1"
+  default_tags { tags = local.tags }
 }
 
 provider "aws" {
   region = "us-west-2"
   alias  = "modernisation-platform-us-west-2"
+  default_tags { tags = local.tags }
 }
 
 # AWS provider for core-logging
@@ -92,4 +115,5 @@ provider "aws" {
   assume_role {
     role_arn = "arn:aws:iam::${local.environment_management.account_ids["core-logging-production"]}:role/ModernisationPlatformAccess"
   }
+  default_tags { tags = local.tags }
 }
