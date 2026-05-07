fc6b3c849768454c706ef1ffa3bf0a6472b300c8
Rich Green <richard.green@digital.justice.gov.uk>
2026-05-07 08:30:53 +0100

    Add default tags to AWS provider configurations in secure-baselines and single-sign-on

--- terraform/environments/bootstrap/single-sign-on/providers.tf
+++ terraform/environments/bootstrap/single-sign-on/providers.tf
@@ -1,6 +1,7 @@
 # AWS provider (default)
 provider "aws" {
   region = "eu-west-2"
+  default_tags { tags = local.tags }
 }
 
 # AWS provider (AWS root account for AWS SSO management)
@@ -10,6 +11,7 @@ provider "aws" {
   assume_role {
     role_arn = "arn:aws:iam::${local.environment_management.aws_organizations_root_account_id}:role/ModernisationPlatformSSOAdministrator"
   }
+  default_tags { tags = local.tags }
 }
 
 # AWS provider (modernisation-secrets-read): Required for assuming a role into modernisation platform account to read secrets
@@ -28,6 +30,7 @@ provider "aws" {
   assume_role {
     role_arn = "arn:aws:iam::${local.environment_management.account_ids[terraform.workspace]}:role/ModernisationPlatformAccess"
   }
+  default_tags { tags = local.tags }
 }
 
 # AWS provider for the Modernisation Platform, to get things from there if required
