f9b0e9fef29695e8f41beafbfe02ece3410c4bbf
Kudzai-moj <kudzai.mtoko@digital.justice.gov.uk>
2026-05-11 14:33:26 +0100

    push

--- terraform/environments/core-shared-services/bucket.tf
+++ terraform/environments/core-shared-services/bucket.tf
@@ -1,15 +1,16 @@
 # tfsec:ignore:aws-s3-enable-versioning tfsec:ignore:aws-s3-encryption-customer-key
 module "imagebuilder_log_bucket" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=479b92623954a75f96a6da8ebb4070a8581c227e"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket?ref=c67758cd6d263bec9c225b99b6f76d6074514159"
 
   providers = {
     aws.bucket-replication = aws.bucket-replication
   }
 
-  bucket_prefix       = "ec2-image-builder-logs-"
-  sse_algorithm       = "AES256"
-  versioning_enabled  = false
-  replication_enabled = false
+  bucket_prefix               = "ec2-image-builder-logs-"
+  sse_algorithm               = "aws:kms"
+  enforce_kms_request_headers = false
+  versioning_enabled          = false
+  replication_enabled         = false
 
   lifecycle_rule = [
     {
