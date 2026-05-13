af2e8ffd0f753dec63a5d3f5f5644d4c67894247
Mark Roberts <mark.roberts@justice.gov.uk>
2026-05-13 10:30:23 +0100

    Ram share module update

--- terraform/modules/ram-principal-association/main.tf
+++ terraform/modules/ram-principal-association/main.tf
@@ -1,6 +1,3 @@
-locals {
-  share_secondary = var.vpc_name == "hmpps" && var.environment == "production" ? true : false
-}
 
 data "aws_ram_resource_share" "default" {
   provider = aws.share-host
@@ -22,28 +19,6 @@ resource "aws_ram_principal_association" "default" {
   resource_share_arn = data.aws_ram_resource_share.default.arn
 }
 
-# Secondary CIDR subnet share association
-data "aws_ram_resource_share" "secondary" {
-  count    = local.share_secondary ? 1 : 0
-  provider = aws.share-host
-
-  name = "${var.vpc_name}-${var.environment}-${var.subnet_set}-secondary-resource-share"
-
-  resource_owner = "SELF"
-
-  filter {
-    name   = "Name"
-    values = ["${var.vpc_name}-${var.environment}-${var.subnet_set}-secondary"]
-  }
-}
-
-resource "aws_ram_principal_association" "secondary" {
-  provider = aws.share-host
-  count    = local.share_secondary ? 1 : 0
-
-  principal          = var.principal
-  resource_share_arn = data.aws_ram_resource_share.secondary[0].arn
-}
 
 #configure acm certificate share association
 data "aws_ram_resource_share" "acm" {
