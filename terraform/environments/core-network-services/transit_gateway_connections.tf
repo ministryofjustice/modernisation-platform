d007566ae9416555d13e3db2b0ea9076aecbca1c
David Sibley <david.sibley@justice.gov.uk>
2026-06-18 13:39:47 +0100

    introduced try() to ensure presence of Name tag when filtering for live or non_live data attachments

--- terraform/environments/core-network-services/transit_gateway_connections.tf
+++ terraform/environments/core-network-services/transit_gateway_connections.tf
@@ -74,19 +74,21 @@ locals {
   }
 
   tgw_live_data_attachments = {
-    for k, v in data.aws_ec2_transit_gateway_vpc_attachment.transit_gateway_all : k => v.tags.Name if(
+    for k, v in data.aws_ec2_transit_gateway_vpc_attachment.transit_gateway_all : k => v.tags.Name if try(
       length(regexall("(?:production-attachment)", v.tags.Name)) > 0 ||
       length(regexall("(?:preproduction-attachment)", v.tags.Name)) > 0 ||
-      length(regexall("(?:-live_data-attachment)", v.tags.Name)) > 0
+      length(regexall("(?:-live_data-attachment)", v.tags.Name)) > 0,
+      false
     )
   }
 
   tgw_non_live_data_attachments = {
-    for k, v in data.aws_ec2_transit_gateway_vpc_attachment.transit_gateway_all : k => v.tags.Name if(
+    for k, v in data.aws_ec2_transit_gateway_vpc_attachment.transit_gateway_all : k => v.tags.Name if try(
       length(regexall("(?:development-attachment)", v.tags.Name)) > 0 ||
       length(regexall("(?:sandbox-attachment)", v.tags.Name)) > 0 ||
       length(regexall("(?:test-attachment)", v.tags.Name)) > 0 ||
-      length(regexall("(?:-non_live_data-attachment)", v.tags.Name)) > 0
+      length(regexall("(?:-non_live_data-attachment)", v.tags.Name)) > 0,
+      false
     )
   }
 
