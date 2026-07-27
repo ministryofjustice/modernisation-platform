d1dc3b15b19db371e8e0fb62341d7b6d33b562f8
mikereiddigital <mike.reid@justice.gov.uk>
2026-07-27 14:34:03 +0100

    Removed external from versions

--- terraform/environments/core-shared-services/versions.tf
+++ terraform/environments/core-shared-services/versions.tf
@@ -4,10 +4,6 @@ terraform {
       version = "~> 6.0"
       source  = "hashicorp/aws"
     }
-    external = {
-      version = "~> 2.0"
-      source  = "hashicorp/external"
-    }
   }
   required_version = "~> 1.0"
 }
