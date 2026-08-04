52b251d750221f32e8f6bebf259b0703e97bb325
mikereiddigital <mike.reid@justice.gov.uk>
2026-08-04 10:19:11 +0100

    Add skip for CKV_SECRET_6

--- terraform/environments/core-shared-services/transform.tf
+++ terraform/environments/core-shared-services/transform.tf
@@ -214,6 +214,7 @@ resource "aws_secretsmanager_secret" "aws_transform_url" {
 }
 
 resource "aws_secretsmanager_secret_version" "aws_transform_url" {
+  #checkov:skip=CKV_SECRET_6: Seeded placeholder value is not a real secret; actual value is set outside Terraform
   secret_id     = aws_secretsmanager_secret.aws_transform_url.id
   secret_string = "placeholder"
   lifecycle {
