f08fe642f6b78e9d3253b388ff8741408a987301
ghill26 <George.Hill2@justice.gov.uk>
2026-08-06 10:23:46 +0100

    Update primary rota

--- terraform/pagerduty/member-services-integrations.tf
+++ terraform/pagerduty/member-services-integrations.tf
@@ -1888,9 +1888,9 @@ locals {
     "william.gibbon",
   ]
   octo_platform_operations_primary_schedule_user_order = [
-    "steve.williams4",
-    "george.hill2",
     "annesa.mariyam",
+    "george.hill2",
+    "steve.williams4",
   ]
   octo_platform_operations_secondary_schedule_user_order = [
     "sebastian.norris",
