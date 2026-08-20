238f0580dd0e56e967155bb330156274e7f3df80
mikereiddigital <mike.reid@justice.gov.uk>
2026-08-20 16:27:43 +0100

    Adds back modernisation-platform as a repo that can call github-actions role which is required for the ram-association workflow to run.

--- terraform/modernisation-platform-account/iam.tf
+++ terraform/modernisation-platform-account/iam.tf
@@ -274,7 +274,7 @@ module "github-oidc" {
   source                      = "github.com/ministryofjustice/modernisation-platform-github-oidc-provider?ref=5dc9bc211d10c58de4247fa751c318a3985fc87b" # v4.0.0
   additional_permissions      = data.aws_iam_policy_document.oidc-deny-specific-actions.json
   additional_managed_policies = ["arn:aws:iam::aws:policy/AdministratorAccess"]
-  github_repositories         = ["ministryofjustice/modernisation-platform-ami-builds:*"]
+  github_repositories         = ["ministryofjustice/modernisation-platform:*", "ministryofjustice/modernisation-platform-ami-builds:*"]
   tags_common                 = { "Name" = format("%s-oidc", terraform.workspace) }
   tags_prefix                 = ""
 }
